dccdcc4dfe21ebab0929ddbdabda127ddbfa40ea
Adrian Reber <areber@redhat.com>
2016-03-15 13:21:11 +0000

    Remove static from prepare_task_entries function
    For the upcoming userfaultfd integration the lazy-pages mode needs to
    setup the criu infrastructure to read the pages files.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/crtools.h
+++ criu/include/crtools.h
@@ -23,6 +23,7 @@ extern int convert_to_elf(char *elf_path, int fd_core);
 extern int cr_check(void);
 extern int cr_exec(int pid, char **opts);
 extern int cr_dedup(void);
+extern int prepare_task_entries(void);
 
 extern int check_add_feature(char *arg);
 
