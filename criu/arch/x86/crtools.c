25299a50e87d890cd78e1154f5280f35126f2c88
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-04-12 19:10:42 +0300

    cr-exec: add non-generated sys-exec-tbl for x86
    Impact: search sys-exec-tbl-32.c for compatible tasks.
    
    Rename task_in_compat_mode to arch_task_compatible and use it in
    find_syscall for 64-bit to check compatible task's syscall nr.
    It still will not execute syscall in 32-tasks, as we still do not
    have 32-bit pie (arch_can_dump_task will return false for these tasks).
    
    NOTE: be sure to `make mrproper` on criu directory before applying this
    patch, as before `criu/arch/x86/sys-exec-tbl.c` was autogenerated,
    it will make conflict if you try to apply this patch on dirty directory.
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/arch/x86/crtools.c
+++ criu/arch/x86/crtools.c
@@ -53,7 +53,7 @@ void parasite_setup_regs(unsigned long new_ip, void *stack, user_regs_struct_t *
 	regs->flags &= ~(X86_EFLAGS_TF | X86_EFLAGS_DF | X86_EFLAGS_IF);
 }
 
-static int task_in_compat_mode(pid_t pid)
+int arch_task_compatible(pid_t pid)
 {
 	unsigned long cs, ds;
 
@@ -77,7 +77,7 @@ static int task_in_compat_mode(pid_t pid)
 
 bool arch_can_dump_task(pid_t pid)
 {
-	if (task_in_compat_mode(pid)) {
+	if (arch_task_compatible(pid)) {
 		pr_err("Can't dump task %d running in 32-bit mode\n", pid);
 		return false;
 	}
