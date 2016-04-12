9f8bf116af17c40a44c70bb8d7edce521ee6fa90
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-04-12 19:10:48 +0300

    parasite: refactor macros for compat/native usage
    In code they yet use native parasite type, but in definition
    this parameter is not used (will be used at the next patch).
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -2257,7 +2257,7 @@ static int prepare_restorer_blob(void)
 	 * in turn will lead to set-exe-file prctl to fail with EBUSY.
 	 */
 
-	restorer_len = pie_size(restorer_blob);
+	restorer_len = pie_size(restorer);
 	restorer = mmap(NULL, restorer_len,
 			PROT_READ | PROT_WRITE | PROT_EXEC,
 			MAP_PRIVATE | MAP_ANON, 0, 0);
@@ -2281,7 +2281,7 @@ static int remap_restorer_blob(void *addr)
 		return -1;
 	}
 
-	ELF_RELOCS_APPLY_RESTORER(addr, addr);
+	ELF_RELOCS_APPLY(restorer, addr, addr);
 	return 0;
 }
 
