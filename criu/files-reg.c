5e2458e377410de5425976136a209fb947263573
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-05-18 19:28:15 +0300

    vz7: files-reg: create spfs file in case of parent mount is SPFS
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -45,6 +45,8 @@
 #include "fault-injection.h"
 #include "external.h"
 #include "memfd.h"
+#include "spfs.h"
+#include "filesystems.h"
 
 #include "protobuf.h"
 #include "util.h"
@@ -2087,6 +2089,7 @@ int open_path(struct file_desc *d,
 	char *orig_path = NULL;
 	char path[PATH_MAX];
 	int inh_fd = -1;
+	struct mount_info *mi;
 
 	if (inherited_fd(d, &tmp))
 		return tmp;
@@ -2155,6 +2158,15 @@ int open_path(struct file_desc *d,
 	}
 
 	mntns_root = mntns_get_root_by_mnt_id(rfi->rfe->mnt_id);
+
+	mi = lookup_mnt_id(rfi->rfe->mnt_id);
+	if (mi && mi->fstype->mount == spfs_mount) {
+		if (spfs_create_file(mntns_root, rfi) < 0) {
+			pr_err("Failed to create SPFS path\n");
+			return -1;
+		}
+	}
+
 ext:
 	tmp = open_cb(mntns_root, rfi, arg);
 	if (tmp < 0) {
