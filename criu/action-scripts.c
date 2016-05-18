d922d226f0276521d5e68764d44526270fc91949
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-05-18 19:28:34 +0300

    vz7: action-scripts: ACT_POST_NET_LOCK stage added
    This stage is required for NFS migration to add additional iptables
    rules to allow NFS ports. Without this any request to a file, located on
    NFS, will stuck. This stage is executed right _after_ network lock.
    
    Feature: SPFS
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/action-scripts.c
+++ criu/action-scripts.c
@@ -30,6 +30,7 @@ static const char *action_names[ACT_MAX] = {
 	[ACT_POST_RESUME] = "post-resume",
 	[ACT_ORPHAN_PTS_MASTER] = "orphan-pts-master",
 	[ACT_STATUS_READY] = "status-ready",
+	[ACT_POST_NET_LOCK] = "post-network-lock",
 };
 
 struct script {
