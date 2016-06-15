c760530135f8c7e70573e4be71449d632d2fac1e
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-06-15 17:46:41 +0300

    vz7: files-reg: create spfs file in case of parent mount is SPFS
    Instead of relying on "unreachable" image field
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -30,6 +30,7 @@
 #include "fault-injection.h"
 #include "external.h"
 #include "spfs.h"
+#include "filesystems.h"
 
 #include "protobuf.h"
 #include "util.h"
@@ -1449,6 +1450,7 @@ int open_path(struct file_desc *d,
 	struct reg_file_info *rfi;
 	char *orig_path = NULL;
 	char path[PATH_MAX];
+	struct mount_info *mi;
 
 	if (inherited_fd(d, &tmp))
 		return tmp;
@@ -1513,7 +1515,8 @@ int open_path(struct file_desc *d,
 
 	mntns_root = mntns_get_root_by_mnt_id(rfi->rfe->mnt_id);
 
-	if (rfi->rfe->unreachable) {
+	mi = lookup_mnt_id(rfi->rfe->mnt_id);
+	if (mi && mi->fstype->mount == spfs_mount) {
 		if (spfs_create_file(mntns_root, rfi) < 0) {
 			pr_err("Failed to create SPFS path\n");
 			return -1;
