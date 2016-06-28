e43e8aa716223afea0c43db939e10d95aa2904c8
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-06-28 22:24:01 +0300

    syscalls: add __NR(syscall, compat) wrapper
    Generic code uses raw syscall numbers for:
     - syscall_seized, to execute through parasite control needed syscall;
     - parasite_stop_on_syscall, to trap couple of tasks on needed syscall.
    
    As syscall numbers may differ (and differ for x86) between native and
    compatible mode, we need proper syscall wrapper here.
    
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/arch/arm/include/asm/parasite-syscall.h
+++ criu/arch/arm/include/asm/parasite-syscall.h
@@ -4,6 +4,7 @@
 
 #define ARCH_SI_TRAP TRAP_BRKPT
 
+#define __NR(syscall, compat)	__NR_##syscall
 
 void parasite_setup_regs(unsigned long new_ip, void *stack, user_regs_struct_t *regs);
 
--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -79,6 +79,7 @@
 #include "sk-queue.h"
 #include "syscall-types.h"
 #include "parasite-syscall.h"
+#include "syscall-codes.h"
 
 #include "protobuf.h"
 #include "images/sa.pb-c.h"
@@ -91,6 +92,7 @@
 #include "asm/restore.h"
 #include "asm/atomic.h"
 #include "asm/bitops.h"
+#include "asm/parasite-syscall.h"
 
 #include "cr-errno.h"
 
@@ -1823,7 +1825,7 @@ static int restore_root_task(struct pstree_item *init)
 
 	if (ret == 0)
 		ret = parasite_stop_on_syscall(task_entries->nr_threads,
-						__NR_rt_sigreturn, flag);
+			__NR(rt_sigreturn, 0), __NR(rt_sigreturn, 1), flag);
 
 	if (clear_breakpoints())
 		pr_err("Unable to flush breakpoints\n");
