2fe7b438a9d50a8efd45a16bce90d316630a46cc
Mike Rapoport <rppt@linux.vnet.ibm.com>
2016-06-29 08:55:00 +0300

    criu: page-pipe: add a wrapping structure for holes and pages iovs
    The holes and actual pages iovs bookkeeping uses the same nr_X, free_X and
    X pattern which could be wrapped by a structure. This structure will be
    later reused for addition of zero-mapped pages to pagemap.
    
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/page-pipe.h
+++ criu/include/page-pipe.h
@@ -80,18 +80,22 @@ struct page_pipe_buf {
 	struct list_head l;	/* links into page_pipe->bufs */
 };
 
+struct page_pipe_iovs {
+	unsigned int nr_iovs;	/* number of iovs */
+	unsigned int free_iov;	/* number of iovs in use */
+	struct iovec *iovs;	/* iovs */
+};
+
 struct page_pipe {
 	unsigned int nr_pipes;	/* how many page_pipe_bufs in there */
 	struct list_head bufs;	/* list of bufs */
 	struct list_head free_bufs;	/* list of bufs */
-	unsigned int nr_iovs;	/* number of iovs */
-	unsigned int free_iov;	/* first free iov */
-	struct iovec *iovs;	/* iovs. They are provided into create_page_pipe
-				   and all bufs have their iov-s in there */
 
-	unsigned int nr_holes;	/* number of holes allocated */
-	unsigned int free_hole;	/* number of holes in use */
-	struct iovec *holes;	/* holes */
+	struct page_pipe_iovs pages;	/* iovs for pages. They are provided
+					   into create_page_pipe and all bufs
+					   have their iov-s in there */
+
+	struct page_pipe_iovs holes;	/* iovs for holes */
 
 	bool chunk_mode;	/* Restrict the maximum buffer size of pipes
 				   and dump memory for a few iterations */
