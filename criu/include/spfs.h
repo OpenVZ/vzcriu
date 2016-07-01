b28aef7ec3096c34925ef2a24bb24df55d6e158f
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-01 15:10:10 +0300

    vz7: spfs: pass mount ID instead of namespace descriptor to spfs_create_file
    Precursor patch. SPFS files will be created as "link remap" in future.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/include/spfs.h
+++ criu/include/spfs.h
@@ -6,6 +6,6 @@ int spfs_set_env(void);
 struct mount_info;
 int spfs_mount(struct mount_info *mi, const char *source, const char *filesystemtype, unsigned long mountflags);
 
-int spfs_create_file(int ns_root_fd, const char *path, unsigned mode, size_t size);
+int spfs_create_file(int mnt_id, const char *path, unsigned mode, size_t size);
 
 #endif
