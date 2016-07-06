a118929da21b8547332f3d806e9708e27914bea6
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-06 17:09:19 +0300

    pcs7: spfs: add device files creation support
    https://jira.sw.ru/browse/PSBM-48985
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/include/spfs.h
+++ criu/include/spfs.h
@@ -7,7 +7,8 @@ struct mount_info;
 int spfs_mount(struct mount_info *mi, const char *source,
 	      const char *filesystemtype, unsigned long mountflags);
 
-int spfs_create_file(int mnt_id, const char *path, unsigned mode, size_t size);
+int spfs_create_file(int mnt_id, const char *path, unsigned mode, size_t size,
+		     dev_t rdev);
 int spfs_remap_path(const char *path, const char *link_remap);
 
 #endif
