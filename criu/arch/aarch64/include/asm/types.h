8dbb3347eae6f94c8e9747df58cd0d0406969914
Andrew Vagin <avagin@virtuozzo.com>
2016-07-07 08:59:00 +0300

    parasite: pass parameter regs as pointer
    CID 163751 (#1 of 1): Big parameter passed by value (PASS_BY_VALUE)
    pass_by_value: Passing parameter regs of type user_regs_struct_t (size 224 bytes) by value.
    
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Reviewed-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/arch/aarch64/include/asm/types.h
+++ criu/arch/aarch64/include/asm/types.h
@@ -54,9 +54,9 @@ typedef struct {
 typedef struct user_pt_regs user_regs_struct_t;
 
 
-#define REG_RES(regs)		((u64)(regs).regs[0])
-#define REG_IP(regs)		((u64)(regs).pc)
-#define REG_SYSCALL_NR(regs)	((u64)(regs).regs[8])
+#define REG_RES(r)		((u64)(r).regs[0])
+#define REG_IP(r)		((u64)(r).pc)
+#define REG_SYSCALL_NR(r)	((u64)(r).regs[8])
 
 #define user_regs_native(pregs)			true
 #define core_is_compat(core)			false
