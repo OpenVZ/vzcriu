00f4422dd183b30606768bb84b1513e0d0e12458
Tycho Andersen <tycho.andersen@canonical.com>
2016-07-13 15:10:56 +0000

    restore: fix race with helpers' kids dying too early
    We masked off SIGCHLD in wait_on_helpers_zombies(), but in fact this is too
    late: zombies can die any time after CR_STATE_RESTORE before this function
    is called, which lead to us getting "unexpected" deaths. Instead, we should
    mask off SIGCHLD before the helpers finish CR_STATE_RESTORE, since they're
    explicitly going to wait on all their kids to make sure they don't die
    anyway.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>
    Acked-by: Andrew Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -750,15 +750,6 @@ static unsigned long task_entries_pos;
 static int wait_on_helpers_zombies(void)
 {
 	struct pstree_item *pi;
-	sigset_t blockmask, oldmask;
-
-	sigemptyset(&blockmask);
-	sigaddset(&blockmask, SIGCHLD);
-
-	if (sigprocmask(SIG_BLOCK, &blockmask, &oldmask) == -1) {
-		pr_perror("Can not set mask of blocked signals");
-		return -1;
-	}
 
 	list_for_each_entry(pi, &current->children, sibling) {
 		pid_t pid = pi->pid.virt;
@@ -778,11 +769,6 @@ static int wait_on_helpers_zombies(void)
 			}
 			break;
 		}
-	}
-
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) == -1) {
-		pr_perror("Can not unset mask of blocked signals");
-		BUG();
 	}
 
 	return 0;
@@ -872,6 +858,16 @@ static int restore_one_task(int pid, CoreEntry *core)
 	else if (current->pid.state == TASK_DEAD)
 		ret = restore_one_zombie(core);
 	else if (current->pid.state == TASK_HELPER) {
+		sigset_t blockmask, oldmask;
+
+		sigemptyset(&blockmask);
+		sigaddset(&blockmask, SIGCHLD);
+
+		if (sigprocmask(SIG_BLOCK, &blockmask, &oldmask) == -1) {
+			pr_perror("Can not set mask of blocked signals");
+			return -1;
+		}
+
 		restore_finish_stage(CR_STATE_RESTORE);
 		if (rsti(current)->helper_cb)
 			rsti(current)->helper_cb();
