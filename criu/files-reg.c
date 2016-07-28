6550e3a5ee241d6c5a549178b2976dcb17d3a3ac
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-28 13:00:26 +0300

    vz7: files-reg: improve spfs_file helper
    It should return false in case of:
    
    1) mount namespaces are not migrated.
    3) root mount (also not migrated).
    2) external mounts (have to be provided on restore as external resource).
    
    IOW, in all of these cases SPFS shouldn't be used on restore at all, but
    generic link-remap logic instead.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -26,6 +26,7 @@
 #include "fs-magic.h"
 #include "namespaces.h"
 #include "proc_parse.h"
+#include "path.h"
 #include "pstree.h"
 #include "fault-injection.h"
 #include "external.h"
@@ -994,6 +995,29 @@ static int dump_linked_remap_type(char *path, int len, const struct stat *ost,
 			&rpe, PB_REMAP_FPATH);
 }
 
+static inline bool spfs_file(const struct fd_parms *parms, struct ns_id *nsid)
+{
+	struct mount_info *mi;
+
+	if (parms->fs_type != NFS_SUPER_MAGIC)
+		return false;
+
+	if (!(root_ns_mask & CLONE_NEWNS))
+		return false;
+
+	mi = lookup_mnt_id(parms->mnt_id);
+	if (!mi)
+		return false;
+
+	if (is_root_mount(mi))
+		return false;
+
+	if (mi->external)
+		return false;
+
+	return true;
+}
+
 static int dump_linked_remap(char *path, int len, const struct stat *ost,
 				int lfd, u32 id, struct ns_id *nsid)
 {
@@ -1113,11 +1137,6 @@ static bool is_sillyrename_name(char *name)
 			return false;
 
 	return true;
-}
-
-static inline bool spfs_file(const struct fd_parms *parms)
-{
-	return parms->fs_type == NFS_SUPER_MAGIC;
 }
 
 static inline bool nfs_silly_rename(char *rpath, const struct fd_parms *parms)
@@ -1244,7 +1263,7 @@ static int check_path_remap(struct fd_link *link, const struct fd_parms *parms,
 		return dump_ghost_remap(rpath + 1, ost, lfd, id, nsid);
 	}
 
-	if (spfs_file(parms)) {
+	if (spfs_file(parms, nsid)) {
 		pr_debug("Dump SPFS file remap for %x [%s]\n", id, rpath + 1);
 		if (dump_spfs_remap(rpath + 1, ost, lfd, id, nsid))
 			return -1;
