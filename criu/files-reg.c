d8188a79c384485087dd549bce5cd9e33e57ea30
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-08-02 18:33:07 +0300

    vz7: files-reg: move ghost_apply_metadata out of create_ghost_dentry
    The reason for this, is that truncate call drop SUID bit (always).
    Thus in case of create_spfs metadata has to be applied _after_ truncate.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -240,9 +240,6 @@ again:
 		goto err;
 	}
 
-	if (ghost_apply_metadata(path, gfe))
-		goto err;
-
 	ret = 0;
 err:
 	return ret;
@@ -263,6 +260,9 @@ static int create_ghost(struct ghost_file *gf, GhostFileEntry *gfe, struct cr_im
 
 	ret = create_ghost_dentry(path, gfe, img);
 	if (ret)
+		return -1;
+
+	if (ghost_apply_metadata(path, gfe))
 		return -1;
 
 	strcpy(gf->remap.rpath, path + root_len + 1);
@@ -525,6 +525,10 @@ static int create_spfs(int mnt_id, const char *rpath, size_t size, GhostFileEntr
 		pr_perror("failed to truncate %s", path);
 		return -1;
 	}
+
+	if (ghost_apply_metadata(path, gfe))
+		return -1;
+
 	return 0;
 }
 
