541ac97aaefd9fbc311361ee115010e1da0cf2ce
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-08-03 13:42:48 +0300

    vz7: spfs: add new helpers to replces bash scripts calls
    These helpers allow to set mode for all spfs instanced and release replace
    request for all spfs instances, which is being on hold waiting, till container
    is restored.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/include/spfs.h
+++ criu/include/spfs.h
@@ -1,11 +1,22 @@
 #ifndef __CR_SPFS_H__
 #define __CR_SPFS_H__
 
+#include <stdbool.h>
+
+#define SPFS_MODE_STUB		"stub"
+#define SPFS_MODE_PROXY		"proxy"
+
+int spfs_mngr_status(bool *active);
+int spfs_mngr_sock(void);
+
 int spfs_set_env(void);
 
 struct mount_info;
 int spfs_mount(struct mount_info *mi, const char *source,
 	      const char *filesystemtype, unsigned long mountflags);
+
+int spfs_set_mode(int sock, const char *mode);
+int spfs_release_replace(int sock);
 
 int spfs_create_file(int mnt_id, const char *path, unsigned mode, size_t size);
 int spfs_remap_path(const char *path, const char *link_remap);
