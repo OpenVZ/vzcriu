a5ddd8de98d9954370cb18535130376ee37a8fb4
Eugene Batalov <eabatalov89@gmail.com>
2016-08-07 16:11:12 +0300

    mem: make page_is_zero externally linked
    We'll use it in anon shmem dedup so we need to have access
    to it in shmem.c
    
    Signed-off-by: Eugene Batalov <eabatalov89@gmail.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/mem.h
+++ criu/include/mem.h
@@ -2,6 +2,7 @@
 #define __CR_MEM_H__
 
 #include <stdbool.h>
+#include "asm/int.h"
 
 struct parasite_ctl;
 struct vm_area_list;
@@ -9,6 +10,7 @@ struct page_pipe;
 struct pstree_item;
 
 extern int task_reset_dirty_track(int pid);
+extern bool page_is_zero(u64 pme);
 extern bool page_in_parent(bool dirty);
 extern int prepare_mm_pid(struct pstree_item *i);
 extern int do_task_reset_dirty_track(int pid);
