14733928cb093967ac3b960c0921a17ec97251be
Kirill Tkhai <ktkhai@virtuozzo.com>
2016-08-23 19:17:10 +0300

    mount: Forced mount unmounted binfmt_misc to do not lost its content
    Umount does not remove binfmt_misc content. If it's mounted once again,
    the same entries remain registered.
    
    Criu does not dump content of umounted binfmt_misc. So, after C/R we
    lose it at all.
    
    This patch forces mounting of unmounted binfmt_misc after we collected
    mountpoints. If it's unmounted, we mount it back and add this mount
    to the tree of collected mounted mountpoints. Further, binfmt_misc
    content is dumped in usual way with the only difference, that mount
    point itself is not dumped.
    
    v2: Print error in case of umount() fail.
        Move add_forced_mount() to another patch.
    v3: Close binfmt_misc dir before its umount().
    v4: Do not dump forced mounted mountpoint.
    v5: Do not search for binfmt_misc mounted: use opts.has_binfmt_misc.
        Do not count number of entries in binfmt_misc directory.
    v6: Add CRTIME_MNT_ID.
        Make mount function generic.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/mount.h
+++ criu/include/mount.h
@@ -128,5 +128,6 @@ extern int ext_mount_add(char *key, char *val);
 extern int mntns_maybe_create_roots(void);
 extern int read_mnt_ns_img(void);
 extern void cleanup_mnt_ns(void);
+extern void clean_cr_time_mounts(void);
 
 #endif /* __CR_MOUNT_H__ */
