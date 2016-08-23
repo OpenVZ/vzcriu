6f0b25adffe21366a826f380b12ceed4e492c98e
Kirill Tkhai <ktkhai@virtuozzo.com>
2016-08-23 19:16:49 +0300

    mount: Create binfmt_misc image w/o device suffix
    Use name binfmt_misc.img for any mounted binfmt_misc.
    Note, that newly created binfmt_misc images won't restore
    on old criu.
    
    Iterate over binfmt_misc_list if it's not empty on restore.
    
    Also, because of above, let's change a behaviour of dump and now
    we do not create binfmt_misc images if there is no registered
    binfmt_misc entries.
    
    v5: New
    v6: Use {open,close}_image sequence to determ if image exists
        Iterate over binfmt_misc_list
    
    Suggested-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -1373,7 +1373,7 @@ err:
 
 static int binfmt_misc_dump(struct mount_info *pm)
 {
-	struct cr_img *img;
+	struct cr_img *img = NULL;
 	struct dirent *de;
 	DIR *fdir = NULL;
 	int fd, ret;
@@ -1393,10 +1393,6 @@ static int binfmt_misc_dump(struct mount_info *pm)
 	}
 
 	ret = -1;
-	img = open_image(CR_FD_BINFMT_MISC_OLD, O_DUMP, pm->s_dev);
-	if (!img)
-		goto out;
-
 	while ((de = readdir(fdir))) {
 		if (dir_dots(de))
 			continue;
@@ -1404,6 +1400,13 @@ static int binfmt_misc_dump(struct mount_info *pm)
 			continue;
 		if (!strcmp(de->d_name, "status"))
 			continue;
+
+		if (!img) {
+			/* Create image only if an extry exists, i.e. here */
+			img = open_image(CR_FD_BINFMT_MISC, O_DUMP);
+			if (!img)
+				goto out;
+		}
 
 		if (dump_binfmt_misc_entry(fd, de->d_name, img))
 			goto out;
@@ -1532,8 +1535,24 @@ static int binfmt_misc_restore(struct mount_info *mi)
 	if (!buf)
 		return -1;
 
+	if (!list_empty(&binfmt_misc_list)) {
+		struct binfmt_misc_info *bmi;
+
+		list_for_each_entry(bmi, &binfmt_misc_list, list) {
+			ret = binfmt_misc_restore_bme(mi, bmi->bme, buf);
+			if (ret)
+				break;
+		}
+		goto free_buf;
+	}
+
 	img = open_image(CR_FD_BINFMT_MISC_OLD, O_RSTR, mi->s_dev);
 	if (!img) {
+		pr_err("Can't open binfmt_misc_old image\n");
+		goto free_buf;
+	} else if (empty_image(img)) {
+		close_image(img);
+		ret = 0;
 		goto free_buf;
 	}
 
