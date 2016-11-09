ab8f9896af1a048b2592d0e7ffb4ce38dd2e8386
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-11-09 18:52:13 +0300

    pcs7: criu: enable soccr logging
    Initialize logging function and log_level for libsoccr.
    Otherwise all soccr messages are skipped.
    
    travis-ci: success for libsoccr debug/info messages
    Reported-by: Adrian Reber <adrian@lisas.de>
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    
    Conflicts:
    	criu/crtools.c

--- criu/crtools.c
+++ criu/crtools.c
@@ -54,6 +54,8 @@
 #include "setproctitle.h"
 #include "sysctl.h"
 
+#include "../soccr/soccr.h"
+
 struct cr_options opts;
 
 void init_opts(void)
@@ -696,6 +698,8 @@ int main(int argc, char *argv[], char *envp[])
 
 	if (log_init(opts.output))
 		return 1;
+
+	libsoccr_set_log(log_level, print_on_level);
 
 	pr_debug("Version: %s (gitid %s)\n", CRIU_VERSION, CRIU_GITID);
 
