841d6f3d5885c72ba21be6a43c6eb6ca7d2b7435
Andrei Vagin <avagin@virtuozzo.com>
2016-11-21 17:07:20 -0800

    pcs7: mount: don't create a temporary directory if /tmp exists
    pivot_root requires a place where to move an old root. Currently
    a temporary directory is created for that, but it doesn't
    work if the / directory is read-only.
    
    Actually we can use any existing directory. In this patch,
    criu tries to use /tmp and only if it doesn't exist,
    criu creates a temporary directory.
    
    https://bugs.openvz.org/browse/OVZ-6778
    
    v2: don't give a constant string to mkdtemp
    
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -3019,8 +3019,11 @@ static int do_umount_one(struct mount_info *mi)
 
 static int cr_pivot_root(char *root)
 {
-	char put_root[] = "crtools-put-root.XXXXXX";
+	char tmp_dir_tmpl[] = "crtools-put-root.XXXXXX";
+	bool tmp_dir = false;
+	char *put_root = "tmp";
 	int exit_code = -1;
+	struct stat st;
 
 	pr_info("Move the root to %s\n", root ? : ".");
 
@@ -3031,9 +3034,13 @@ static int cr_pivot_root(char *root)
 		}
 	}
 
-	if (mkdtemp(put_root) == NULL) {
-		pr_perror("Can't create a temporary directory");
-		return -1;
+	if (stat(put_root, &st) || !S_ISDIR(st.st_mode)) {
+		put_root = mkdtemp(tmp_dir_tmpl);
+		if (put_root == NULL) {
+			pr_perror("Can't create a temporary directory");
+			return -1;
+		}
+		tmp_dir = true;
 	}
 
 	if (mount(put_root, put_root, NULL, MS_BIND, NULL)) {
@@ -3070,7 +3077,7 @@ err_tmpfs:
 	}
 
 err_root:
-	if (rmdir(put_root)) {
+	if (tmp_dir && rmdir(put_root)) {
 		pr_perror("Can't remove the directory %s", put_root);
 		return -1;
 	}
