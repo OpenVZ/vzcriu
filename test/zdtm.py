d4bca78abb28c207d6e0ed6a68d77dcf0daaf23d
Andrei Vagin <avagin@virtuozzo.com>
2016-12-01 11:32:39 +0300

    pcs7: zdtm: check sequence numbers for tcp sockets
    They have to be the same on each iteration.
    
    travis-ci: success for series starting with [01/21] build: install libnet-dev
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    
    Conflicts:
    	scripts/travis/travis-tests

--- test/zdtm.py
+++ test/zdtm.py
@@ -1008,6 +1008,7 @@ def cr(cr_api, test, opts):
 			try_run_hook(test, ["--pre-restore"])
 			cr_api.restore()
 			os.environ["ZDTM_TEST_PID"] = str(test.getpid())
+                        os.environ["ZDTM_IMG_DIR"] = cr_api.logs()
 			try_run_hook(test, ["--post-restore"])
 			sbs('post-restore')
 
