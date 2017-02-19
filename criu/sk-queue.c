70713d4d83ddcfef910fe49b75e64cc50e12403a
Andrei Vagin <avagin@virtuozzo.com>
2017-02-18 22:58:14 -0800

    vz7: socket: don't restore ucred from kernel messages
    ucred.pid is zero for kernel messages and in this case it is
    impossible to restore it.
    
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/sk-queue.c
+++ criu/sk-queue.c
@@ -92,7 +92,9 @@ static int dump_sk_creds(struct ucred *ucred, SkPacketEntry *pe, int flags)
 		int ret;
 
 		/* Does a process exist? */
-		if (pidns) {
+		if (ucred->pid == 0) {
+			ret = 0;
+		} else if (pidns) {
 			snprintf(path, sizeof(path), "%d", ucred->pid);
 			ret = faccessat(get_service_fd(CR_PROC_FD_OFF),
 							path, R_OK, 0);
@@ -322,7 +324,7 @@ int restore_sk_queue(int fd, unsigned int peer_id)
 			goto err;
 		}
 
-		if (entry->ucred) {
+		if (entry->ucred && entry->ucred->pid) {
 			struct ucred *ucred;
 			struct cmsghdr *ch;
 
