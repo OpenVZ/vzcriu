990364a32d63079603d1a287bc189226426241e9
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-02-23 18:16:57 +0300

    ns: Allow nested user namespaces
    Everything is prepared for nested user namespaces support.
    The only thing, we should do more, is to enter to dumped
    user namespace's parent before the dump.
    We use CLONE_VM for child tasks, so they may populate
    user_ns maps in parent memory without any tricks.
    
    v3: Check for WIFEXITED(). Fixed stack size.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    
    +++
    ns: Do not reuse PROC_SELF after CLONE_VM child
    
    Child opens PROC_SELF, populates open_proc_self_pid and exits. If parent creates
    one more child with the same pid later, the new child will try to reuse PROC_SELF,
    set by exited child. So, we need to close PROC_SELF after the first child has finished.
    
    We have this issue in two places, which have the same code. Let's move the code
    into new function call_in_child_process() and fix the issue there using close_pid_proc().
    
    https://travis-ci.org/tkhai/criu/builds/214182862
    
    v2: Introduce the helper call_in_child_process() and fix issue there.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    
    rebase: merge to "ns: Allow nested user namespaces"
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Pid & User namespaces

--- criu/include/namespaces.h
+++ criu/include/namespaces.h
@@ -47,7 +47,7 @@
 	 CLONE_NEWTIME)
 
 /* Nested namespaces are supported only for these types */
-#define CLONE_SUBNS (CLONE_NEWNS | CLONE_NEWNET)
+#define CLONE_SUBNS (CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWUSER)
 
 #define EXTRA_SIZE 20
 
