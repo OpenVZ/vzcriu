e03964dc008134bc36c6fe72e4df0c869056328e
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-03-03 11:00:38 +0300

    vz7: Zap file size checking for "atop"
    If some program has called for acct(2) and setup file
    for accounting, the kernel continue wirting into the
    file even upon container stop:
    
    do_exit
     ...
     acct_process
      slow_acct_process
       do_acct_process
        ...
        __kernel_write(file, (char *)&ac, sizeof(acct_t), &pos);
    
    For example "atop" daemon is known to doing so, and
    on restore we hit a problem
    
     | 216: Error (criu/files-reg.c:1734): File tmp/atop.d/atop.acct has bad size 123520 (expect 121984)
    
    because we wrote file size at moment of container stop
    but kernel append data on exit.
    
    Currently we can't handle it without modifying kernel
    and criu itself, thus make a workaround and check if
    the file we're dumping belong to "atop" utility
    so disable file checking on it.
    
    Note the file size cheking on itself is rather a simple
    test to make sure that FS has not been modified between
    c/r cycles, and in most situations it is not critical
    to have such test inplace.
    
    https://jira.sw.ru/browse/PSBM-61459
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    rebase to 3.15 mainstream:
    move zap check to validation callbacks
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -57,6 +57,8 @@
 #include "files-reg.h"
 #include "plugin.h"
 
+#define ATOP_ACCT_FILE "tmp/atop.d/atop.acct"
+
 int setfsuid(uid_t fsuid);
 int setfsgid(gid_t fsuid);
 
@@ -1894,6 +1896,19 @@ static bool store_validation_data(RegFileEntry *rfe,
 	rfe->has_size = true;
 	rfe->size = p->stat.st_size;
 
+	/*
+	 * FIXME: A temp solution until we can
+	 * detect tasks which has called acct()
+	 * where file get filled after container
+	 * stop. In particular atop utility setups
+	 * statistics file into a known place.
+	 */
+	if (!strcmp(rfe->name, "/"ATOP_ACCT_FILE)) {
+		pr_warn("Zap size check for %s\n", &rfe->name[1]);
+		rfe->has_size = false;
+		rfe->size = 0;
+	}
+
 	if (opts.file_validation_method == FILE_VALIDATION_BUILD_ID)
 		result = store_validation_data_build_id(rfe, lfd, p);
 
@@ -2314,7 +2329,10 @@ static bool validate_file(const int fd, const struct stat *fd_status,
 	if (rfi->rfe->has_size && (fd_status->st_size != rfi->rfe->size)) {
 		pr_err("File %s has bad size %"PRIu64" (expect %"PRIu64")\n",
 				rfi->path, fd_status->st_size, rfi->rfe->size);
-		return false;
+		if (strcmp(rfi->path, ATOP_ACCT_FILE))
+			return false;
+		else
+			pr_warn("Skip size test on %s\n", ATOP_ACCT_FILE);
 	}
 
 	if (opts.file_validation_method == FILE_VALIDATION_BUILD_ID)
