6c9efb9dde9601fa7c15c12118caadd7a4f4b025
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-03-29 21:00:58 +0300

    vz7: page-xfer -- Handle partial splicing
    In case if pipe buffers are full the splicing
    may wakeup reader first with incomplete data
    transfer. Thus splice data in cycle.
    
    https://jira.sw.ru/browse/PSBM-63161
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/page-xfer.c
+++ criu/page-xfer.c
@@ -95,11 +95,19 @@ static int write_pagemap_to_server(struct page_xfer *xfer,
 static int write_pages_to_server(struct page_xfer *xfer,
 		int p, unsigned long len)
 {
+	ssize_t ret, left = len;
+
 	pr_debug("Splicing %lu bytes / %lu pages into socket\n", len, len / PAGE_SIZE);
 
-	if (splice(p, NULL, xfer->sk, NULL, len, SPLICE_F_MOVE) != len) {
-		pr_perror("Can't write pages to socket");
-		return -1;
+	while (left > 0) {
+		ret = splice(p, NULL, xfer->sk, NULL, left, SPLICE_F_MOVE);
+		if (ret < 0) {
+			pr_perror("Can't write pages to socket");
+			return -1;
+		}
+
+		pr_debug("\tSpliced: %lu bytes sent\n", (unsigned long)ret);
+		left -= ret;
 	}
 
 	return 0;
