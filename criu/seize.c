4695baf68d1a18001788d6203870bc082b4562c7
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-04-07 13:26:22 +0300

    vz7: seize -- Wait for seize in thawed state
    It is been discovered that on early startup stage
    the processes inside container may use ptrace facility
    for own needs. So we can't checkpoint them until tracing
    is finished
    
     | root      126754  0.0  0.0  33256  2548 ?        Ds   20:31   0:00 init
     | root      126953  0.0  0.0  26148  1412 ?        Ds   20:31   0:00  \_ rsyslogd
     | ...
     | (180.080738 /proc/126953/status:
     | Name:	rsyslogd
     | State:	D (disk sleep)
     | Tgid:	126953
     | Ngid:	0
     | Pid:	126953
     | PPid:	126754
     | TracerPid:	126754
    
    Thus lets do a trick -- if such configuration met thaw the
    container's freezer give it some time a try again in a cycle.
    To eliminate waiting forever timeout considered.
    
    https://jira.sw.ru/browse/PSBM-63841
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    rebase to 3.15 mainstream:
    switch to freezer_state enum and freezer_write_state
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/seize.c
+++ criu/seize.c
@@ -274,6 +274,34 @@ static FILE *freezer_open_thread_list(char *root_path)
 static int processes_to_wait;
 static pid_t *processes_to_wait_pids;
 
+static bool is_traced(pid_t pid)
+{
+	char path[PATH_MAX];
+	FILE *f;
+
+	snprintf(path, sizeof(path), "/proc/%d/status", pid);
+	f = fopen(path, "r");
+	if (!f) {
+		pr_perror("Unable to open %s", path);
+		return false;
+	}
+
+	while (fgets(path, sizeof(path), f)) {
+		pid_t tpid;
+
+		if (strncmp("TracerPid:\t", path, 11))
+			continue;
+
+		fclose(f);
+
+		tpid = atol(&path[11]);
+		pr_debug("pid %d is traced by %d\n", pid, tpid);
+		return tpid ? true : false;
+	}
+	fclose(f);
+	return false;
+}
+
 static int seize_cgroup_tree(char *root_path, enum freezer_state state)
 {
 	DIR *dir;
@@ -318,6 +346,12 @@ static int seize_cgroup_tree(char *root_path, enum freezer_state state)
 			snprintf(buf, sizeof(buf), "/proc/%d/exe", pid);
 			if (stat(buf, &st) == -1 && errno == ENOENT)
 				continue;
+
+			if (is_traced(pid)) {
+				fclose(f);
+				return -EAGAIN;
+			}
+
 			/*
 			 * fails when meets a zombie, or exiting process:
 			 * there is a small race in a kernel -- the process
@@ -588,6 +622,8 @@ static int freeze_processes(void)
 		close(fd);
 		return -1;
 	}
+
+again:
 	if (state == THAWED) {
 		freezer_thawed = true;
 
@@ -635,6 +671,15 @@ static int freeze_processes(void)
 		if (exit_code == -EAGAIN) {
 			if (alarm_timeouted())
 				goto err;
+			if (freezer_thawed) {
+				if (freezer_write_state(fd, THAWED)) {
+					exit_code = -1;
+					goto err;
+				}
+				state = THAWED;
+				nanosleep(&req, NULL);
+				goto again;
+			}
 			nanosleep(&req, NULL);
 		} else
 			break;
