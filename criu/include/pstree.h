f80462578971046b0c389d5520e27eb840a238d1
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-04-10 11:16:15 +0300

    pid: Add pid::level field and level argument for __alloc_pstree_item()
    Pid may contain several levels, so add level field to this struct.
    Currently, level is always "1".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    rebase: initially use PID_SIZE() helper
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/include/pstree.h
+++ criu/include/pstree.h
@@ -88,8 +88,8 @@ static inline bool task_alive(struct pstree_item *i)
 }
 
 extern void free_pstree(struct pstree_item *root_item);
-extern struct pstree_item *__alloc_pstree_item(bool rst);
-#define alloc_pstree_item() __alloc_pstree_item(false)
+extern struct pstree_item *__alloc_pstree_item(bool rst, int level);
+#define alloc_pstree_item() __alloc_pstree_item(false, 1)
 extern int init_pstree_helper(struct pstree_item *ret);
 
 extern struct pstree_item *lookup_create_item(pid_t pid);
