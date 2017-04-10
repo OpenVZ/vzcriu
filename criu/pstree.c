0d61ee588412e5131c739a69a4398b4151a19a20
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-04-10 11:17:40 +0300

    pid: Add top_pid_ns
    It's the most parent pid namespace, which is seen by dumpees.
    It's NS_ROOT if root_ns_mask has CLONE_NEWPID, and NS_CRIU
    otherwise.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    Feature: Pid & User namespaces

--- criu/pstree.c
+++ criu/pstree.c
@@ -600,6 +600,14 @@ static int read_pstree_ids(pid_t pid, TaskKobjIdsEntry **ids)
 	if ((*ids)->has_pid_ns_id) {
 		if (rst_add_ns_id((*ids)->pid_ns_id, pid, &pid_ns_desc))
 			return -1;
+		if (!top_pid_ns) {
+			/*
+			 * If top_pid_ns is not set, this means that here is old dump,
+			 * which does not contain ns.img. It can have only one pid_ns,
+			 * so we set it here.
+			 */
+			top_pid_ns = lookup_ns_by_id((*ids)->pid_ns_id, &pid_ns_desc);
+		}
 	}
 	if ((*ids)->has_user_ns_id) {
 		if (rst_add_ns_id((*ids)->user_ns_id, pid, &user_ns_desc))
