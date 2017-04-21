26fde00ecbca70e1926c0bc43329efba0de02952
Cyrill Gorcunov <gorcunov@openvz.org>
2017-04-21 13:19:16 +0300

    vz7: sk-unix: Don't wait on socketpair
    If master and slave are bound to same deleted
    name we should generate two unique name instead,
    otherwise with current scheme we cant resolve
    such situation. It will be reworked later.
    
    https://jira.sw.ru/browse/PSBM-65051
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- criu/sk-unix.c
+++ criu/sk-unix.c
@@ -1766,6 +1766,8 @@ static int resolve_unix_ghosts(void)
 			list_for_each_entry(t, &unix_sockets, list) {
 				if (t->flags & (USK_GHOST_NAME | USK_GHOST_WAIT))
 					continue;
+				if (t->flags & (USK_PAIR_MASTER | USK_PAIR_SLAVE))
+					continue;
 				if (t->peer != ui)
 					continue;
 				pr_debug("\t\tghost: connected to us %#x -> %#x\n",
