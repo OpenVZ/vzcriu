78d055f963f0163d8425bbdfd275316b036c7114
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-05-10 16:52:03 +0300

    vz7: dump: Show task comm early
    When error happens on file dumping stage the only
    information about the task we dumping is its PID.
    For debug purpose show task's @comm early.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/cr-dump.c
+++ criu/cr-dump.c
@@ -1131,7 +1131,7 @@ static int pre_dump_one_task(struct pstree_item *item)
 	vmas.nr = 0;
 
 	pr_info("========================================\n");
-	pr_info("Pre-dumping task (pid: %d)\n", pid);
+	pr_info("Pre-dumping task (pid: %d comm: %s)\n", pid, __task_comm_info(pid));
 	pr_info("========================================\n");
 
 	if (item->pid->state == TASK_STOPPED) {
@@ -1210,7 +1210,7 @@ static int dump_one_task(struct pstree_item *item)
 	vmas.nr = 0;
 
 	pr_info("========================================\n");
-	pr_info("Dumping task (pid: %d)\n", pid);
+	pr_info("Dumping task (pid: %d comm: %s)\n", pid, __task_comm_info(pid));
 	pr_info("========================================\n");
 
 	if (item->pid->state == TASK_DEAD)
@@ -1665,7 +1665,7 @@ int cr_dump_tasks(pid_t pid)
 	int ret = -1;
 
 	pr_info("========================================\n");
-	pr_info("Dumping processes (pid: %d)\n", pid);
+	pr_info("Dumping processes (pid: %d comm: %s)\n", pid, __task_comm_info(pid));
 	pr_info("========================================\n");
 
 	root_item = alloc_pstree_item();
