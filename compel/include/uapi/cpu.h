cb41a7570c5f4b5f9beb4291b4b3ce6ba820dbfd
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-05-10 13:27:39 +0300

    vz7: compel -- Add support for vz7 specific cpuid masking
    In vz7 we support that named cpuid masking via
    procfs "/proc/vz/cpuid_override" interface. Thus
    any attempt to do cpuid() instruciton inside container
    cause kernel fault which provides results requested
    from the entry above.
    
    This works well inside container but we do checkpoint
    via criu on ve0 where masking is not happening. Thus
    on restore we may hit a situation where cpu features
    saved in CRIU image is not supported inside container.
    
    There was a conversation how to make it more convenient:
    
     - modify kernel to cause cpuid fault even on node
     - modify criu code so it could take cpu feature options
       from the command line
     - run criu inside container when checkpointing solely for
       cpu feature testing, then jump out and proceed a natural
       suspend
    
    https://jira.sw.ru/browse/PSBM-47748
    https://jira.sw.ru/browse/PSBM-49423
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- compel/include/uapi/cpu.h
+++ compel/include/uapi/cpu.h
@@ -8,4 +8,6 @@
 extern int compel_cpuid(compel_cpuinfo_t *info);
 extern bool compel_cpu_has_feature(unsigned int feature);
 
+extern int vz_cpu_parse_cpuid_override(void);
+
 #endif /* UAPI_COMPEL_CPU_H__ */
