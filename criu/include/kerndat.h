374ca948dab437789047654faef7281529e6f991
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-05-17 15:19:38 +0300

    ns: Add ns_get_parent() feature
    Check for NS_GET_PARENT nsfs ioctl().
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/include/kerndat.h
+++ criu/include/kerndat.h
@@ -80,6 +80,7 @@ struct kerndat_s {
 	bool task_ct_fields_supported;
 	bool has_nspid;
 	bool has_ns_get_userns;
+	bool has_ns_get_parent;
 };
 
 extern struct kerndat_s kdat;
@@ -106,5 +107,6 @@ extern int kerndat_nl_repair();
 extern bool is_zdtm_run(void);
 extern int kerndat_has_nspid(void);
 extern int kerndat_has_ns_get_userns(void);
+extern int kerndat_has_ns_get_parent(void);
 
 #endif /* __CR_KERNDAT_H__ */
