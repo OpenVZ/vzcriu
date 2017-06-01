a5f89d699f99a5295f777ef841935449d5a1e975
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-06-01 12:36:22 +0300

    vz7: sfd -- Don't forget to skip reserved fds on clone
    https://jira.sw.ru/browse/PSBM-66874
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/servicefd.c
+++ criu/servicefd.c
@@ -171,6 +171,12 @@ int clone_service_fd(int id)
 		ret = fcntl(old, F_DUPFD, new);
 		if (ret < 0 || ret != new) {
 			if (ret < 0) {
+				/*
+				 * The source fd might be reserved
+				 * and not yet created.
+				 */
+				if (errno == EBADF)
+					continue;
 				pr_perror("clone %d/%d -> %d/%d (%s) transition failed",
 					  service_fd_id, old, id, new, type_str[i]);
 			} else {
