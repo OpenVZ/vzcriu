971d101fbad364cc9b5258607271242c7f723374
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2017-06-28 16:06:26 +0300

    pstree: add id intersection bug_on to prepare_pstree_for_shell_job
    What if we replace ids in pstree to some new id, which we already
    have in tree, that can leed to unexpected behaviour.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/pstree.c
+++ criu/pstree.c
@@ -486,6 +486,7 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 		old_sid, current_sid);
 
 	for_each_pstree_item(pi) {
+		BUG_ON(vsid(pi) == current_sid);
 		if (vsid(pi) == old_sid)
 			vsid(pi) = current_sid;
 	}
@@ -499,6 +500,7 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 			old_gid, current_gid);
 
 		for_each_pstree_item(pi) {
+			BUG_ON(vpgid(pi) == current_gid);
 			if (vpgid(pi) == old_gid)
 				vpgid(pi) = current_gid;
 		}
