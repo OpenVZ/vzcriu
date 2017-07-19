8ad43d520e66f12cb3fa49b8b426aba4d8429722
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-07-19 15:53:53 +0300

    vz7: mount: allow any types of NFS mount for a while
    Current check for unsupported mounts is obviously broken. So, skip it for a
    while.
    
    https://jira.sw.ru/browse/PSBM-68663
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -721,6 +721,8 @@ static bool unsupported_nfs_bindmounts(const struct mount_info *m)
 
 static bool unsupported_nfs_mount(const struct mount_info *m)
 {
+	return false;
+
 	switch (m->nsid->type) {
 		case NS_ROOT:
 			if (m->flags & MS_SHARED)
