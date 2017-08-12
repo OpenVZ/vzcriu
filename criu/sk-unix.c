da23b661c97f198b4ba5dd496c2b7a28d9662bb3
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-08-12 17:47:00 +0300

    sk-unix: Save mnt_id for bindmounted entries
    Mount points might be beindmount to some resources (say unix binded
    sockets) thus when times come to do real bind mount call we need
    to prepare appropriate resource first.
    
    On dump procedure we walk over all bind-mounts and check if
    the mountpoint is a unix socket saving the mnt_id into the image
    then.
    
    Note at moment we support only DGRAM closed sockets.
    
    https://jira.sw.ru/browse/PSBM-69473
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/sk-unix.c
+++ criu/sk-unix.c
@@ -89,6 +89,10 @@ struct unix_sk_desc {
 	struct list_head	peer_node;
 
 	UnixSkEntry		*ue;
+
+	/* To handle bindmounts over socks */
+	struct unix_diag_vfs	uv;		/* FIXME merge with rel_name_desc_t */
+	int			mnt_id;
 };
 
 static LIST_HEAD(unix_sockets);
@@ -374,6 +378,20 @@ static int dump_one_unix_fd(int lfd, u32 id, const struct fd_parms *p)
 		ue->deleted	= sk->deleted;
 	}
 
+	/*
+	 * @mnt_id is a sign for bindmounted socket,
+	 * it must be predefined type/state and not
+	 * deleted.
+	 */
+	if (sk->mnt_id) {
+		ue->has_mnt_id	= true;
+		ue->mnt_id	= sk->mnt_id;
+		if (sk->deleted) {
+			pr_err("Unix socket %#x is deleted, unsupported\n", ue->ino);
+			goto err;
+		}
+	}
+
 	sk_encode_shutdown(ue, sk->shutdown);
 
 	if (ue->peer) {
@@ -564,6 +582,8 @@ static int unix_process_name(struct unix_sk_desc *d, const struct unix_diag_msg 
 		}
 
 		uv = RTA_DATA(tb[UNIX_DIAG_VFS]);
+		memcpy(&d->uv, uv, sizeof(d->uv));
+
 		if (name[0] != '/') {
 			/*
 			 * Relative names are be resolved later at first
@@ -823,6 +843,73 @@ int fix_external_unix_sockets(void)
 	return 0;
 err:
 	return -1;
+}
+
+int collect_unix_bindmounts(void)
+{
+	int ns_old = -1;
+	struct mount_info *mi;
+	struct stat st = {};
+	int ret = 0;
+
+	pr_debug("Collecting unix bindmounts\n");
+
+	for (mi = mntinfo; mi; mi = mi->next) {
+		if (list_empty(&mi->mnt_bind))
+			continue;
+
+		if (switch_ns(mi->nsid->ns_pid, &mnt_ns_desc, &ns_old) < 0) {
+			pr_err("Can't switch ns to mnt_id %d", mi->mnt_id);
+			if (restore_ns(ns_old, &mnt_ns_desc)) {
+				pr_err("Can't switch mount ns back from mnt_id %d\n", mi->mnt_id);
+				return -1;
+			}
+			return -1;
+		}
+
+		if (stat(mi->mountpoint, &st)) {
+			pr_warn("Can't stat on %s: %m\n", mi->mountpoint);
+			if (restore_ns(ns_old, &mnt_ns_desc)) {
+				pr_err("Can't switch mount ns back from mnt_id %d\n", mi->mnt_id);
+				return -1;
+			}
+			continue;
+		}
+
+		if (S_ISSOCK(st.st_mode)) {
+			struct unix_sk_desc *sk;
+
+			list_for_each_entry(sk, &unix_sockets, list) {
+				if (sk->uv.udiag_vfs_ino == (int)st.st_ino &&
+				    sk->uv.udiag_vfs_dev == (int)st.st_dev) {
+					pr_debug("Found sock s_dev %#x ino %#x bindmounted mnt_id %d %s\n",
+						 (int)st.st_dev, (int)st.st_ino, mi->mnt_id, mi->mountpoint);
+					if (sk->mnt_id) {
+						pr_err("Many bindings for sockets are not yet supported %#x at %s\n",
+						       (int)st.st_ino, mi->mountpoint);
+						ret = -1;
+					} else
+						sk->mnt_id = mi->mnt_id;
+					if (sk->type != SOCK_DGRAM && sk->state != TCP_CLOSE) {
+						pr_err("Unsupported bindmounted socket ino %#x at %s\n",
+						       (int)st.st_ino, mi->mountpoint);
+						ret = -1;
+					}
+					break;
+				}
+			}
+		}
+
+		if (restore_ns(ns_old, &mnt_ns_desc)) {
+			pr_err("Can't switch mount ns back from %d\n", mi->nsid->ns_pid);
+			return -1;
+		}
+
+		if (ret)
+			break;
+	}
+
+	return ret;
 }
 
 struct unix_sk_info {
