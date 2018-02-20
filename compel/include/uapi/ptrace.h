7d1686b7ed7a4a78e0b53de161e75b8a9ca71d14
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2018-02-21 01:20:04 +0300

    compel: seccomp -- Add PTRACE_SECCOMP_GET_METADATA data
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- compel/include/uapi/ptrace.h
+++ compel/include/uapi/ptrace.h
@@ -49,6 +49,16 @@
 #define PTRACE_SECCOMP_GET_FILTER	0x420c
 #endif
 
+#ifndef PTRACE_SECCOMP_GET_METADATA
+# define PTRACE_SECCOMP_GET_METADATA	0x420d
+
+struct seccomp_metadata {
+	unsigned long	filter_off;	/* Input: which filter */
+	unsigned int	flags;		/* Output: filter's flags */
+};
+
+#endif /* PTRACE_SECCOMP_GET_METADATA */
+
 #ifdef PTRACE_EVENT_STOP
 # if PTRACE_EVENT_STOP == 7 /* Bad value from Linux 3.1-3.3, fixed in 3.4 */
 #  undef PTRACE_EVENT_STOP
