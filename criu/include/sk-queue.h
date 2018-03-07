f67cd0e63f2bfbf44bd05f20e122a04ed02b711e
Andrei Vagin <avagin@virtuozzo.com>
2018-03-07 18:41:02 +0300

    vz7: netlink: dump and restore ucred-s for netlink messages
    ucred in a netlink message contains the same pid and
    it doesn't metter from which pidns it is read.
    
    https://jira.sw.ru/browse/PSBM-59339
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    Feature: Ucreds

--- criu/include/sk-queue.h
+++ criu/include/sk-queue.h
@@ -3,6 +3,7 @@
 
 extern struct collect_image_info sk_queues_cinfo;
 
+#define SK_QUEUE_REAL_PID      0x1 /* scm creds contains a real pid */
 #define SK_QUEUE_DUMP_ADDR 0x2 /* save a sender address for messages */
 #define SK_QUEUE_TRACK_ENOBUFS 0x4 /* don't fail at ENOBUFS, fill *val if it is present */
 extern int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val);
