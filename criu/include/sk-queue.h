9a0cb6e3e9a16d8261c3c171254eff7068b990f4
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2018-03-07 20:47:27 +0300

    vz7: sk-queue -- Add support of ucreds in unix sockets
    Checkpoint and restore ucreds on unix sockets are not
    that trivial: the @pid returned by the kernel upon
    message fetching is mapped to current user namespace
    so it is not the same as it would be received inside
    container. So we need to map real pid returned into
    virtual seen from inside, but we know virtual pids
    only when complete process tree is built.
    
    Thus what we do -- queue such packets into own
    queue and when process tree is build walk over
    and transform former pids into virtual ones.
    
    https://jira.sw.ru/browse/PSBM-61095
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    Feature: Ucreds

--- criu/include/sk-queue.h
+++ criu/include/sk-queue.h
@@ -7,6 +7,7 @@ extern struct collect_image_info sk_queues_cinfo;
 #define SK_QUEUE_DUMP_ADDR 0x2 /* save a sender address for messages */
 #define SK_QUEUE_TRACK_ENOBUFS 0x4 /* don't fail at ENOBUFS, fill *val if it is present */
 extern int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val);
+extern int sk_queue_post_actions(void);
 extern int restore_sk_queue(int fd, unsigned int peer_id);
 
 #endif /* __CR_SK_QUEUE_H__ */
