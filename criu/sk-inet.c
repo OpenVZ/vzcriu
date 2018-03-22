06c743ffde7e74c15b01a6a2b1c064dfe09728ee
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2018-03-22 12:43:18 +0300

    Revert "vz7: sk-inet: Add initial support for raw sockets"
    This reverts commit 35e41d8fa9fa444eb89dc4f23ac8cdca83320d59.

--- criu/sk-inet.c
+++ criu/sk-inet.c
@@ -102,7 +102,7 @@ static void show_one_inet_img(const char *act, const InetSkEntry *e)
 		e->state, src_addr);
 }
 
-static int can_dump_ipproto(int ino, int proto, int type)
+static int can_dump_ipproto(int ino, int proto)
 {
 	/* Make sure it's a proto we support */
 	switch (proto) {
@@ -112,12 +112,8 @@ static int can_dump_ipproto(int ino, int proto, int type)
 	case IPPROTO_UDPLITE:
 		break;
 	default:
-		/* Raw sockets may have any protocol inside */
-		if (type != SOCK_RAW) {
-			pr_err("Unsupported proto %d (type %d) for socket %x\n",
-			       proto, type, ino);
-			return 0;
-		}
+		pr_err("Unsupported proto %d for socket %x\n", proto, ino);
+		return 0;
 	}
 
 	return 1;
@@ -146,9 +142,9 @@ static int can_dump_inet_sk(const struct inet_sk_desc *sk)
 		return 1;
 	}
 
-	if (sk->type != SOCK_STREAM && sk->type != SOCK_RAW) {
+	if (sk->type != SOCK_STREAM) {
 		pr_err("Can't dump %d inet socket %x. "
-				"Only stream, dgram and raw are supported.\n",
+				"Only stream and dgram are supported.\n",
 				sk->type, sk->sd.ino);
 		return 0;
 	}
@@ -299,24 +295,12 @@ err:
 	return NULL;
 }
 
-
-static int dump_ip_opts(int family, int type, int sk, IpOptsEntry *ioe)
+static int dump_ip_opts(int sk, IpOptsEntry *ioe)
 {
 	int ret = 0;
 
-	if (type == SOCK_RAW) {
-		if (family == AF_INET6) {
-			ret |= dump_opt(sk, SOL_IPV6, IPV6_HDRINCL, &ioe->hdrincl);
-		} else {
-			ret |= dump_opt(sk, SOL_IP, IP_HDRINCL, &ioe->hdrincl);
-			ret |= dump_opt(sk, SOL_IP, IP_NODEFRAG, &ioe->nodefrag);
-			ioe->has_nodefrag = ioe->nodefrag;
-		}
-		ioe->has_hdrincl = ioe->hdrincl;
-	} else {
-		ret |= dump_opt(sk, SOL_IP, IP_FREEBIND, &ioe->freebind);
-		ioe->has_freebind = ioe->freebind;
-	}
+	ret |= dump_opt(sk, SOL_IP, IP_FREEBIND, &ioe->freebind);
+	ioe->has_freebind = ioe->freebind;
 
 	return ret;
 }
@@ -347,24 +331,17 @@ static int do_dump_one_inet_fd(int lfd, u32 id, const struct fd_parms *p, int fa
 	InetSkEntry ie = INET_SK_ENTRY__INIT;
 	IpOptsEntry ipopts = IP_OPTS_ENTRY__INIT;
 	SkOptsEntry skopts = SK_OPTS_ENTRY__INIT;
-	int ret = -1, err = -1, proto, aux, type;
+	int ret = -1, err = -1, proto, aux;
 
 	ret = do_dump_opt(lfd, SOL_SOCKET, SO_PROTOCOL,
 					&proto, sizeof(proto));
 	if (ret)
 		goto err;
-	ret = do_dump_opt(lfd, SOL_SOCKET, SO_TYPE,
-			  &type, sizeof(type));
-	if (ret)
+
+	if (!can_dump_ipproto(p->stat.st_ino, proto))
 		goto err;
 
-	if (!can_dump_ipproto(p->stat.st_ino, proto, type))
-		goto err;
-
-	sk = (struct inet_sk_desc *)lookup_socket(p->stat.st_ino, family,
-						  type == SOCK_RAW ?
-						  IPPROTO_RAW :
-						  proto);
+	sk = (struct inet_sk_desc *)lookup_socket(p->stat.st_ino, family, proto);
 	if (IS_ERR(sk))
 		goto err;
 	if (!sk) {
@@ -459,7 +436,7 @@ static int do_dump_one_inet_fd(int lfd, u32 id, const struct fd_parms *p, int fa
 	memcpy(ie.src_addr, sk->src_addr, pb_repeated_size(&ie, src_addr));
 	memcpy(ie.dst_addr, sk->dst_addr, pb_repeated_size(&ie, dst_addr));
 
-	if (dump_ip_opts(family, sk->type, lfd, &ipopts))
+	if (dump_ip_opts(lfd, &ipopts))
 		goto err;
 
 	if (dump_socket_opts(lfd, &skopts))
@@ -473,7 +450,7 @@ static int do_dump_one_inet_fd(int lfd, u32 id, const struct fd_parms *p, int fa
 
 	switch (proto) {
 	case IPPROTO_TCP:
-		err = (sk->type != SOCK_RAW) ? dump_one_tcp(lfd, sk) : 0;
+		err = dump_one_tcp(lfd, sk);
 		break;
 	case IPPROTO_UDP:
 	case IPPROTO_UDPLITE:
@@ -662,18 +639,12 @@ static int post_open_inet_sk(struct file_desc *d, int sk)
 	return 0;
 }
 
-int restore_ip_opts(int family, int sk, IpOptsEntry *ioe)
+int restore_ip_opts(int sk, IpOptsEntry *ioe)
 {
 	int ret = 0;
 
 	if (ioe->has_freebind)
 		ret |= restore_opt(sk, SOL_IP, IP_FREEBIND, &ioe->freebind);
-	if (ioe->has_nodefrag)
-		ret |= restore_opt(sk, SOL_IP, IP_NODEFRAG, &ioe->nodefrag);
-	if (ioe->has_hdrincl)
-		ret |= restore_opt(sk, family == AF_INET6 ? SOL_IPV6 : SOL_IP,
-				   family == AF_INET6 ? IPV6_HDRINCL : IP_HDRINCL,
-				   &ioe->hdrincl);
 
 	return ret;
 }
@@ -697,7 +668,7 @@ static int open_inet_sk(struct file_desc *d, int *new_fd)
 		return -1;
 	}
 
-	if ((ie->type != SOCK_STREAM) && (ie->type != SOCK_DGRAM) && (ie->type != SOCK_RAW)) {
+	if ((ie->type != SOCK_STREAM) && (ie->type != SOCK_DGRAM)) {
 		pr_err("Unsupported socket type: %d\n", ie->type);
 		return -1;
 	}
@@ -777,7 +748,7 @@ done:
 	if (rst_file_params(sk, ie->fown, ie->flags))
 		goto err;
 
-	if (ie->ip_opts && restore_ip_opts(ie->family, sk, ie->ip_opts))
+	if (ie->ip_opts && restore_ip_opts(sk, ie->ip_opts))
 		goto err;
 
 	if (restore_socket_opts(sk, ie->opts))
