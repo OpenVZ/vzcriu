fa8bbe6d478cd537209f7d0b289e1d9d584ea67a
Cyrill Gorcunov <gorcunov@gmail.com>
2018-06-09 16:26:06 +0300

    unix: Collect bindmounted unix sockets
    Mount points might be beindmount to some resources (say unix binded
    sockets) thus when times come to do real bind mount call we need
    to prepare appropriate resource first.
    
    On dump procedure we walk over all bind-mounts and check if
    the mountpoint is a unix socket saving the mnt_id into
    the image then. To distinguish such sockets from others
    we use UNIX_UFLAGS__BINDMOUNT flag.
    
    Note at moment we support only DGRAM closed sockets.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    
    +++
    sk-unix: fix device check in bindmount resolving
    
    See new_decode_dev in kernel, sys_stat gives us device number in non
    kernel format. And netlink UNIX_DIAG_VFS gives us kernel format. So
    we need to convert it for comparison.
    
    https://jira.sw.ru/browse/PSBM-96943
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    rebase to 3.15 mainstream:
    
    1) split device check fixup from rework of phys_stat_dev_match and merge
    
    2) simplify collect_unix_bindmounts by using mntns_get_root_fd:
    
    Instead of entering each mount namespace with setns for each mount we
    can just stat file via /proc/ns_pid/root using mntns_get_root_fd helper.
    
    This also fixes the problem in cgroup_yard test. Old code with all the
    setns'es changed cwd, but --cgroup-yard option relative path requires
    cwd to be same as working dir. New way does not change cwd.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/sk-unix.c
+++ criu/sk-unix.c
@@ -75,6 +75,9 @@ struct unix_sk_desc {
 	unsigned char		shutdown;
 	bool			deleted;
 
+	bool			bindmount;
+	unsigned int		mnt_id;
+
 	mode_t			mode;
 	uid_t			uid;
 	gid_t			gid;
@@ -393,6 +396,9 @@ static int dump_one_unix_fd(int lfd, uint32_t id, const struct fd_parms *p)
 	if (unix_resolve_name(lfd, id, sk, ue, p))
 		goto err;
 
+	if (sk->bindmount)
+		ue->uflags |= UNIX_UFLAGS__BINDMOUNT;
+
 	/*
 	 * Check if this socket is connected to criu service.
 	 * Dump it like closed one and mark it for restore.
@@ -581,11 +587,16 @@ static int unix_resolve_name(int lfd, uint32_t id, struct unix_sk_desc *d,
 	if (d->namelen == 0 || name[0] == '\0')
 		return 0;
 
-	if (kdat.sk_unix_file && (root_ns_mask & CLONE_NEWNS)) {
-		if (get_mnt_id(lfd, &mnt_id))
-			return -1;
-		ue->mnt_id = mnt_id;
-		ue->has_mnt_id = mnt_id;
+	if (!d->bindmount) {
+		if (kdat.sk_unix_file && (root_ns_mask & CLONE_NEWNS)) {
+			if (get_mnt_id(lfd, &mnt_id))
+				return -1;
+			ue->mnt_id = mnt_id;
+			ue->has_mnt_id = mnt_id;
+		}
+	} else {
+		ue->mnt_id = d->mnt_id;
+		ue->has_mnt_id = true;
 	}
 
 	if (ue->mnt_id >= 0)
@@ -717,6 +728,7 @@ static int unix_collect_one(const struct unix_diag_msg *m,
 	INIT_LIST_HEAD(&d->peer_list);
 	INIT_LIST_HEAD(&d->peer_node);
 	d->fd = -1;
+	d->mnt_id = -1;
 
 	if (tb[UNIX_DIAG_SHUTDOWN])
 		d->shutdown = nla_get_u8(tb[UNIX_DIAG_SHUTDOWN]);
@@ -910,6 +922,67 @@ int fix_external_unix_sockets(void)
 	return 0;
 err:
 	return -1;
+}
+
+int collect_unix_bindmounts(void)
+{
+	struct mount_info *mi;
+	struct stat st = {};
+	int ret = 0;
+
+	pr_debug("Collecting unix bindmounts\n");
+
+	for (mi = mntinfo; mi; mi = mi->next) {
+		struct unix_sk_desc *sk;
+		int mntns_root;
+
+		if (list_empty(&mi->mnt_bind))
+			continue;
+
+		/**
+		 * FIXME to stat overmounted mountpoints we need something like
+		 * open_mountpoint, but unix sockets can be open only with
+		 * O_PATH, also we don't reeally need to open it only stat it.
+		 */
+		if (mnt_is_overmounted(mi))
+			continue;
+
+		mntns_root = mntns_get_root_fd(mi->nsid);
+		if (mntns_root < 0)
+			return -1;
+
+		if (fstatat(mntns_root, mi->mountpoint, &st, 0)) {
+			pr_warn("Can't stat on %s: %m\n", mi->mountpoint);
+			continue;
+		}
+
+		if (!S_ISSOCK(st.st_mode))
+			continue;
+
+		list_for_each_entry(sk, &unix_sockets, list) {
+			if (sk->vfs_ino == (int)st.st_ino &&
+			    __phys_stat_dev_match(st.st_dev, sk->vfs_dev, NULL, NULL, mi)) {
+				pr_debug("Found sock s_dev %#x ino %d bindmounted mnt_id %d %s\n",
+					 (int)st.st_dev, (int)st.st_ino, mi->mnt_id, mi->mountpoint);
+				if (sk->bindmount) {
+					pr_err("Many bindings for sockets are not yet supported %d at %s\n",
+					       (int)st.st_ino, mi->mountpoint);
+					return -1;
+				} else {
+					sk->mnt_id = mi->mnt_id;
+					sk->bindmount = true;
+				}
+				if (sk->type != SOCK_DGRAM && sk->state != TCP_CLOSE) {
+					pr_err("Unsupported bindmounted socket ino %d at %s\n",
+					       (int)st.st_ino, mi->mountpoint);
+					return -1;
+				}
+				break;
+			}
+		}
+	}
+
+	return ret;
 }
 
 struct unix_sk_info {
