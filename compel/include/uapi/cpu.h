84b164f7797805fd1a7e3f12eccacff962af852e
Cyrill Gorcunov <gorcunov@gmail.com>
2018-06-14 13:14:41 +0300

    compel: cpu -- Add ability to clear features
    Will need them to mask some of the features from
    command line options.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- compel/include/uapi/cpu.h
+++ compel/include/uapi/cpu.h
@@ -7,6 +7,8 @@
 
 extern int compel_cpuid(compel_cpuinfo_t *info);
 extern bool compel_cpu_has_feature(unsigned int feature);
+extern void compel_cpu_clear_feature(unsigned int feature);
+extern void compel_cpu_copy_cpuinfo(compel_cpuinfo_t *c);
 
 extern int vz_cpu_parse_cpuid_override(void);
 
