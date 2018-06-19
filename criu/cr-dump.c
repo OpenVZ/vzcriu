851bfc20924c48df5c40a00d1e847220e8c81885
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2018-06-19 17:54:55 +0300

    memory: don't use parent memdump if detected possible pid reuse
    We have a problem when a pid is reused between consequent dumps we can't
    understand if pagemap and pages from images of parent dump are invalid
    to restore these pid already. That can lead even to wrong memory
    restored for these pid, see the test in last patch.
    
    So these is a try do separate processes with (likely) invalid previous
    memory dump from processes with 100% valid previous dump.
    
    For that we use the value of /proc/<pid>/stat's start_time and also the
    timestamp of each (pre)dump. If the start time is strictly less than the
    timestamp, that means that the pagemap for these pid from previous dump
    is valid - was done for exactly the same process.
    
    Creation time is in centiseconds by default so if predump is really fast
    (<1csec) we can have false negative decisions for some processes, but in
    case of long running processes we are fine.
    
    https://jira.sw.ru/browse/PSBM-67502
    
    v2: remove __maybe_unused for get_parent_stats; fix get_parent_stats to
    have static typing; print warning only if unsure; check has_dump_uptime
    v3: read parent stats from image only once; reuse stat from previous
    parse_pid_stat call on dump
    v4: move code to function; use unsigned long long for ticks; put
    proc_pid_stat on mem_dump_ctl; print warning on all pid-reuse cases
    v5: free parent's stats entry properly, pass it in arguments to
    (pre_)dump_one_task
    v6: free parent's stats in error path too
    v7: zero init parent_se
    v8: improve error message
    v9: switch to inventory image from stats, if pid-reuse fails - fail
    current dump
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-dump.c
+++ criu/cr-dump.c
@@ -1446,7 +1446,7 @@ err:
 	return ret;
 }
 
-static int pre_dump_one_task(struct pstree_item *item)
+static int pre_dump_one_task(struct pstree_item *item, InventoryEntry *parent_ie)
 {
 	pid_t pid = item->pid->real;
 	struct vm_area_list vmas;
@@ -1509,6 +1509,8 @@ static int pre_dump_one_task(struct pstree_item *item)
 
 	mdc.pre_dump = true;
 	mdc.lazy = false;
+	mdc.stat = NULL;
+	mdc.parent_ie = parent_ie;
 
 	ret = parasite_dump_pages_seized(item, &vmas, &mdc, parasite_ctl);
 	if (ret)
@@ -1528,7 +1530,7 @@ err_cure:
 	goto err_free;
 }
 
-static int dump_one_task(struct pstree_item *item)
+static int dump_one_task(struct pstree_item *item, InventoryEntry *parent_ie)
 {
 	pid_t pid = item->pid->real;
 	struct vm_area_list vmas;
@@ -1674,6 +1676,8 @@ static int dump_one_task(struct pstree_item *item)
 
 	mdc.pre_dump = false;
 	mdc.lazy = opts.lazy_pages;
+	mdc.stat = &pps_buf;
+	mdc.parent_ie = parent_ie;
 
 	ret = parasite_dump_pages_seized(item, &vmas, &mdc, parasite_ctl);
 	if (ret)
@@ -1870,6 +1874,7 @@ err:
 
 int cr_pre_dump_tasks(pid_t pid)
 {
+	InventoryEntry *parent_ie = NULL;
 	struct pstree_item *item;
 	int ret = -1;
 
@@ -1928,9 +1933,17 @@ int cr_pre_dump_tasks(pid_t pid)
 
 	ve_bc_unlimit(&bc_set);
 
+	/* Errors handled later in detect_pid_reuse */
+	parent_ie = get_parent_inventory();
+
 	for_each_pstree_item(item)
-		if (pre_dump_one_task(item))
+		if (pre_dump_one_task(item, parent_ie))
 			goto err;
+
+	if (parent_ie) {
+		inventory_entry__free_unpacked(parent_ie, NULL);
+		parent_ie = NULL;
+	}
 
 	ret = cr_dump_shmem();
 	if (ret)
@@ -1941,6 +1954,9 @@ int cr_pre_dump_tasks(pid_t pid)
 
 	ret = 0;
 err:
+	if (parent_ie)
+		inventory_entry__free_unpacked(parent_ie, NULL);
+
 	return cr_pre_dump_finish(ret);
 }
 
@@ -2056,6 +2072,7 @@ static int cr_dump_finish(int ret)
 int cr_dump_tasks(pid_t pid)
 {
 	InventoryEntry he = INVENTORY_ENTRY__INIT;
+	InventoryEntry *parent_ie = NULL;
 	struct pstree_item *item;
 	int pre_dump_ret = 0;
 	int ret = -1;
@@ -2153,9 +2170,17 @@ int cr_dump_tasks(pid_t pid)
 
 	ve_bc_unlimit(&bc_set);
 
+	/* Errors handled later in detect_pid_reuse */
+	parent_ie = get_parent_inventory();
+
 	for_each_pstree_item(item) {
-		if (dump_one_task(item))
+		if (dump_one_task(item, parent_ie))
 			goto err;
+	}
+
+	if (parent_ie) {
+		inventory_entry__free_unpacked(parent_ie, NULL);
+		parent_ie = NULL;
 	}
 
 	/*
@@ -2217,5 +2242,8 @@ int cr_dump_tasks(pid_t pid)
 	if (ret)
 		goto err;
 err:
+	if (parent_ie)
+		inventory_entry__free_unpacked(parent_ie, NULL);
+
 	return cr_dump_finish(ret);
 }
