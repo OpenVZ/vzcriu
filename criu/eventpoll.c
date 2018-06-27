a3e070e8324b664363172478bbc77b06a1f57f3e
Cyrill Gorcunov <gorcunov@gmail.com>
2018-06-27 12:23:19 +0300

    epoll: Drop unneeded variable from cycle
    To make clang happy
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- criu/eventpoll.c
+++ criu/eventpoll.c
@@ -159,7 +159,7 @@ static int dump_one_eventpoll(int lfd, u32 id, const struct fd_parms *p)
 		toff = xzalloc(sizeof(*toff) * e.n_tfd);
 		if (!toff)
 			goto out;
-		for (i = e.n_tfd - 1; i >= 0; i--) {
+		for (i = e.n_tfd - 1; ; i--) {
 			for (j = i + 1; j < e.n_tfd; j++) {
 				if (e.tfd[i]->tfd == e.tfd[j]->tfd) {
 					toff[i] = toff[j] + 1;
