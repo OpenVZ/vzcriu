ee58df9d93eaaa37cefd1c9d65d7a0eb1756cf88
Cyrill Gorcunov <gorcunov@gmail.com>
2018-09-13 22:57:46 +0300

    inet: raw -- Check for kernel diag module support
    To collect raw sockets we need the kernel to support raw_diag module.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/cr-check.c
+++ criu/cr-check.c
@@ -1099,6 +1099,13 @@ static int check_kcmp_epoll(void)
 	return 0;
 }
 
+static int check_net_diag_raw(void)
+{
+	check_sock_diag();
+	return (socket_test_collect_bit(AF_INET, IPPROTO_RAW) &&
+		socket_test_collect_bit(AF_INET6, IPPROTO_RAW)) ? 0 : -1;
+}
+
 static int (*chk_feature)(void);
 
 /*
@@ -1207,6 +1214,7 @@ int cr_check(void)
 		ret |= check_uffd_noncoop();
 		ret |= check_sk_netns();
 		ret |= check_kcmp_epoll();
+		ret |= check_net_diag_raw();
 	}
 
 	/*
@@ -1290,6 +1298,7 @@ static struct feature_list feature_list[] = {
 	{ "can_map_vdso", check_can_map_vdso},
 	{ "sk_ns", check_sk_netns },
 	{ "sk_unix_file", check_sk_unix_file },
+	{ "net_diag_raw", check_net_diag_raw },
 	{ "nsid", check_nsid },
 	{ "link_nsid", check_link_nsid},
 	{ "nl_repair", check_nl_repair },
