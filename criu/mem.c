4ed80aa5a447c5b78e60d3e8c43962a26092bd2a
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2018-09-28 16:11:34 +0300

    image/dedup: use userns_call to be able to open images for write
    When restoring a task in it's user namespace we had: "Unable to open
    pages-3.img: Permission denied" when trying to open pages images for
    write. (Write is needed for memory images deduplication feature to
    work.)
    
    These patch makes do_open_image open images fd through usernsd, to have
    root permissions.
    
    Also increase MAX_UNSFD_MSG_SIZE as path and metadata don't fit, and
    remove a workaround in prepare_vma_ios.
    
    https://jira.sw.ru/browse/PSBM-89056
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/mem.c
+++ criu/mem.c
@@ -1271,8 +1271,9 @@ static int prepare_vma_ios(struct pstree_item *t, struct task_restore_args *ta)
 {
 	struct cr_img *pages;
 
-	/* if auto-dedup is on we need RDWR mode to be able to punch holes
-	 * in the input files (in restorer.c)
+	/*
+	 * If auto-dedup is on we need RDWR mode to be able to punch holes in
+	 * the input files (in restorer.c)
 	 */
 	pages = open_image(CR_FD_PAGES, opts.auto_dedup ? O_RDWR : O_RSTR,
 				rsti(t)->pages_img_id);
