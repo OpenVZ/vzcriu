e0df823482b7e6bec3359b9325c8b1bf2e0ceaa4
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2018-11-02 20:27:39 +0300

    restore: Add more detailed log in wait_exiting_children
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    Feature: Debug

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1055,7 +1055,7 @@ static int wait_on_helpers_zombies(void)
 	return 0;
 }
 
-static int wait_exiting_children(void);
+static int wait_exiting_children(char *prefix);
 
 static int restore_one_zombie(CoreEntry *core)
 {
@@ -1072,7 +1072,7 @@ static int restore_one_zombie(CoreEntry *core)
 	prctl(PR_SET_NAME, (long)(void *)core->tc->comm, 0, 0, 0);
 
 	if (task_entries != NULL) {
-		wait_exiting_children();
+		wait_exiting_children("zombie");
 		zombie_prepare_signals();
 	}
 
@@ -1181,6 +1181,9 @@ static bool child_death_expected(void)
 		switch (pi->pid->state) {
 		case TASK_DEAD:
 		case TASK_HELPER:
+			pr_debug("\texpecting %s child %d to exit\n",
+				 pi->pid->state == TASK_DEAD ?
+				 "zombie" : "helper", vpid(pi));
 			return true;
 		}
 	}
@@ -1188,11 +1191,12 @@ static bool child_death_expected(void)
 	return false;
 }
 
-static int wait_exiting_children(void)
+static int wait_exiting_children(char *prefix)
 {
 	siginfo_t info;
 
 	if (!child_death_expected()) {
+		pr_debug("%s: no exiting children expected\n", prefix);
 		/*
 		 * Restoree has no children that should die, during restore,
 		 * wait for the next stage on futex.
@@ -1202,6 +1206,8 @@ static int wait_exiting_children(void)
 		restore_finish_stage(task_entries, CR_STATE_RESTORE);
 		return 0;
 	}
+
+	pr_debug("%s: gonna wait for children to exit\n", prefix);
 
 	/*
 	 * The restoree has children which will die - decrement itself from
@@ -1231,6 +1237,7 @@ static int wait_exiting_children(void)
 		return -1;
 	}
 
+	pr_debug("%s: finished waiting for children to exit\n", prefix);
 	return 0;
 }
 
@@ -1249,7 +1256,7 @@ static int restore_one_helper(void)
 	if (prepare_fds(current))
 		return -1;
 
-	if (wait_exiting_children())
+	if (wait_exiting_children("helper"))
 		return -1;
 
 	sfds_protected = false;
