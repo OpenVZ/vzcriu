2296625bd40e13dceaafdbf8f48038baf7574bd9
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2018-12-13 20:13:33 +0300

    files: Select proper master fd when collecting fd
    When choosing the master file which gonna be sending file
    descriptor to the children we must not only look into
    their PIDs but consider process tree relations, in particular
    the child of a process might be choosen as a master and
    epoll restore will fail because target files are simply
    not present in child tree.
    
    |  31964  31964  31964       epoll
    |    585  31964  31964           epoll
    |    586  31964  31964           epoll
    |...
    | (04.797121)    585: Error (criu/eventpoll.c:449): epoll: Can't add event on 0x000787: Bad file descriptor
    
    That's because the target files are blong to 31964 and not
    present in child 585, but because PID has wrapped the restore
    of epoll started from 585 instead of 31964 and we have no
    target files in the 585 because they all are inherited
    from 31964.
    
    Note the help of kcmp syscall which we started to use
    since vz7-u8 won't helper here because on checkpoint
    we choose the proper master and find 31964 carrying
    all target files as expected.
    
    https://jira.sw.ru/browse/PSBM-90293
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/files.c
+++ criu/files.c
@@ -830,10 +830,22 @@ static struct fdinfo_list_entry *alloc_fle(int pid, FdinfoEntry *fe)
 static void __collect_desc_fle(struct fdinfo_list_entry *new_le, struct file_desc *fdesc)
 {
 	struct fdinfo_list_entry *le;
+	int ret;
 
-	list_for_each_entry(le, &fdesc->fd_info_head, desc_list)
-		if (pid_rst_prio(new_le->pid, le->pid))
+	list_for_each_entry(le, &fdesc->fd_info_head, desc_list) {
+		ret = pstree_pid_cmp(new_le->pid, le->pid);
+		if (ret < 0) {
+			/*
+			 * Fall back into old algo, should not
+			 * happen though.
+			 */
+			pr_warn("Can't compare pids %d, %d (%d)\n",
+				new_le->pid, le->pid, ret);
+			if (pid_rst_prio(new_le->pid, le->pid))
+				break;
+		} else if (ret == 1)
 			break;
+	}
 	list_add_tail(&new_le->desc_list, &le->desc_list);
 }
 
