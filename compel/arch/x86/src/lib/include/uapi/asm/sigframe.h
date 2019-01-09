e823077dffdffbaec4f0e1a382d9d10a11298eb8
Jeff Law <law@redhat.com>
2019-01-09 12:49:11 +0000

    Fix gcc9 build failures
    I received this patch from Jeff Law as a fix for build failures with the
    upcoming GCC 9. The following is Jeff Law's description of the patch:
    
     Attached you'll find the fix for criu.  You'll see it's just a matter
     of dropping the sp/esp clobber from the relevant asm statements.  THe
     details:
    
     criu has a macro which defines an asm which appears to want to set a new
     stack pointer, then directly issue a sigreturn call to the kernel.  Some
     variants clobber sp (aarch64, arm, x86), others do not (ppc, s390)
    
     While the asm does indeed set a new stack pointer, we never return from
     a sigreturn syscall -- at least not in the normal way.  We actually
     return back to the point where the process was interrupted by the
     signal.  So describing the affect of the asm on the stack pointer is
     pedantically correct, it actually has no real effect and can just be
     dropped to avoid the hard error from gcc-9.
    
    Suggested-by: Jeff Law <law@redhat.com>
    Reviewed-by: Dmitry Safonov <0x7f454c46@gmail.com>
    Signed-off-by: Adrian Reber <areber@redhat.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>
    
    (cherry-picked from ms commit 901f5d48e ("Fix gcc9 build failures"))
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- compel/arch/x86/src/lib/include/uapi/asm/sigframe.h
+++ compel/arch/x86/src/lib/include/uapi/asm/sigframe.h
@@ -206,7 +206,7 @@ void rt_sigframe_erase_sigset(struct rt_sigframe *sigframe)
 		".code64					\n"	\
 		:							\
 		: "rdi"(new_sp)						\
-		: "eax","esp", "r8", "r9", "r10", "r11",		\
+		: "eax", "r8", "r9", "r10", "r11",		\
 			"r12", "r13", "r14", "r15", "memory")
 
 #define ARCH_RT_SIGRETURN(new_sp, rt_sigframe)				\
