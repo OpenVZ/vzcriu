fdc1b34b078750487f18c7ec748f05864996c65c
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2019-01-17 16:31:39 +0300

    vz7: spfs: delayed mounts facility introduced
    This facility allows to migated such mounts, like NFS, by replacing the actual
    mount by FUSE (spfs) mount.
    Spfs allows to create and open files, required for restore, and then (by
    switching spfs to "Stub" mode) to block any incomming file system requests.
    This facility does spfs mount via spfs manager, which is then instructed, how
    to mount the actual file system.
    Spfs manager is also responsible for replacing of spfs mount and opened files
    by the actual one (and actual files) once actual mount point is remounted.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    +++
    vz7: spfs: remove "ns_list" option from "replace" command as obsolete
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    +++
    vz7: spfs: explicitly pass size and mode to spfs_create_file
    
    Instead of passing reg_file_info object. Allows to remove dependence on
    "files-reg.h" header.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    +++
    vz7: criu: spfs -- Fixup various nits for NFS sake
    
    This is a nitpicking sent by Stas as a new series
    for NFS migration support
    
    https://lists.openvz.org/pipermail/criu/2016-May/028591.html
    
    Since v3 is already merged into this CRIU instace I did
    an interdiff and here are the changes to match v4.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    rebase to 3.17.1 mainstream:
    merge fixups, remove excess errno printing in pr_perror
    
    https://jira.sw.ru/browse/PSBM-144416
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

---
+++ criu/include/spfs.h
@@ -0,0 +1,11 @@
+#ifndef __CR_SPFS_H__
+#define __CR_SPFS_H__
+
+int spfs_set_env(void);
+
+struct mount_info;
+int spfs_mount(struct mount_info *mi, const char *source, const char *filesystemtype, unsigned long mountflags);
+
+int spfs_create_file(int ns_root_fd, const char *path, unsigned mode, size_t size);
+
+#endif
