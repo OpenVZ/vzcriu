99f59550f2953fc17e3d304170311119c2f1974b
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2019-01-28 14:17:50 +0300

    ve: tty -- Add support of tty inheritance, v2
    To support tty inheritance we need a kernel help first
    because we're forking before we restore files opened,
    thus every process forked comes with no tty assigned.
    
    The kernel part is addressed in
    
     | commit a8a373f98db37f95ed86afd41965af4f0991597e
     | Author: Cyrill Gorcunov <gorcunov@virtuozzo.com>
     | Date:   Mon Feb 19 19:16:13 2018 +0300
     |
     |     ve/tty: Provide interface for current tty inheritance
    
    So now criu part is addressed as:
    
     * on checkpoint:
    
       - tty_pgrp number is transformed into virtual
         pid and comes into the core image;
    
       - before the checkpoint finished we walk over
         all terminals obtained and check if the current
         terminal is belonging to some session, which
         should assure us that we will be able to restore
         it back then.
    
     * on restore:
    
       - we fetch tty_pgrp from cores and collect them
         into chain (due to shmem restorer engine we're
         to use chains instead of some fast structure,
         may need to speedup if it become too slow);
    
       - once controlling terminal is restored we
         propagate the session to all children which
         have same tty inherited.
    
    https://jira.sw.ru/browse/PSBM-76490
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    +++
    vtty: tty_verify_ctty -- Fix a typo
    
    https://jira.sw.ru/browse/PSBM-96852
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    rebase to 3.15 mainstream:
    merge fixup
    
    Note: probably we still have another problem here as in tty_verify_ctty
    we have `if (ve_can_inherit_tty(n->pid_real))` which is always true as n
    is TTY_TYPE__PTY, but currently I don't know how to make it right.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    rebase to 3.17.1 mainstream:
    merge fixup
    https://src.openvz.org/projects/OVZ/repos/criu/commits/e901a7e5e
    https://src.openvz.org/projects/OVZ/repos/criu/commits/37c44253a
    
    Feature: TTY
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1998,6 +1998,8 @@ static int restore_task_with_children(void *_arg)
 		 */
 		if (restore_wait_other_tasks())
 			goto err;
+		if (ve_itty_resolve())
+			goto err;
 		fini_restore_mntns();
 		__restore_switch_stage(CR_STATE_RESTORE);
 	} else {
@@ -2771,6 +2773,9 @@ int cr_restore_tasks(void)
 		goto err;
 
 	if (inherit_fd_move_to_fdstore())
+		goto err;
+
+	if (ve_itty_init())
 		goto err;
 
 	if (crtools_prepare_shared() < 0)
