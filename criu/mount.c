5770d09b34c26251cd8a2ce0ee5633208e6384bb
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2019-02-07 15:17:52 +0300

    mount: check that mounts tree restored without any hidden problems
    Collect mount trees for each mount namespace after restoring. Sort both
    trees deterministically, as we don't have sibling mounts with same
    mountpoint strcmp will be enough. Compare nodes content and topology
    with step-by-step dfs for these trees, and report errors.
    
    Put it under option (--check-mounts), as it can affect performance, we
    will likely enable it only in VZ7 container migration tests, zdtm (we
    already have similar function in zdtm, but to test these feature itself
    we need to enable it) and to debug problems with mounts.
    
    https://jira.sw.ru/browse/PSBM-86686
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -4213,3 +4213,133 @@ int remount_readonly_mounts(void)
 	 */
 	return call_helper_process(ns_remount_readonly_mounts, NULL);
 }
+
+enum tree_step {
+	STEP_STOP = -2,
+	STEP_CHILD = -1,
+	STEP_SIBLING = 0,
+	STEP_PARENT = 1,
+};
+
+static struct mount_info *mnt_subtree_step(struct mount_info *mi,
+					   struct mount_info *root,
+					   enum tree_step *step)
+{
+	*step = STEP_CHILD;
+
+	if (!list_empty(&mi->children))
+		return list_entry(mi->children.next, struct mount_info, siblings);
+
+	*step = STEP_SIBLING;
+
+	while (mi->parent && mi != root) {
+		if (mi->siblings.next == &mi->parent->children) {
+			mi = mi->parent;
+			*step += STEP_PARENT;
+		} else
+			return list_entry(mi->siblings.next, struct mount_info, siblings);
+	}
+
+	*step = STEP_STOP;
+	return NULL;
+}
+
+static void mnt_resort_siblings_full(struct mount_info *tree)
+{
+	struct mount_info *m, *p;
+	LIST_HEAD(list);
+
+	pr_info("\tResorting siblings full on %d\n", tree->mnt_id);
+	while (!list_empty(&tree->children)) {
+		m = list_first_entry(&tree->children, struct mount_info, siblings);
+		list_del(&m->siblings);
+
+		list_for_each_entry(p, &list, siblings)
+			if (strcmp(p->ns_mountpoint, m->ns_mountpoint) < 0)
+				break;
+
+		list_add_tail(&m->siblings, &p->siblings);
+		mnt_resort_siblings_full(m);
+	}
+
+	list_splice(&list, &tree->children);
+}
+
+static int __check_mounts(struct ns_id *ns)
+{
+	struct ns_id _new_ns = { .ns_pid = PROC_SELF, .nd = &mnt_ns_desc }, *new_ns = &_new_ns;
+	struct mount_info *mnt, *new_mnt, *new;
+	int step, new_step;
+
+	if (do_restore_task_mnt_ns(ns))
+		return -1;
+
+	pr_debug("Checking mountinfo for mntns %d:%d\n", ns->kid, ns->id);
+	new = collect_mntinfo(new_ns, true);
+	if (new == NULL)
+		return -1;
+
+
+	mnt = ns->mnt.mntinfo_tree;
+	new_mnt = new_ns->mnt.mntinfo_tree;
+	mnt_resort_siblings_full(mnt);
+	mnt_resort_siblings_full(new_mnt);
+
+	while (mnt && new_mnt) {
+		/* Consider that leading '.' was lost in collect_mnt_from_image. */
+		if (strcmp(mnt->ns_mountpoint, new_mnt->ns_mountpoint+1) ||
+		    strcmp(mnt->root, new_mnt->root) ||
+		    mnt->flags != new_mnt->flags ||
+		    mnt->sb_flags != new_mnt->sb_flags) {
+			pr_err("Mounts %s[%s,%d,%d] and %s[%s,%d,%d] does not match\n",
+			       mnt->ns_mountpoint, mnt->root, mnt->flags, mnt->sb_flags,
+			       new_mnt->ns_mountpoint+1, new_mnt->root, new_mnt->flags, new_mnt->sb_flags);
+			goto err;
+		}
+
+		mnt = mnt_subtree_step(mnt, ns->mnt.mntinfo_tree, &step);
+		new_mnt = mnt_subtree_step(new_mnt, new_ns->mnt.mntinfo_tree, &new_step);
+
+		if (step != new_step) {
+			pr_err("The restored mount tree for mntns %d:%d has wrong topology\n",
+			       ns->kid, ns->id);
+			goto err;
+		}
+	}
+
+	return 0;
+err:
+	pr_err("Old mntns tree:\n");
+	mnt_tree_show(ns->mnt.mntinfo_tree, 0);
+	pr_err("New mntns tree:\n");
+	mnt_tree_show(new_ns->mnt.mntinfo_tree, 0);
+	return -1;
+}
+
+static int ns_check_mounts(void *arg)
+{
+	struct ns_id *nsid;
+	int *ret = arg;
+
+	for (nsid = ns_ids; nsid != NULL; nsid = nsid->next) {
+		if (nsid->nd != &mnt_ns_desc)
+			continue;
+
+		*ret = __check_mounts(nsid);
+		if (*ret)
+			return 0;
+	}
+
+	return 0;
+}
+
+int check_mounts(void)
+{
+	int ret = 0;
+
+	pr_info("Check restored mount trees\n");
+	if (call_helper_process(ns_check_mounts, &ret))
+		return -1;
+
+	return ret;
+}
