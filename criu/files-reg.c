2532aef1e58e78528f69182b1647a78717310ca0
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2019-02-14 13:24:49 +0300

    mount: temporary create needed ancestor directories for deleted root
    Else on improved (in next patch) zdtm/static/mntns_deleted we get:
    
    (00.032469)      1: mnt:        Bind /tmp/.criu.mntns.MsWZ6q/12-0000000000/zdtm/static/mntns_deleted.test/test-mid/test-src to /tmp/.criu.mntns.MsWZ6q/12-0000000000/zdtm/static/mntns_deleted.test/test-dst
    (00.032495)      1: Error (criu/mount.c:2279): mnt: Can't re-create deleted directory /tmp/.criu.mntns.MsWZ6q/12-0000000000/zdtm/static/mntns_deleted.test/test-mid/test-src: No such file or directory
    
    https://jira.sw.ru/browse/PSBM-77022
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -1757,7 +1757,7 @@ out:
 	return ret;
 }
 
-static void rm_parent_dirs(int mntns_root, char *path, int count)
+void rm_parent_dirs(int mntns_root, char *path, int count)
 {
 	char *p, *prev = NULL;
 
@@ -1784,7 +1784,7 @@ static void rm_parent_dirs(int mntns_root, char *path, int count)
 }
 
 /* Construct parent dir name and mkdir parent/grandparents if they're not exist */
-static int make_parent_dirs_if_need(int mntns_root, char *path)
+int make_parent_dirs_if_need(int mntns_root, char *path)
 {
 	char *p, *last_delim;
 	int err, count = 0;
@@ -1804,6 +1804,11 @@ static int make_parent_dirs_if_need(int mntns_root, char *path)
 	}
 
 	p = path;
+
+	/* when used for absolute paths we need to skip 1-st '/' */
+	if (p[0] == '/')
+		p++;
+
 	do {
 		p = strchr(p, '/');
 		if (p)
