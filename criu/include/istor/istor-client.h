c1ca7bc7094f9f2c160ada18aa667c1195b9cf24
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2019-02-26 18:14:36 +0300

    client
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/include/istor/istor-client.h
+++ criu/include/istor/istor-client.h
@@ -1,4 +1,9 @@
 #ifndef __CR_ISTOR_CLIENT_H__
 #define __CR_ISTOR_CLIENT_H__
 
+struct cr_options;
+
+extern int istor_client_init(struct cr_options *opts);
+extern void istor_client_fini(void);
+
 #endif /* __CR_ISTOR_CLIENT_H__ */
--- criu/istor/istor-client.c
+++ criu/istor/istor-client.c
@@ -1,1 +1,28 @@
+#include <unistd.h>
+
+#include "cr_options.h"
+#include "util.h"
+
 #include "istor/istor-client.h"
+
+static int istor_server_sk = -1;
+
+int istor_client_init(struct cr_options *opts)
+{
+	if (!opts->istor_use_server)
+		return 0;
+
+	istor_server_sk = setup_tcp_client(opts->addr, opts->port);
+	if (istor_server_sk < 0) {
+		pr_err("Can't connect to server\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+void istor_client_fini(void)
+{
+	if (istor_server_sk != -1)
+		close(istor_server_sk);
+}
