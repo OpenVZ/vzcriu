77f96af10cc09b3fcb0454be892af33dd443e693
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2019-02-27 12:07:18 +0300

    update
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/istor/istor-client.c
+++ criu/istor/istor-client.c
@@ -1,28 +1,45 @@
+#include <stdlib.h>
 #include <unistd.h>
+
+#include <uuid/uuid.h>
 
 #include "cr_options.h"
 #include "util.h"
+#include "log.h"
 
 #include "istor/istor-client.h"
 
-static int istor_server_sk = -1;
+#ifdef LOG_PREFIX
+# undef LOG_PREFIX
+#endif
+
+#define LOG_PREFIX "istor-client: "
+
+static int client_sk = -1;
+//static uuid_t oid;
 
 int istor_client_init(struct cr_options *opts)
 {
 	if (!opts->istor_use_server)
 		return 0;
 
-	istor_server_sk = setup_tcp_client(opts->addr, opts->port);
-	if (istor_server_sk < 0) {
+	pr_debug("Setting up client\n");
+	
+	client_sk = setup_tcp_client(opts->istor_server_ip, opts->istor_server_port);
+	if (client_sk < 0) {
 		pr_err("Can't connect to server\n");
 		return -1;
 	}
 
-	return 0;
+	exit(1);
+	(void)opts->imgs_dir;
+	return -1;
 }
 
 void istor_client_fini(void)
 {
-	if (istor_server_sk != -1)
-		close(istor_server_sk);
+	if (client_sk != -1) {
+		close(client_sk);
+		client_sk = -1;
+	}
 }
