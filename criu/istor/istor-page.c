911e230b90ca101a099dede2606d20921e3d7f7f
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2019-03-14 10:46:16 +0300

    update
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

--- criu/istor/istor-page.c
+++ criu/istor/istor-page.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <uuid/uuid.h>
+
+#include "cr_options.h"
+#include "image.h"
+#include "util.h"
+#include "log.h"
+
+#include "istor/istor-net.h"
 #include "istor/istor-page.h"
+
+#ifdef LOG_PREFIX
+# undef LOG_PREFIX
+#endif
+
+#define LOG_PREFIX "istor-page: "
