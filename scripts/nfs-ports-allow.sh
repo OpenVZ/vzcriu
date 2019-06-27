c5ae49be0c00f59cae217c5644bca728bf5a4304
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2019-06-27 18:02:32 +0300

    scripts/nfs: collect nfs mount servers only from ve processes
    There is a problem when one mounts nfs share on host and suspends the CT
    with no network, nfs-ports-allow.sh fails with:
    
    /usr/bin/nsenter -t 17191 -u -p -n rpcinfo -p 10.94.3.197
    rpcinfo: can't contact portmapper: RPC: Remote system error - Network is unreachable
    
    That's because our /proc/*/mountinfo way to get nfs servers for nfs
    mounts also gets servers for all other containers and host. And if those
    servers are not accessible from a CT script fails on them.
    
    So restrict mountinfo parsing to CT processes only.
    
    https://bugs.openvz.org/browse/OVZ-7111
    https://jira.sw.ru/browse/PSBM-95775
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    rebase to mainstream 3.18:
    fix make lint
    
    https://jira.sw.ru/browse/PSBM-144416
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: SPFS

--- scripts/nfs-ports-allow.sh
+++ scripts/nfs-ports-allow.sh
@@ -25,8 +25,15 @@ JOIN_CT="${NS_ENTER} -t $CRTOOLS_INIT_PID -u -p -n"
 
 ${JOIN_CT} test -e /proc/self/net/nfsfs || exit 0
 
-servers=$(${JOIN_CT} cat /proc/*/mountinfo | awk '/ - nfs/ {print $NF}' |\
-	awk -F "," '{for (i = 1; i <= NF; i++) { if ($i~"^addr="){sub(/^addr=/,"", $i); print $i}}}' | sort -u)
+[ -z "$VEID" ] && exit 1
+
+# note: pstree is frozen by criu at these point in collect_pstree()
+servers=''
+while read -r vepid; do
+	servers="$servers $(awk '/ - nfs/ {print $NF}' < "/proc/$vepid/mountinfo" |\
+		awk -F "," '{for (i = 1; i <= NF; i++) { if ($i~"^addr="){sub(/^addr=/,"", $i); print $i}}}')"
+done < "/sys/fs/cgroup/ve/$VEID/tasks"
+servers=$(echo "$servers" | tr ' ' '\n' | sort -u)
 
 [ -n "$servers" ] || exit 0
 
