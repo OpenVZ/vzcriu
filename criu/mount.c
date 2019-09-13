OpenVZ/vzcriu

commit fd48e2239fc3853da5bf1b1d33773d702ab719d7

mount: fix external mounts so that they pass check-mounts
1) Prohibit external mounts in non-root mntns. - Currently when we add
an external mount, it is identified by the path of the mountpoint on
dump irrelevant to mntns. If one creates a new mntns in CT all external
mounts are copied to these new ns, and shared mounts become shared with
corresponding copies. So if shared external mount in CT root mntns has a
shared copy in other mntns in CT (same shared_id), the copy is currently
detected as external too, and both of them are restored with external
binding. These way mounts loose share between each other.

2) Restore shared options for external mounts. - Since commit ("vz7:
mount: create a temporary mount to restore shared and master groups") we
don't restore shared options here but we should.

3) Assume manual external mounts as internal sharing. - We have a full
control over manual external mounts, we (vzctl) give them to CT, thus we
can remove external sharing from them, which only adds excessive
complexity to mount restore. Rely on it though it is not yet done, these
can only be detected by --check-mounts.

https://jira.sw.ru/browse/PSBM-96385

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -517,6 +517,9 @@ static int try_resolve_ext_mount(struct mount_info *info)
 		pr_info("Found %s mapping for %s mountpoint\n",
 				ext, info->mountpoint);
 		info->external = ext;
+		/* assume manual external mounts as internal sharing */
+		if (info->shared_id)
+			info->internal_sharing = true;
 		return 1;
 	}
 
@@ -919,6 +922,14 @@ static int resolve_external_mounts(struct mount_info *info)
 		struct mount_info *match;
 
 		if (m->parent == NULL || m->is_ns_root)
+			continue;
+
+		/*
+		 * Only allow external mounts in root mntns. External mounts
+		 * lookup is based on mountpoint path, but there may be a lot of
+		 * mounts with same mountpoint across mntnses.
+		 */
+		if (m->nsid->type != NS_ROOT)
 			continue;
 
 		ret = try_resolve_ext_mount(m);
@@ -2624,6 +2635,10 @@ do_bind:
 		}
 	}
 out:
+	if (mi->external && restore_shared_options(mi, private, mi->shared_id,
+						   mi->master_id))
+		return -1;
+
 	mi->mounted = true;
 	exit_code = 0;
 err:

commit 2287d9507905e2978926b6c0276fcd56557091a6

mount: restore shared options in clean mount for external mounts
After prohibiting to handle mounts in non-root CT mntnses as external
only one mount from these fs - "first" is restored as external and all
others are restored from the "first". In test we've had cgroup external
mount which propagated in multiple namespaces when mounting the "first"
mount, so we din't restore sharing on propagations right.

https://jira.sw.ru/browse/PSBM-97787

Fixes: fd48e2239 ("mount: fix external mounts so that they pass check-mounts")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -2496,8 +2496,29 @@ static int do_bind_mount(struct mount_info *mi)
 		 * is tuned in collect_mnt_from_image to refer
 		 * to proper location in the namespace we restore.
 		 */
-		root = mi->external;
 		private = !mi->master_id && (mi->internal_sharing || !mi->shared_id);
+
+		/*
+		 * External mount's parent can be in a shared mount group, thus
+		 * we need to restore shared options before mounting, else our
+		 * propagations may still have external sharing.
+		 *
+		 * FIXME Merge these with non-external code path.
+		 */
+		if (mount(mi->external , mnt_clean_path, NULL, MS_BIND, NULL)) {
+			pr_perror("Unable to bind-mount %s to %s",
+				  mi->external, mnt_clean_path);
+			return -1;
+		}
+		root = mnt_clean_path;
+		umount_mnt_fd = open(mnt_clean_path, O_PATH);
+		if (umount_mnt_fd < 0) {
+			pr_perror("Unable to open %s", mnt_clean_path);
+			return -1;
+		}
+		if (__restore_shared_options(root, private, mi->shared_id,
+					     mi->master_id))
+			goto err;
 		goto do_bind;
 	}
 
@@ -2635,10 +2656,6 @@ do_bind:
 		}
 	}
 out:
-	if (mi->external && restore_shared_options(mi, private, mi->shared_id,
-						   mi->master_id))
-		goto err;
-
 	mi->mounted = true;
 	exit_code = 0;
 err: