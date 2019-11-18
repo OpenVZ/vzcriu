cc7cb793621ea0c69169140e519bf0e295cc4634
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2019-11-18 17:00:28 +0300

    Revert "restore: Serialize access to last_pid"
    This reverts commit 56cf97f5174e617151a55d42b5271b217f38f78a.

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1909,7 +1909,6 @@ static int restore_task_with_children(void *_arg)
 		if (ve_itty_resolve())
 			goto err;
 		fini_restore_mntns();
-		clone_noasan_set_mutex(&task_entries->last_pid_mutex);
 		__restore_switch_stage(CR_STATE_RESTORE);
 	} else {
 		if (restore_finish_stage(task_entries, CR_STATE_FORKING) < 0)
@@ -2531,7 +2530,6 @@ skip_ns_bouncing:
 	if (!opts.restore_detach && !opts.exec_cmd)
 		wait(NULL);
 
-	clone_noasan_fini();
 	return 0;
 
 out_kill:
@@ -2562,7 +2560,6 @@ out:
 	depopulate_roots_yard(mnt_ns_fd, true);
 	stop_usernsd();
 	__restore_switch_stage(CR_STATE_FAIL);
-	clone_noasan_fini();
 	pr_err("Restoring FAILED.\n");
 	return -1;
 }
@@ -2621,9 +2618,6 @@ int cr_restore_tasks(void)
 	timing_start(TIME_RESTORE);
 
 	if (cpu_init() < 0)
-		goto err;
-
-	if (clone_noasan_init() < 0)
 		goto err;
 
 	if (vdso_init_restore())
