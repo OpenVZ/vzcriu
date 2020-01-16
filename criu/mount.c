OpenVZ/vzcriu

commit 8eec2567e027453979bcf1c650cd075725f5efa5

mount: mark mounts of external devices external
Thus such mounts will be skipped in content dump in dump_one_mountpoint
and mnt_is_external will also work for them. Fixing mnt_is_external
means that such mounts are skipped in validate_mounts right, as now such
mounts in nested mntns'es will be restored as normal non-external binds
of their external prototypes from ct-root mntns. Also the process of
binding such mounts is fixed in propagate_mount.

The only place where we used the fact that external dev mounts are not
marked as external mounts is in do_mount_one, where we chose mount/bind
based on these, so update it.

https://jira.sw.ru/browse/PSBM-97810

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -38,6 +38,7 @@
  * debugging.
  */
 #define AUTODETECTED_MOUNT "CRIU:AUTOGENERATED"
+#define EXTERNAL_DEV_MOUNT "CRIU:EXTERNAL_DEV"
 #define NO_ROOT_MOUNT "CRIU:NO_ROOT"
 #define MS_PROPAGATE (MS_SHARED | MS_PRIVATE | MS_UNBINDABLE | MS_SLAVE)
 
@@ -536,6 +537,7 @@ static int try_resolve_ext_mount(struct mount_info *info)
 
 			pr_info("Found %s dev-mapping for %s mountpoint\n",
 				val, info->mountpoint);
+			info->external = EXTERNAL_DEV_MOUNT;
 
 			len = strlen(val) + sizeof("dev[]");
 			source = xrealloc(info->source, len);
@@ -2896,7 +2898,8 @@ static int do_mount_one(struct mount_info *mi)
 			return -1;
 		mi->mounted = true;
 		ret = 0;
-	} else if (!mi->bind && !mi->need_plugin && !mi->external)
+	} else if ((!mi->bind && !mi->need_plugin && !mi->external) ||
+		   (mi->external && !strcmp(mi->external, EXTERNAL_DEV_MOUNT)))
 		ret = do_new_mount(mi);
 	else
 		ret = do_bind_mount(mi);
@@ -3276,10 +3279,11 @@ static int get_mp_root(MntEntry *me, struct mount_info *mi)
 	 * from the command line and put into root's place
 	 */
 
-	ext = ext_mount_lookup(me->ext_key);
-	if (!ext) {
+	if (!strcmp(me->ext_key, AUTODETECTED_MOUNT)) {
 		if (!opts.autodetect_ext_mounts) {
-			pr_err("No mapping for %s mountpoint\n", me->mountpoint);
+			pr_err("Mount %d:%s is autodetected external mount. "
+			       "Try \"--ext-mount-map auto\" to allow them.\n",
+			       mi->mnt_id, mi->mountpoint);
 			return -1;
 		}
 
@@ -3293,6 +3297,15 @@ static int get_mp_root(MntEntry *me, struct mount_info *mi)
 		 */
 
 		ext = mi->source;
+	} else if (!strcmp(me->ext_key, EXTERNAL_DEV_MOUNT)) {
+		ext = strdup(me->ext_key);
+	} else {
+		ext = ext_mount_lookup(me->ext_key);
+		if (!ext) {
+			pr_err("No mapping for %d:%s mountpoint\n",
+			       mi->mnt_id, mi->mountpoint);
+			return -1;
+		}
 	}
 
 	mi->external = ext;

commit 732d812fd3c8ac0d72f6b621ac94f977bb114c7b

mount: make external dev mounts resolve to only root mounts
Function do_new_mount only allows creating mounts with root "/", as it
does simple mount without MS_BIND. Restoring non-root mounts via
do_new_mount creates wrong mounts.

While on it, also even if mount is marked as EXTERNAL_DEV_MOUNT but
it has a bind, mount it through do_bind_mount.

https://jira.sw.ru/browse/PSBM-100775

Fixes: 8eec2567e027 ("mount: mark mounts of external devices external")

criu/mount.c
@@ -542,7 +542,8 @@ static int try_resolve_ext_mount(struct mount_info *info)
 	snprintf(devstr, sizeof(devstr), "dev[%d/%d]",
 			kdev_major(info->s_dev),  kdev_minor(info->s_dev));
 
-	if (info->fstype->code == FSTYPE__UNSUPPORTED) {
+	if (info->fstype->code == FSTYPE__UNSUPPORTED &&
+	    fsroot_mounted(info)) {
 		char *val;
 
 		val = external_lookup_by_key(devstr);
@@ -2913,8 +2914,8 @@ static int do_mount_one(struct mount_info *mi)
 			return -1;
 		mi->mounted = true;
 		ret = 0;
-	} else if ((!mi->bind && !mi->need_plugin && !mi->external) ||
-		   (mi->external && !strcmp(mi->external, EXTERNAL_DEV_MOUNT)))
+	} else if (!mi->bind && !mi->need_plugin && (!mi->external ||
+		   !strcmp(mi->external, EXTERNAL_DEV_MOUNT)))
 		ret = do_new_mount(mi);
 	else
 		ret = do_bind_mount(mi);