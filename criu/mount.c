OpenVZ/vzcriu

commit fd48e2239fc3853da5bf1b1d33773d702ab719d7

mount: fix external mounts so that they pass check-mounts
1) Prohibit external mounts in non-root mntns. - Currently when we add
an external mount, it is identified by the path of the mountpoint on
dump irrelevant to mntns. If one creates a new mntns in CT all external
mounts are copied to these new ns, and shared mounts become shared with
corresponding copies. So if shared external mount in CT root mntns has a
shared copy in other mntns in CT (same shared_id), the copy is currently
detected as external too, and both of them are restored with external
binding. These way mounts loose share between each other.

2) Restore shared options for external mounts. - Since commit ("vz7:
mount: create a temporary mount to restore shared and master groups") we
don't restore shared options here but we should.

3) Assume manual external mounts as internal sharing. - We have a full
control over manual external mounts, we (vzctl) give them to CT, thus we
can remove external sharing from them, which only adds excessive
complexity to mount restore. Rely on it though it is not yet done, these
can only be detected by --check-mounts.

https://jira.sw.ru/browse/PSBM-96385

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -517,6 +517,9 @@ static int try_resolve_ext_mount(struct mount_info *info)
 		pr_info("Found %s mapping for %s mountpoint\n",
 				ext, info->mountpoint);
 		info->external = ext;
+		/* assume manual external mounts as internal sharing */
+		if (info->shared_id)
+			info->internal_sharing = true;
 		return 1;
 	}
 
@@ -919,6 +922,14 @@ static int resolve_external_mounts(struct mount_info *info)
 		struct mount_info *match;
 
 		if (m->parent == NULL || m->is_ns_root)
+			continue;
+
+		/*
+		 * Only allow external mounts in root mntns. External mounts
+		 * lookup is based on mountpoint path, but there may be a lot of
+		 * mounts with same mountpoint across mntnses.
+		 */
+		if (m->nsid->type != NS_ROOT)
 			continue;
 
 		ret = try_resolve_ext_mount(m);
@@ -2624,6 +2635,10 @@ do_bind:
 		}
 	}
 out:
+	if (mi->external && restore_shared_options(mi, private, mi->shared_id,
+						   mi->master_id))
+		return -1;
+
 	mi->mounted = true;
 	exit_code = 0;
 err:

commit 9c0e8d8994c8e44c21432908965ef0d27b72d1cc

mount: remove source check from mounts_sb_equal
When we will have external mounts detected only in root (primary) mntns
of container (see next patch), we will have a problem that source field
on mount_info will be overwriten for dev-external and auto-external
mounts only in primary mntns, thus e.g. mounts_sb_equal and mnt_bind
list population will be broken. Fix it in advance.

If device numbers are equal mount source should be equal too according
to my understanding of kernel code, thus source check is excessive and
we can safely remove it.

Found these problem on VZ7 criu where we already have next patch on
mnt_ext_auto zdtm test:

  (gdb) p m.s_dev
  $ = 74
  (gdb) p m.mnt_master.mnt_master.s_dev
  $ = 74
  (gdb) p m.source
  $ = 0x1db8fb0 "zdtm_auto_ext_mnt"
  (gdb) p m.mnt_master.mnt_master.source
  $ = 0x1db8420 "/tmp/zdtm_ext_auto.XXXXXX/atest"
  (gdb) p m.fstype
  $ = (struct fstype *) 0x534f50 <fstypes+432>
  (gdb) p m.mnt_master.mnt_master.fstype
  $ = (struct fstype *) 0x534f50 <fstypes+432>
  (gdb) p m.options
  $ = 0x1db8ae0 ""
  (gdb) p m.mnt_master.mnt_master.options
  $ = 0x1db8530 ""
  (gdb) p m.mountpoint
  $ = 0x1db8f00 "./zdtm/static/mnt_ext_auto_slave_bind.test"
  (gdb) p m.mnt_master.mnt_master.mountpoint
  $ = 0x1db8e90 "./zdtm/static/mnt_ext_auto_bind.test"
  (gdb) p m.mnt_bind
  $ = {prev = 0x1db8968, next = 0x1db8968}
  (gdb) p &m.mnt_master.mnt_bind
  $ = (struct list_head *) 0x1db8968

The mount m is slave of the slave of m.mnt_master.mnt_master thus they
are obviousely equal enough for a mnt_bind list but they have different
souce and thus they are in different bind lists.

https://jira.sw.ru/browse/PSBM-100080

Fixes: fd48e2239fc3 ("mount: fix external mounts so that they pass check-mounts")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -387,9 +387,6 @@ static bool mounts_sb_equal(struct mount_info *a, struct mount_info *b)
 	if (a->s_dev != b->s_dev)
 		return false;
 
-	if (strcmp(a->source, b->source) != 0)
-		return false;
-
 	if (a->fstype->sb_equal) /* :) */
 		return b->fstype->sb_equal(a, b);
 