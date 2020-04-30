OpenVZ/vzcriu

commit 6944c18705b3201c52d2f1541752a96950121e99

mount: make honest external/internal sharing detection
What we had before was wrong and only worked for auto detected external
mounts. The only right way to detect that the mount has external sharing,
is to check if there is a mount in criu's mount namespace with the same
shared id.

Partially revert hunk from fd48e2239fc3 ("mount: fix external mounts so
that they pass check-mounts")

https://jira.sw.ru/browse/PSBM-100080

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -531,9 +531,6 @@ static int try_resolve_ext_mount(struct mount_info *info)
 		pr_info("Found %s mapping for %s mountpoint\n",
 				ext, info->mountpoint);
 		info->external = ext;
-		/* assume manual external mounts as internal sharing */
-		if (info->shared_id)
-			info->internal_sharing = true;
 		return 1;
 	}
 
@@ -857,6 +854,23 @@ static int validate_mounts(struct mount_info *info, bool for_dump)
 	return 0;
 }
 
+static bool has_external_sharing(struct mount_info *list, struct mount_info *mi) {
+	struct mount_info *t;
+
+	if (!(mi->flags & MS_SHARED))
+		return false;
+
+	for (t = list; t; t = t->next) {
+		if (!mounts_sb_equal(mi, t))
+			continue;
+
+		if (t->flags & MS_SHARED && mi->shared_id == t->shared_id)
+			return true;
+	}
+
+	return false;
+}
+
 static struct mount_info *find_best_external_match(struct mount_info *list, struct mount_info *info)
 {
 	struct mount_info *it, *candidate = NULL;
@@ -937,11 +951,9 @@ static int resolve_external_mounts(struct mount_info *info)
 	struct ns_id *ext_ns = NULL;
 	struct mount_info *m;
 
-	if (opts.autodetect_ext_mounts) {
-		ext_ns = find_ext_ns_id();
-		if (!ext_ns)
-			return -1;
-	}
+	ext_ns = find_ext_ns_id();
+	if (!ext_ns)
+		return -1;
 
 	for (m = info; m; m = m->next) {
 		int ret;
@@ -950,6 +962,13 @@ static int resolve_external_mounts(struct mount_info *info)
 
 		if (m->parent == NULL || m->is_ns_root)
 			continue;
+
+		/* Check mount has external sharing */
+		m->internal_sharing =
+			!has_external_sharing(ext_ns->mnt.mntinfo_list, m);
+		if (!m->internal_sharing)
+			pr_debug("Detected external sharing on %d\n",
+				 m->mnt_id);
 
 		/*
 		 * Only allow external mounts in root mntns. External mounts
@@ -962,20 +981,15 @@ static int resolve_external_mounts(struct mount_info *info)
 		ret = try_resolve_ext_mount(m);
 		if (ret < 0)
 			return ret;
-		if (ret == 1 || !ext_ns)
+		if (ret == 1 || !opts.autodetect_ext_mounts)
 			continue;
 
 		match = find_best_external_match(ext_ns->mnt.mntinfo_list, m);
 		if (!match)
 			continue;
 
-		if (m->flags & MS_SHARED) {
-			if (!opts.enable_external_sharing)
-				continue;
-
-			if (m->shared_id != match->shared_id)
-				m->internal_sharing = true;
-		}
+		if (m->flags & MS_SHARED && !opts.enable_external_sharing)
+			continue;
 
 		if (m->flags & MS_SLAVE) {
 			if (!opts.enable_external_masters)

commit d873c44e0459fc45d852d234d5cb97d37287a59b

mount: don't forget to detect internal_sharing for the root mount
If internal_sharing is not detected it was zero by default meaning
external sharing that's why root mount after migration got external
sharing if it was internal before that.

Fixes: 6944c1870 ("mount: make honest external/internal sharing detection")

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -994,15 +994,15 @@ static int resolve_external_mounts(struct mount_info *info)
 		char *p, *cut_root;
 		struct mount_info *match;
 
-		if (m->parent == NULL || m->is_ns_root)
-			continue;
-
 		/* Check mount has external sharing */
 		m->internal_sharing =
 			!has_external_sharing(ext_ns->mnt.mntinfo_list, m);
 		if (!m->internal_sharing)
 			pr_debug("Detected external sharing on %d\n",
 				 m->mnt_id);
+
+		if (m->parent == NULL || m->is_ns_root)
+			continue;
 
 		/*
 		 * Only allow external mounts in root mntns. External mounts