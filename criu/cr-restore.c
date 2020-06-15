8602a8d2a4af6c23dbeff6434382e19a4d158485
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-06-15 16:44:10 +0300

    cr-restore: move CR_STATE_RESTORE switch to restore_root_task
    The main criu task switches to the CR_STATE_FORKING, but the next stage
    CR_STATE_RESTORE is special and is switched to by the root task instead.
    And the main task is waiting for CR_STATE_RESTORE to finish.
    
    Change it so that the actual switch is done in the main task, and the
    root task still waits all other tasks and calls it's stage ending
    callbacks (ve_itty_resolve and ve_itty_resolve) and gives the power to
    switch stage to the main task.
    
    This way the main task would also be able to call it's own stage ending
    callbacks for CR_STATE_FORKING, which is a real goal of this patch.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -2323,11 +2323,10 @@ static int restore_task_with_children(void *_arg)
 		if (ve_itty_resolve())
 			goto err;
 		fini_restore_mntns();
-		__restore_switch_stage(CR_STATE_RESTORE);
-	} else {
-		if (restore_finish_stage(task_entries, CR_STATE_FORKING) < 0)
-			goto err;
 	}
+
+	if (restore_finish_stage(task_entries, CR_STATE_FORKING) < 0)
+		goto err;
 
 	if (restore_one_task(vpid(current), ca->core))
 		goto err;
@@ -2785,6 +2784,12 @@ static int restore_root_task(struct pstree_item *init)
 	__restore_switch_stage(CR_STATE_FORKING);
 
 skip_ns_bouncing:
+
+	ret = restore_wait_inprogress_tasks();
+	if (ret < 0)
+		goto out_destroy;
+
+	__restore_switch_stage(CR_STATE_RESTORE);
 
 	ret = restore_wait_inprogress_tasks();
 	if (ret < 0)
