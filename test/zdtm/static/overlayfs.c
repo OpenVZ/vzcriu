ed3c19b223413768efc2b3eafadd9add9057b7da
Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
2020-06-15 20:22:48 +0300

    zdtm: add internal overlayfs mounts c/r test
    Three cases checked:
    1. overlayfs mount with upper and lowerdirs (rw)
    2. overlayfs mount with lowerdirs only (ro)
    3. overlayfs mount with overmounted layer (om)
    
    Note:
    Consider the following mount:
    mount -t overlay overlay -o lowerdir=/lower1:/lower2
    	:/lower3,upperdir=/upper,workdir=/work /merged
    In this case from /merged subtree we will see the files
    from /upper, then from /lower1, /lower2, /lower3.
    
    https://jira.sw.ru/browse/PSBM-58614
    
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    
    +++
    zdtm: overlayfs overmount test fix
    
    To check that overmounts c/r works correctly we need
    to have a difference in path depth on overlayfs mountpoint
    and overmounted lowerdir path depth.
    
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    
    +++
    zdtm: don't mount overlayfs in unprivileged user namespace
    
    Test output: ================================
    05:54:55.081:     4: ERR: fs.c:179: Failed to mount overlayfs on
       overlayfs.test/ro/overlayfs with opts:
    nfs_export=on,index=on,lowerdir=overlayfs.test/ro/lower0:overlayfs.test/ro/lower
    105:54:55.081:     4: FAIL: overlayfs.c:87: failed to setup overlayfs
        for ro test (errno = 1 (Operation not permitted))
    
    https://ci.openvz.org/job/CRIU/job/CRIU-virtuozzo-stable/job/vz7-u15/745/
    
    https://jira.sw.ru/browse/PSBM-121284
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    rebase to 3.15 mainstream:
    merge fixups to make test work
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    rebase to mainstream 3.18:
    merge hunks and fixes
    https://src.openvz.org/projects/OVZ/repos/criu/commits/d562f92bb
    https://src.openvz.org/projects/OVZ/repos/criu/commits/f20515021
    use --no-mntns-compat-mode as overmounts are only supported on mount-v2
    
    https://jira.sw.ru/browse/PSBM-144416
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Overlayfs

---
+++ test/zdtm/static/overlayfs.c
@@ -0,0 +1,231 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/mount.h>
+#include <linux/limits.h>
+#include "fs.h"
+
+#include "zdtmtst.h"
+
+const char *test_doc = "Check overlayfs mounts";
+const char *test_author = "Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>";
+// and Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
+
+char *dirname = "overlayfs_mount";
+TEST_OPTION(dirname, string, "directory name", 1);
+
+#define TEST_WORD  "testtest"
+#define TEST_WORD2 "TESTTEST"
+
+/**
+ * Prepare dirname, so that all mounts in it will not propagate and
+ * will be destroyed together with our mount namespace. All files
+ * created in it will not be visible on host and will remove together
+ * with our mountns too.
+ */
+static int prepare_dirname(void)
+{
+	if (mkdir(dirname, 0700) && errno != EEXIST) {
+		pr_perror("Failed to create %s", dirname);
+		return -1;
+	}
+
+	if (mount("none", dirname, "tmpfs", 0, NULL)) {
+		pr_perror("Failed to mount tmpfs on %s", dirname);
+		return -1;
+	}
+
+	if (mount(NULL, dirname, NULL, MS_PRIVATE, NULL)) {
+		pr_perror("Failed to make mount %s private", dirname);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int create_test_file(const char *dirpath, const char *buf, size_t size)
+{
+	int fdo;
+	char fname[PATH_MAX];
+
+	ssprintf(fname, "%s/test.file", dirpath);
+	fdo = open(fname, O_RDWR | O_CREAT, 0644);
+	if (fdo < 0) {
+		pr_perror("open failed");
+		return 1;
+	}
+
+	if (write(fdo, buf, size) != size) {
+		pr_perror("write() failed");
+		close(fdo);
+		return 1;
+	}
+
+	close(fdo);
+	return 0;
+}
+
+static int check_test_file(const char *prefixdir)
+{
+	int fdo;
+	char fname[PATH_MAX], buf[1024];
+
+	ssprintf(fname, "%s/%s/overlayfs/test.file", dirname, prefixdir);
+	fdo = open(fname, O_RDONLY, 0644);
+	if (fdo < 0) {
+		pr_perror("open failed");
+		return 1;
+	}
+
+	buf[sizeof(TEST_WORD) + 1] = '\0';
+	if (read(fdo, buf, sizeof(TEST_WORD)) != sizeof(TEST_WORD)) {
+		fail("Read failed");
+		close(fdo);
+		return 1;
+	}
+	close(fdo);
+
+	if (strcmp(buf, TEST_WORD)) {
+		fail("File corrupted");
+		return 1;
+	}
+
+	return 0;
+}
+
+/* overlayfs mount without upperdir */
+static int create_overlayfs_mount_ro(const char *prefixdir)
+{
+	const char *lower_list[] = { "lower0", "lower1", NULL };
+	char dir[PATH_MAX], lowerdir1[PATH_MAX], lowerdir2[PATH_MAX];
+
+	ssprintf(dir, "%s/%s", dirname, prefixdir);
+	ssprintf(lowerdir1, "%s/%s", dir, lower_list[0]);
+	ssprintf(lowerdir2, "%s/%s", dir, lower_list[1]);
+
+	mkdir(dir, 0700);
+
+	if (overlayfs_setup(dir, lower_list, NULL, NULL, "overlayfs")) {
+		fail("failed to setup overlayfs for %s test", prefixdir);
+		return 1;
+	}
+
+	/*
+	 * Creating files on two lower layers with the same name,
+	 * in result mount we should see the file from the lowest
+	 * layer.
+	 */
+	if (create_test_file(lowerdir1, TEST_WORD, sizeof(TEST_WORD)))
+		return 1;
+	if (create_test_file(lowerdir2, TEST_WORD2, sizeof(TEST_WORD2)))
+		return 1;
+
+	return 0;
+}
+
+/* overlayfs mount with upperdir (as docker uses) */
+static int create_overlayfs_mount_rw(const char *prefixdir)
+{
+	const char *lower_list[] = { "lower0", "lower1", NULL };
+	const char *upper = "upper";
+	char dir[PATH_MAX], upperdir[PATH_MAX], lowerdir2[PATH_MAX];
+
+	ssprintf(dir, "%s/%s", dirname, prefixdir);
+	ssprintf(upperdir, "%s/%s", dir, upper);
+	ssprintf(lowerdir2, "%s/%s", dir, lower_list[1]);
+	mkdir(dir, 0700);
+
+	if (overlayfs_setup(dir, lower_list, upper, "work", "overlayfs")) {
+		fail("failed to setup overlayfs for %s test", prefixdir);
+		return 1;
+	}
+
+	/*
+	 * Creating files on two lower layers with the same name,
+	 * in result mount we should see the file from upper
+	 * layer.
+	 */
+	if (create_test_file(upperdir, TEST_WORD, sizeof(TEST_WORD)))
+		return 1;
+	if (create_test_file(lowerdir2, TEST_WORD2, sizeof(TEST_WORD2)))
+		return 1;
+
+	return 0;
+}
+
+/* overlayfs mount with overmounted source dirs */
+static int create_overlayfs_mount_overmount(const char *prefixdir)
+{
+	const char *lower_list[] = { "tmp/lower1", "lower2", NULL };
+	char dir[PATH_MAX], lowerdir1[PATH_MAX];
+
+	ssprintf(dir, "%s/%s", dirname, prefixdir);
+	ssprintf(lowerdir1, "%s/%s", dir, lower_list[0]);
+
+	mkdir(dir, 0700);
+
+	if (overlayfs_setup(dir, lower_list, NULL, NULL, "overlayfs")) {
+		fail("failed to setup overlayfs for %s test", prefixdir);
+		return 1;
+	}
+
+	/*
+	 * Creating first file on the lowest layer, after overmount
+	 * the lowest layer path, and create second file on overmounted
+	 * path. We should see first file (overlayfs must be mounted
+	 * before lowerest layer path will be overmounted).
+	 */
+	if (create_test_file(lowerdir1, TEST_WORD, sizeof(TEST_WORD)))
+		return 1;
+
+	/*
+	 * Let's mount tmpfs on lowerdir
+	 */
+	if (mount("none", lowerdir1, "tmpfs", 0, NULL)) {
+		pr_perror("Failed to mount tmpfs on %s", lowerdir1);
+		return 1;
+	}
+
+	if (mount(NULL, lowerdir1, NULL, MS_PRIVATE, NULL)) {
+		pr_perror("Failed to make mount %s private", lowerdir1);
+		return 1;
+	}
+
+	if (create_test_file(lowerdir1, TEST_WORD2, sizeof(TEST_WORD2)))
+		return 1;
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int ret = 1;
+
+	test_init(argc, argv);
+
+	if (prepare_dirname())
+		return 1;
+
+	if (create_overlayfs_mount_ro("ro"))
+		goto err;
+	if (create_overlayfs_mount_rw("rw"))
+		goto err;
+	if (create_overlayfs_mount_overmount("om"))
+		goto err;
+
+	test_daemon();
+	test_waitsig();
+
+	if (check_test_file("ro"))
+		goto err;
+	if (check_test_file("rw"))
+		goto err;
+	if (check_test_file("om"))
+		goto err;
+
+	pass();
+	ret = 0;
+err:
+	return ret;
+}
