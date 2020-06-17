4fc0dcbfba638c34adae1fab0a1a5baa54b7b765
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-06-17 12:30:39 +0300

    jenkins: run all tests both for old mounts and mounts-v2
    For old mounts also disable zdtm/static/ns_file_bindmount test.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- test/jenkins/criu.sh
+++ test/jenkins/criu.sh
@@ -4,4 +4,11 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./test/zdtm.py run --all --keep-going --report report --parallel 4 || fail
+FAIL=0
+./test/zdtm.py run --all -x ns_file_bindmount --keep-going --report report --parallel 4 || \
+	FAIL=$?
+./test/zdtm.py run --all --mounts-v2 --keep-going --report report --parallel 4 || FAIL=$?
+
+if [ $FAIL -ne 0 ]; then
+	fail
+fi
