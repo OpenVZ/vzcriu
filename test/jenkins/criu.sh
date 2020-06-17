52195add81acb0244d529a893f5206180267d369
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-06-17 12:30:39 +0300

    jenkins: run all tests both for old mounts and mounts-v2
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Mounts

--- test/jenkins/criu.sh
+++ test/jenkins/criu.sh
@@ -4,4 +4,10 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./test/zdtm.py run --all --flavor h,ns,uns --keep-going --report report --parallel 4 || fail
+FAIL=0
+./test/zdtm.py run --all --flavor h,ns,uns --keep-going --report report --parallel 4 || FAIL=$?
+./test/zdtm.py run --all --flavor h,ns,uns --no-mntns-compat-mode --keep-going --report report --parallel 4 || FAIL=$?
+
+if [ $FAIL -ne 0 ]; then
+        fail
+fi
