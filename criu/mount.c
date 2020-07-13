55d5c6636fc146deb874da0939ae1880f763080a
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-07-13 11:39:30 +0300

    mount: detect unsupported mntns root overmount
    We can't enter a mount namespace and see all mounts if mountns has
    overmounted root, we get to the overmount as root of our chroot instead.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Mounts

--- criu/mount.c
+++ criu/mount.c
@@ -798,12 +798,26 @@ static bool unsupported_mount(const struct mount_info *m)
 	return false;
 }
 
+int validate_mntns_root_overmount(struct mount_info *m)
+{
+	struct mount_info *parent = m->parent;
+
+	if (!parent || !m->is_ns_root || !parent->is_ns_root)
+		return 0;
+
+	pr_err("Overmounted mntns root is not supported\n");
+	return -1;
+}
+
 int validate_mounts(struct mount_info *info, bool for_dump)
 {
 	struct mount_info *m, *t;
 
 	for (m = info; m; m = m->next) {
 		if (validate_children_collision(m))
+			return -1;
+
+		if (validate_mntns_root_overmount(m))
 			return -1;
 
 		if (mnt_is_external_bind(m))
