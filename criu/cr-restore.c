OpenVZ/vzcriu

commit 31680a21732c8f03f6efcd6ff86d16bf4fc28e7d

pid: Create pid_ns helpers
Task may set last_pid only for its active pid namespace,
so if NSpid of a child contains more then one level, we
need external help to populate the whole pid hierarhy
(pid in parent pid_ns, pid in grand parent etc). Pid ns
helpers are used for that.

These are childred of usernsd, which are listening for
socket, and setting requested last pid in their active
pid_ns.

v4: Move destroy_pid_ns_helpers() before CR_STATE_RESTORE_SIGCHLD
change, as they must die before zombies.

v3: Block SIGCHLD during stoppinig of pid_ns helpers.

Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

+++
namespaces: Silence coverity on get_service_fd()

Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

rebase: split do_create_pid_ns_helper part

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

+++
pid_ns: Destroy helpers via kill()

When INIT_PID of a pid_ns emergency exits, kernel
kills all processes belonging to the namespace.
So, it's hopelessly to wait helper answer to destroy
request. Use kill() to destroy instead of that.
It will be noop in case of a handler is already
killed, and we won't stuck.

Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

+++
restore: create tansport socket for pidns helpers from the root task

The root task can live in another netns and it has to be restored
before executing setup-namespaces scripts.

Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Looks-good-to: Pavel Emelyanov <xemul@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

rebase: split do_create_pid_ns_helper part

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

+++
pid_ns: Rename do_destroy_pid_ns_helper()

It must sound do_destroy_pid_ns_helpers() with *s*.

Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

+++

criu/cr-restore.c
@@ -1794,7 +1794,7 @@ static int setup_current_pid_ns(void)
 		pr_err("Can't add fd to fdstore\n");
 		return -1;
 	}
-	return 0;
+	return create_pid_ns_helper(ns);
 }
 
 /*
@@ -2400,7 +2400,7 @@ static int restore_root_task(struct pstree_item *init)
 
 		if (ptrace(PTRACE_SEIZE, init->pid->real, 0, 0)) {
 			pr_perror("Can't attach to init");
-			goto out_kill;
+			goto out_destroy;
 		}
 	}
 
@@ -2412,25 +2412,25 @@ static int restore_root_task(struct pstree_item *init)
 	 * prepare_userns_creds() must be called after filling mappings.
 	 */
 	if ((root_ns_mask & CLONE_NEWUSER) && prepare_userns(init->pid->real, userns_entry))
-		goto out_kill;
+		goto out_destroy;
 
 	pr_info("Wait until namespaces are created\n");
 	ret = restore_wait_inprogress_tasks();
 	if (ret)
-		goto out_kill;
+		goto out_destroy;
 
 	ret = run_scripts(ACT_SETUP_NS);
 	if (ret)
-		goto out_kill;
+		goto out_destroy;
 
 	ret = restore_switch_stage(CR_STATE_PREPARE_NAMESPACES);
 	if (ret)
-		goto out_kill;
+		goto out_destroy;
 
 	if (root_ns_mask & CLONE_NEWNS) {
 		mnt_ns_fd = open_proc(init->pid->real, "ns/mnt");
 		if (mnt_ns_fd < 0)
-			goto out_kill;
+			goto out_destroy;
 	}
 
 	if (root_ns_mask & opts.empty_ns & CLONE_NEWNET) {
@@ -2443,12 +2443,12 @@ static int restore_root_task(struct pstree_item *init)
 		 */
 		ret = network_lock_internal();
 		if (ret)
-			goto out_kill;
+			goto out_destroy;
 	}
 
 	ret = run_scripts(ACT_POST_SETUP_NS);
 	if (ret)
-		goto out_kill;
+		goto out_destroy;
 
 	__restore_switch_stage(CR_STATE_FORKING);
 
@@ -2456,7 +2456,7 @@ skip_ns_bouncing:
 
 	ret = restore_wait_inprogress_tasks();
 	if (ret < 0)
-		goto out_kill;
+		goto out_destroy;
 
 	/*
 	 * Zombies die after CR_STATE_RESTORE which is switched
@@ -2467,6 +2467,10 @@ skip_ns_bouncing:
 		if (item->pid->state == TASK_DEAD)
 			task_entries->nr_threads--;
 	}
+
+	ret = destroy_pid_ns_helpers();
+	if (ret < 0)
+		goto out_kill;
 
 	ret = restore_switch_stage(CR_STATE_RESTORE_SIGCHLD);
 	if (ret < 0)
@@ -2605,6 +2609,8 @@ skip_ns_bouncing:
 
 out_kill_network_unlocked:
 	pr_err("Killing processes because of failure on restore.\nThe Network was unlocked so some data or a connection may have been lost.\n");
+out_destroy:
+	destroy_pid_ns_helpers();
 out_kill:
 	/*
 	 * The processes can be killed only when all of them have been created,

criu/include/namespaces.h
@@ -267,5 +267,8 @@ static inline int pid_ns_root_off(void)
 	return 0;
 }
 extern int reserve_pid_ns_helpers(void);
+extern int create_pid_ns_helper(struct ns_id *ns);
+extern int destroy_pid_ns_helpers(void);
+extern int request_set_next_pid(int pid_ns_id, pid_t pid, int sk);
 
 #endif /* __CR_NS_H__ */

criu/namespaces.c
@@ -16,6 +16,7 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/ptrace.h>
+#include <sys/file.h>
 
 #include "page.h"
 #include "rst-malloc.h"
@@ -40,6 +41,11 @@
 #include "fdstore.h"
 #include "clone-noasan.h"
 #include "proc_parse.h"
+
+#define __sys(foo)	foo
+#define __sys_err(ret)	(-errno)
+
+#include "ns-common.c"
 
 static struct ns_desc *ns_desc_array[] = {
 	&net_ns_desc,
@@ -1532,12 +1538,26 @@ static void unsc_msg_pid_fd(struct unsc_msg *um, pid_t *pid, int *fd)
 static void usernsd_handler(int signal, siginfo_t *siginfo, void *data)
 {
 	pid_t pid = siginfo->si_pid;
+	struct ns_id *ns;
+	struct pid *tpid;
 	int status;
 
 	while (pid) {
 		pid = waitpid(-1, &status, WNOHANG);
 		if (pid <= 0)
 			return;
+		for (ns = ns_ids; ns; ns = ns->next) {
+			if (ns->nd != &pid_ns_desc)
+				continue;
+			tpid = pstree_pid_by_virt(ns->ns_pid);
+			if (tpid->real == pid) {
+				tpid->real = -1;
+				break;
+			}
+		}
+
+		if (!ns)
+			pr_err("Spurious pid ns helper: pid=%d\n", pid);
 
 		pr_err("%d finished unexpected: status=%d\n", pid, status);
 		futex_abort_and_wake(&task_entries->nr_in_progress);
@@ -2575,6 +2595,223 @@ int reserve_pid_ns_helpers(void)
 		return 0;
 
 	return walk_namespaces(&pid_ns_desc, do_reserve_pid_ns_helpers, NULL);
+}
+
+static int pid_ns_helper_sock(struct ns_id *ns)
+{
+	struct sockaddr_un addr;
+	socklen_t len;
+	int sk;
+
+	sk = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (sk < 0) {
+		pr_perror("Can't create helper socket");
+		return -1;
+	}
+	pid_ns_helper_socket_name(&addr, &len, ns->id);
+
+	if (bind(sk, (struct sockaddr *)&addr, len) < 0) {
+		pr_perror("Can't bind pid_ns sock");
+		close(sk);
+		return -1;
+	}
+
+	return sk;
+}
+
+static int pid_ns_helper(struct ns_id *ns, int sk)
+{
+	struct sockaddr_un addr;
+	struct msghdr msg = {0};
+	struct iovec iov;
+	pid_t pid;
+
+	msg.msg_name = &addr;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	while (1) {
+		int answer = 0;
+		msg.msg_namelen = sizeof(addr);
+		iov.iov_base = &pid;
+		iov.iov_len = sizeof(pid);
+
+		if (recvmsg(sk, &msg, 0) < 0) {
+			pr_perror("recv() failed to read pid");
+			break;
+		}
+
+		if (__set_next_pid(pid) < 0) {
+			pr_err("Can't set next pid\n");
+			answer = -1;
+		}
+
+		iov.iov_base = &answer;
+		iov.iov_len = sizeof(answer);
+		if (sendmsg(sk, &msg, 0) < 0) {
+			pr_perror("Can't send answer");
+			break;
+		}
+	}
+
+	return -1;
+}
+
+static int do_create_pid_ns_helper(void *arg, int sk, pid_t unused_pid)
+{
+	int pid_ns_fd, i, transport_fd, saved_errno, ret = -1;
+	struct pstree_item *ns_reaper;
+	struct ns_id *ns, *tmp;
+	struct pid *pid;
+	pid_t child;
+
+	ns_reaper = *(struct pstree_item **)arg;
+	ns = lookup_ns_by_id(ns_reaper->ids->pid_ns_id, &pid_ns_desc);
+
+	pid = pstree_pid_by_virt(ns->ns_pid);
+	if (!pid) {
+		pr_err("Can't find helper reserved pid\n");
+		return -1;
+	}
+
+	if (switch_ns(ns_reaper->pid->real, &pid_ns_desc, &pid_ns_fd) < 0)
+		return -1;
+
+	lock_last_pid();
+
+	transport_fd = get_service_fd(TRANSPORT_FD_OFF);
+	BUG_ON(transport_fd < 0);
+	/*
+	 * Starting not from pid->level - 1, as it's helper has not created yet
+	 * (we're creating it in the moment), and the true pid for this level
+	 * is set by the task, who does close(CLONE_NEWPID) (this task is sender of fd).
+	 */
+	for (i = pid->level - 2, tmp = ns->parent; i >= 0; i--, tmp = tmp->parent)
+		if (request_set_next_pid(tmp->id, pid->ns[i].virt, transport_fd)) {
+			unlock_last_pid();
+			goto restore_ns;
+		}
+	child = fork();
+	if (!child)
+		exit(pid_ns_helper(ns, sk));
+	saved_errno = errno;
+	unlock_last_pid();
+	if (child < 0) {
+		errno = saved_errno;
+		pr_perror("Can't fork");
+		goto restore_ns;
+	}
+	futex_set_and_wake(&ns->pid.helper_created, 1);
+	pid->real = child;
+
+	ret = 0;
+restore_ns:
+	if (restore_ns(pid_ns_fd, &pid_ns_desc) < 0)
+		ret = -1;
+	return ret;
+}
+
+/*
+ * Task may set last_pid only for its active pid namespace,
+ * so if NSpid of a child contains more then one level, we
+ * need external help to populate the whole pid hierarhy
+ * (pid in parent pid_ns, pid in grand parent etc). Pid ns
+ * helpers are used for that.
+ *
+ * We need a task or tasks to be a parent of pid_ns helpers.
+ * To live in common hierarhy and to be a TASK_HELPER is not
+ * possible, because it introduces circular dependencies.
+ * The same is to be children of criu main task, because
+ * we already have dependencies between it and root_item
+ * (NO more dependencies!). So, we choose usernsd for that:
+ * it always exists and have command interface.
+ */
+int create_pid_ns_helper(struct ns_id *ns)
+{
+	struct pid *pid;
+	int sk;
+
+	BUG_ON(getpid() != INIT_PID);
+	BUG_ON(ns->parent && !futex_get(&ns->parent->pid.helper_created));
+
+	pid = pstree_pid_by_virt(ns->ns_pid);
+	if (pid == NULL) {
+		pr_err("Can't get pid for %d\n", ns->ns_pid);
+		return -1;
+	}
+	if (__set_next_pid(last_level_pid(pid)) < 0) {
+		pr_err("Can't set next fd\n");
+		return -1;
+	}
+
+	sk = pid_ns_helper_sock(ns);
+	if (sk < 0)
+		return -1;
+
+	if (userns_call(do_create_pid_ns_helper, 0, &current, sizeof(current), sk) < 0) {
+		pr_err("Can't create pid_ns helper\n");
+		close(sk);
+		return -1;
+	}
+
+	close(sk);
+	return 0;
+}
+
+static int do_destroy_pid_ns_helpers(void *arg, int fd, pid_t unused)
+{
+	int status, sig_blocked = true, ret = 0;
+	sigset_t sig_mask;
+	struct ns_id *ns;
+	struct pid *pid;
+
+	if (block_sigmask(&sig_mask, SIGCHLD)) {
+		sig_blocked = false;
+		ret = -1;
+	}
+
+	for (ns = ns_ids; ns; ns = ns->next) {
+		if (ns->nd != &pid_ns_desc)
+			continue;
+		pid = pstree_pid_by_virt(ns->ns_pid);
+		if (!pid) {
+			pr_err("Can't find pid_ns helper\n");
+			ret = -1;
+			continue;
+		}
+		if (pid->real <= 0)
+			continue;
+
+		if (kill(pid->real, SIGKILL) < 0) {
+			pr_perror("Can't kill\n");
+			ret = -1;
+			continue;
+		}
+
+		if (waitpid(pid->real, &status, 0) != pid->real) {
+			pr_perror("Error during waiting pid_ns helper");
+			ret = -1;
+			continue;
+		}
+		pid->real = -1;
+	}
+
+	if (sig_blocked && restore_sigmask(&sig_mask))
+		ret = -1;
+
+	return ret;
+}
+
+int destroy_pid_ns_helpers(void)
+{
+	if (!(root_ns_mask & CLONE_NEWPID))
+		return 0;
+
+	if (userns_call(do_destroy_pid_ns_helpers, 0, NULL, 0, -1) < 0) {
+		pr_err("Can't create pid_ns helper\n");
+		return -1;
+	}
+	return 0;
 }
 
 struct ns_desc pid_ns_desc = NS_DESC_ENTRY(CLONE_NEWPID, "pid");

criu/ns-common.c
@@ -1,0 +1,51 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+
+void pid_ns_helper_socket_name(struct sockaddr_un *addr, socklen_t *len, unsigned int id)
+{
+	const char prefix[] = "0/criu-pid-ns-";
+	const char int_max[] = "2147483647";
+
+	*len = sizeof(*addr) - sizeof(addr->sun_path) +
+	       sizeof(prefix) - 1 + sizeof(int_max) - 1;
+
+	addr->sun_family = AF_UNIX;
+
+	memset(addr->sun_path + sizeof(prefix) - 1, '\0', sizeof(int_max) - 1);
+#ifdef CR_NOGLIBC
+	std_sprintf(addr->sun_path, "%s%d", prefix, id);
+#else
+	sprintf(addr->sun_path, "%s%d", prefix, id);
+#endif
+	addr->sun_path[0] = '\0';
+}
+
+/* Send helper a request to set next pid and receive success */
+int request_set_next_pid(int pid_ns_id, pid_t pid, int sk)
+{
+	struct sockaddr_un addr;
+	int answer, ret;
+	socklen_t len;
+
+	BUG_ON(pid == -1);
+
+	pid_ns_helper_socket_name(&addr, &len, pid_ns_id);
+	ret = __sys(sendto)(sk, &pid, sizeof(pid), 0, (struct sockaddr *)&addr, len);
+	if (ret	< 0) {
+		pr_err("Can't send request: err=%d\n", __sys_err(ret));
+		return -1;
+	}
+
+	ret = __sys(recvfrom)(sk, &answer, sizeof(answer), 0, NULL, NULL);
+	if (ret < 0) {
+		pr_err("Can't recv answer: err=%d\n", __sys_err(ret));
+		return -1;
+	}
+
+	if (answer != 0) {
+		pr_err("Error answer\n");
+		return -1;
+	}
+
+	return 0;
+}

criu/pie/restorer.c
@@ -52,6 +52,11 @@
 #include "restorer.h"
 #include "namespaces.h"
 
+#define __sys(foo)	sys_##foo
+#define __sys_err(ret)	ret
+
+#include "../ns-common.c"
+
 #ifndef PR_SET_PDEATHSIG
 #define PR_SET_PDEATHSIG 1
 #endif

commit 15abd1b3b4f3f7a4ab5ad1286a1729af909facb3

cr-restore: in out_kill_network_unlocked we should not destroy helpers
Fixes: 31680a217 ("pid: Create pid_ns helpers")

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/cr-restore.c
@@ -2941,6 +2941,7 @@ skip_ns_bouncing:
 
 out_kill_network_unlocked:
 	pr_err("Killing processes because of failure on restore.\nThe Network was unlocked so some data or a connection may have been lost.\n");
+	goto out_kill;
 out_destroy:
 	destroy_pid_ns_helpers();
 out_kill: