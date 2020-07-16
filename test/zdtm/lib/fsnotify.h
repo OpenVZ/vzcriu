dd55720235cce9c6bbd3e261706b5ba53c52a0cc
Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>
2020-07-16 12:49:27 +0300

    zdtm: moved fanotify-specific code to lib, renamed functions that form api.
    The code in fanotify00.c test has a bunch of generic helpful routines for
    fanotify objects, so it can be put inside of zdtm core utils and reused
    in other tests.
    
    I've also renamed some of the functions to have common prefix in names:
      show_fanotify_obj -> fanotify_obj_show
      parse_fanotify_fdinfo -> fanotify_obj_parse
      cmp_fanotify_obj -> fanotify_obj_cmp
    
    https://jira.sw.ru/browse/PSBM-104961
    Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>
    
    Feature: Overlayfs

---
+++ test/zdtm/lib/fsnotify.h
@@ -0,0 +1,40 @@
+#pragma once
+
+struct fanotify_mark_inode {
+	unsigned long i_ino;
+	unsigned int s_dev;
+	unsigned int mflags;
+	unsigned int mask;
+	unsigned int ignored_mask;
+	unsigned int fhandle_bytes;
+	unsigned int fhandle_type;
+	unsigned char fhandle[512];
+};
+
+struct fanotify_mark_mount {
+	unsigned int mnt_id;
+	unsigned int mflags;
+	unsigned int mask;
+	unsigned int ignored_mask;
+};
+
+struct fanotify_glob {
+	unsigned int faflags;
+	unsigned int evflags;
+};
+
+struct fanotify_obj {
+	struct fanotify_glob glob;
+	struct fanotify_mark_inode inode;
+	struct fanotify_mark_mount mount;
+};
+
+int fanotify_init(unsigned int flags, unsigned int event_f_flags);
+
+int fanotify_mark(int fanotify_fd, unsigned int flags, unsigned long mask, int dfd, const char *pathname);
+
+void fanotify_obj_show(struct fanotify_obj *obj);
+
+int fanotify_obj_cmp(struct fanotify_obj *old, struct fanotify_obj *new);
+
+int fanotify_obj_parse(int fd, struct fanotify_obj *obj, unsigned int expected_to_meet);
