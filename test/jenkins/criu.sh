10ecc801296192673813ff423f43fabafa40024c
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-07-21 10:23:44 +0300

    zdtm: disable overlayfs test for old mounts engine
    After enabling /sys/module/overlay/parameters/{dyn,mnt_id}_path_opts
    on zdtm.virtuozzo.com, Jenkins started to fail:
    
    FAIL: overlayfs.c:90: File corrupted (errno = 11 (Resource temporarily unavailable))
    https://ci.openvz.org/job/CRIU/job/CRIU-virtuozzo-stable/job/vz7-u15/190/consoleFull
    
    Because without mounts-v2 overmounts of lower dirs break overlayfs
    restore.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- test/jenkins/criu.sh
+++ test/jenkins/criu.sh
@@ -3,7 +3,7 @@ set -e
 source `dirname $0`/criu-lib.sh
 prep
 FAIL=0
-./test/zdtm.py run --all -x ns_file_bindmount -x pidns_proc --keep-going --report report --parallel 4 || \
+./test/zdtm.py run --all -x ns_file_bindmount -x pidns_proc -x overlayfs --keep-going --report report --parallel 4 || \
 	FAIL=$?
 ./test/zdtm.py run --all --mounts-v2 --keep-going --report report --parallel 4 || FAIL=$?
 
