OpenVZ/vzcriu

commit 51e9685fb69f14b69cef6c0351e5964056b86551

mount: add plain mountpoint for cr_time mount
mnt: Add cr-time mountpoint /tmp/.criu.mntns.UQIroZ/binfmt_misc with parent /tmp/.criu.mntns.UQIroZ(0)
mnt: Start with 0:(null)
mnt-v2: Create plain mountpoint (null) for 0
Error (criu/mount-v2.c:528): mnt-v2: Unable to mkdir mountpoint (null): Bad address

https://jira.sw.ru/browse/PSBM-96757

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -1768,6 +1768,7 @@ struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, cha
 {
 	struct mount_info *mi, *t, *parent;
 	bool add_slash = false;
+	char path[PATH_MAX];
 	int len;
 
 	mi = mnt_entry_alloc(rst);
@@ -1789,6 +1790,8 @@ struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, cha
 		sprintf(mi->mountpoint, "%s%s", root->mountpoint, path);
 	else
 		sprintf(mi->mountpoint, "%s/%s", root->mountpoint, path);
+	snprintf(path, sizeof(path), "%s/mnt-crtime", mnt_roots);
+	mi->plain_mountpoint = xstrdup(path);
 	mi->mnt_id = HELPER_MNT_ID;
 	mi->flags = mi->sb_flags = 0;
 	mi->root = xstrdup("/");

commit 1eabf75f32e75538a02fe900c8c6dcb750466e28

mount: fix compilation as path is declared twice
https://jira.sw.ru/browse/PSBM-96757

Fixes: 51e9685fb ("mount: add plain mountpoint for cr_time mount")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -1767,8 +1767,8 @@ err:
 struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, char *fsname, const char *path, unsigned int s_dev, bool rst)
 {
 	struct mount_info *mi, *t, *parent;
+	char plain_mountpoint[PATH_MAX];
 	bool add_slash = false;
-	char path[PATH_MAX];
 	int len;
 
 	mi = mnt_entry_alloc(rst);
@@ -1790,8 +1790,8 @@ struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, cha
 		sprintf(mi->mountpoint, "%s%s", root->mountpoint, path);
 	else
 		sprintf(mi->mountpoint, "%s/%s", root->mountpoint, path);
-	snprintf(path, sizeof(path), "%s/mnt-crtime", mnt_roots);
-	mi->plain_mountpoint = xstrdup(path);
+	snprintf(plain_mountpoint, sizeof(plain_mountpoint), "%s/mnt-crtime", mnt_roots);
+	mi->plain_mountpoint = xstrdup(plain_mountpoint);
 	mi->mnt_id = HELPER_MNT_ID;
 	mi->flags = mi->sb_flags = 0;
 	mi->root = xstrdup("/");