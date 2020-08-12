bb59520508574fa410aaee7d02a8221e0a2f2af3
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-08-12 18:37:41 +0300

    vz/images: add vz_ prefix to vz specific fields in criu_opts
    Just manually do:
    
    s/mounts_v2/vz_mounts_v2/
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/cr-service.c
+++ criu/cr-service.c
@@ -656,7 +656,7 @@ static int setup_opts_from_req(int sk, CriuOpts *req)
 	if (req->check_mounts)
 		opts.check_mounts = true;
 
-	if (req->mounts_v2)
+	if (req->vz_mounts_v2)
 		opts.mounts_v2 = true;
 
 	log_set_loglevel(opts.log_level);
