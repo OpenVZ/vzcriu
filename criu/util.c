OpenVZ/vzcriu

commit cc71dc93dec12f6aa4c91fb17e9db61e26cb86a1

util: add get_relative_path helper
This is a smart way of getting relative paths:

1) Always returns relative path, no unexpected starting '/';
2) Detects subpath even if path formats are different, only real directory
names matter.

We have many places where we need to cut subpath from path. Different code
blocks doing this job spread widely across the codebase for instance see:
cut_root_for_bind and root_path_from_parent. But those implementations rely on
the fact that subpath's and path's formats are the same.

https://jira.sw.ru/browse/PSBM-58617

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/util.c
@@ -1694,3 +1694,48 @@ int open_fd_of_real_pid(pid_t pid, int fd, int flags)
 	       (flags != O_RDWR));
 	return userns_call(fn_open_proc, UNS_FDOUT, &opa, sizeof(opa), -1);
 }
+
+/*
+ * This function cuts sub_path from the path.
+ * 1) It asumes all relative paths given are relative to "/":
+ * 	/a/b/c is the same as a/b/c
+ * 2) It can handle paths with multiple consequent slashes:
+ * 	///a///b///c is the same as /a/b/c
+ * 3) It always returns relative path, with no leading slash:
+ * 	get_relative_path("/a/b/c", "/") would be "a/b/c"
+ * 	get_relative_path("/a/b/c", "/a/b") would be "c"
+ * 	get_relative_path("/", "/") would be ""
+ */
+char *get_relative_path(char *path, char *sub_path)
+{
+	bool skip_slashes = true;
+
+	while (1) {
+		if ((*path == '/' || *path == '\0') &&
+		    (*sub_path == '/' || *sub_path == '\0'))
+			skip_slashes = true;
+
+		if (skip_slashes) {
+			while (*path == '/')
+				path++;
+			while (*sub_path == '/')
+				sub_path++;
+		}
+
+		if (*sub_path == '\0') {
+			if (skip_slashes)
+				return path;
+			return NULL;
+		}
+		skip_slashes = false;
+
+		if (*path == '\0')
+			return NULL;
+
+		if (*path != *sub_path)
+			return NULL;
+
+		path++;
+		sub_path++;
+	}
+}

criu/include/util.h
@@ -254,6 +254,8 @@ static inline bool issubpath(const char *path, const char *sub_path)
 		(end == '/' || end == '\0');
 }
 
+extern char *get_relative_path(char *path, char *sub_path);
+
 /*
  * mkdir -p
  */

commit 0e66af77c86615fc71ec9cbf140cd3361e04b1c6

util: make get_relative_path understand paths with '.'s
Sadly I've missed that ns_mountpoint on dump is prefixed with "./" and
when we use get_relative_path on it the "." between slashes is
considered as normal path component. So to support this kind of paths
lets skip "^./", "/./" and "/.$" at the time of slash skipping.

Now if you call get_relative_path("./a/b", "a/") you get "b" not "null"
as before the patch. Also any silly paths are supported too:

get_relative_path("./a////.///./b//././c", "///./a/b") == "c"

Note ".." in path is stil unsupported, but it would be to hard to
fix it.

Fixes: cc71dc93d ("util: add get_relative_path helper")

https://jira.sw.ru/browse/PSBM-106869

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/util.c
@@ -1709,6 +1709,9 @@ int open_fd_of_real_pid(pid_t pid, int fd, int flags)
  * 	get_relative_path("/a/b/c", "/") would be "a/b/c"
  * 	get_relative_path("/a/b/c", "/a/b") would be "c"
  * 	get_relative_path("/", "/") would be ""
+ * 4) It can handle paths with single dots:
+ * 	get_relative_path("./a/b", "a/") would be "b"
+ * 5) Note ".." in paths are not supported and handled as normal directory name
  */
 char *get_relative_path(char *path, char *sub_path)
 {
@@ -1720,9 +1723,13 @@ char *get_relative_path(char *path, char *sub_path)
 			skip_slashes = true;
 
 		if (skip_slashes) {
-			while (*path == '/')
+			while (*path == '/' ||
+			       (path[0] == '.' && (path[1] == '/' ||
+						   path[1] == '\0')))
 				path++;
-			while (*sub_path == '/')
+			while (*sub_path == '/' ||
+			       (sub_path[0] == '.' && (sub_path[1] == '/' ||
+						       sub_path[1] == '\0')))
 				sub_path++;
 		}
 