7332fa74f676c6742f7389e1ac78dcfb3cb69b0b
Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
2020-08-24 21:10:51 +0300

    zdtm: add openvswitch test
    Add test to check if link configuration is restored correctly.
    However, there are no flows to check if packets are actually passing through.
    
    https://jira.sw.ru/browse/PSBM-105844
    Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>

---
+++ test/zdtm/static/openvswitch00.c
@@ -0,0 +1,75 @@
+#include <sched.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <net/if.h>
+#include <sys/stat.h>
+
+#include "zdtmtst.h"
+
+const char *test_doc    = "Check if datapath and vports links are restored correctly";
+const char *test_author = "Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>";
+
+char *dirname;
+TEST_OPTION(dirname, string, "test directory name", 1);
+
+#define EXEC_CMD(cmdfmt, arg...)	do {			\
+		snprintf(cmd, sizeof(cmd), cmdfmt, ## arg); \
+		if (system(cmd)) { \
+			pr_err("FAILED: %s\n", cmd); \
+			goto err; \
+		} \
+	} while (0)
+
+int main(int argc, char **argv)
+{
+	char cmd[128] = {0};
+	char before[128] = {0};
+	char after[128] = {0};
+	int ret = 0;
+
+	test_init(argc, argv);
+
+	if (mkdir(dirname, 0700) < 0) {
+		pr_perror("Can't make dir");
+		exit(1);
+	}
+
+	system("mknod /dev/urandom c 1 9");
+	EXEC_CMD("ovs-dpctl add-dp dp0");
+
+	/* create veth and plug it into dp0 */
+	EXEC_CMD("ip link add veth1 type veth peer name veth2");
+	EXEC_CMD("ovs-dpctl add-if dp0 veth1");
+	EXEC_CMD("ip link set veth1 up");
+	EXEC_CMD("ip link set veth2 up");
+
+	/* create vxlan and internal vports */
+	EXEC_CMD("ovs-dpctl add-if dp0 vx,type=vxlan,remote_ip=10.0.0.0");
+	EXEC_CMD("ovs-dpctl add-if dp0 int0,type=internal");
+
+	snprintf(before, sizeof(before), "%s/before", dirname);
+	EXEC_CMD("ip a > %s", before);
+
+	test_daemon();
+	test_waitsig();
+
+	snprintf(after, sizeof(after), "%s/after", dirname);
+	EXEC_CMD("ip a > %s", after);
+
+	snprintf(cmd, sizeof(cmd), "diff %s %s", before, after);
+	if (system(cmd)) {
+		pr_err("links before/after c/r differ!\n");
+		goto err;
+	}
+	pass();
+	goto clean;
+
+err:
+	fail();
+	ret = -1;
+clean:
+	rmdir(dirname);
+	return ret;
+}
