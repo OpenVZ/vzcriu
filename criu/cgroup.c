5c57c10307554a89555a90224ee0041445ad9fd0
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-09-01 13:03:32 +0300

    cgroup: add some additional debugging information
    Previousely there were no way to see if we actually walking some
    directory in some cgroup hierarchy on dump => add it.
    
    When we rewrite cgroup roots, lets at least print first cgroup hierarchy
    name from this controller, so that we know for which controller the
    rewrite takes place.
    
    Curently there is always cname[0] as it is originated from
    new_controller, add a bug to check it.
    
    v2: bug on zero n_cname
    
    Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Debug

--- criu/cgroup.c
+++ criu/cgroup.c
@@ -749,6 +749,7 @@ static int collect_cgroups(struct list_head *ctls)
 
 		snprintf(path + path_pref_len, PATH_MAX - path_pref_len, "%s", root);
 
+		pr_info("Walking %s for %s\n", path, cc->name);
 		ret = ftw(path, add_cgroup, 4);
 
 		if (ret < 0)
@@ -2368,7 +2369,8 @@ static int rewrite_cgroup_roots(CgroupEntry *cge)
 			for (j = 0; j < ctrl->n_dirs; j++) {
 				CgroupDirEntry *cgde = ctrl->dirs[j];
 
-				pr_info("rewriting %s to %s\n", cgde->dir_name, newroot);
+				BUG_ON(!ctrl->n_cnames);
+				pr_info("rewriting [%s] %s to %s\n", ctrl->cnames[0], cgde->dir_name, newroot);
 				if (rewrite_cgsets(cge, ctrl->cnames, ctrl->n_cnames, &cgde->dir_name, newroot))
 					return -1;
 			}
