OpenVZ/vzcriu

commit 51e9685fb69f14b69cef6c0351e5964056b86551

mount: add plain mountpoint for cr_time mount
mnt: Add cr-time mountpoint /tmp/.criu.mntns.UQIroZ/binfmt_misc with parent /tmp/.criu.mntns.UQIroZ(0)
mnt: Start with 0:(null)
mnt-v2: Create plain mountpoint (null) for 0
Error (criu/mount-v2.c:528): mnt-v2: Unable to mkdir mountpoint (null): Bad address

https://jira.sw.ru/browse/PSBM-96757

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -1768,6 +1768,7 @@ struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, cha
 {
 	struct mount_info *mi, *t, *parent;
 	bool add_slash = false;
+	char path[PATH_MAX];
 	int len;
 
 	mi = mnt_entry_alloc(rst);
@@ -1789,6 +1790,8 @@ struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, cha
 		sprintf(mi->mountpoint, "%s%s", root->mountpoint, path);
 	else
 		sprintf(mi->mountpoint, "%s/%s", root->mountpoint, path);
+	snprintf(path, sizeof(path), "%s/mnt-crtime", mnt_roots);
+	mi->plain_mountpoint = xstrdup(path);
 	mi->mnt_id = HELPER_MNT_ID;
 	mi->flags = mi->sb_flags = 0;
 	mi->root = xstrdup("/");

commit 661d2b3d0835ad04add53852bdfce73c3eca033c

mount: don't add a plain mountpoint to cr_time mount on dump
(16.279783) Error (criu/mount.c:1658): mnt: Unable to open
./proc/sys/fs/binfmt_misc(0): No such file or directory

Function service_mountpoint() on dump should return mi->mountpoint for
all mounts. Before fix it returned irrelevant (and inexisting on dump)
plain mountpoint for our special binfmtmisc mount. And thus binfmtmisc
mount was put in one place in the mountinfo tree but mounted in another
place. So open_mountpoint can't lookup it and fails.

https://jira.sw.ru/browse/PSBM-107632

Fixes: 51e9685fb ("mount: add plain mountpoint for cr_time mount")
Reviewed-by: Alexander Mikhalitsyn (Virtuozzo) <alexander@mihalicyn.com>
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -1789,8 +1789,10 @@ struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, cha
 		sprintf(mi->mountpoint, "%s%s", root->mountpoint, path);
 	else
 		sprintf(mi->mountpoint, "%s/%s", root->mountpoint, path);
-	snprintf(plain_mountpoint, sizeof(plain_mountpoint), "%s/mnt-crtime", mnt_roots);
-	mi->plain_mountpoint = xstrdup(plain_mountpoint);
+	if (rst) {
+		snprintf(plain_mountpoint, sizeof(plain_mountpoint), "%s/mnt-crtime", mnt_roots);
+		mi->plain_mountpoint = xstrdup(plain_mountpoint);
+	}
 	mi->mnt_id = HELPER_MNT_ID;
 	mi->flags = mi->sb_flags = 0;
 	mi->root = xstrdup("/");