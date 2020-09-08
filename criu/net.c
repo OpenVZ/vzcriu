OpenVZ/vzcriu

commit b7e0724f095da04ade4fe51f6d233d53c43f7043

net: add vxlan interfaces support
https://jira.sw.ru/browse/PSBM-105843

Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>

criu/net.c
@@ -88,6 +88,12 @@ enum {
 };
 #define IFLA_IPTUN_MAX  (__IFLA_IPTUN_MAX - 1)
 
+#ifndef IFLA_VXLAN_DF
+#define IFLA_VXLAN_DF 29
+#undef IFLA_VXLAN_MAX
+#define IFLA_VXLAN_MAX IFLA_VXLAN_DF
+#endif
+
 static int ns_sysfs_fd = -1;
 
 int read_ns_sys_file(char *path, char *buf, int len)
@@ -669,6 +675,96 @@ static int dump_macvlan(NetDeviceEntry *nde, struct cr_imgset *imgset, struct nl
 	return write_netdev_img(nde, imgset, info);
 }
 
+static int dump_vxlan(NetDeviceEntry *nde, struct cr_imgset *imgset, struct nlattr **info)
+{
+	VxlanLinkEntry vxlan = VXLAN_LINK_ENTRY__INIT;
+	int ret;
+	struct nlattr *data[IFLA_VXLAN_MAX+1];
+
+	if (!info || !info[IFLA_INFO_DATA]) {
+		pr_err("no data for vxlan\n");
+		return -1;
+	}
+
+	ret = nla_parse_nested(data, IFLA_VXLAN_MAX, info[IFLA_INFO_DATA], NULL);
+	if (ret < 0) {
+		pr_err("failed ot parse vxlan data\n");
+		return -1;
+	}
+
+#define ENCODE_ENTRY(__type, __ifla, __proto) \
+	do {									\
+		if (data[__ifla]) {						\
+			vxlan.__proto = *(__type *)nla_data(data[__ifla]);	\
+			vxlan.has_##__proto = true;				\
+		}								\
+	} while (0)
+
+	BUG_ON(!data[IFLA_VXLAN_ID]);
+	vxlan.id = *((u32 *)RTA_DATA(data[IFLA_VXLAN_ID]));
+
+	if (data[IFLA_VXLAN_GROUP]) {
+		BUG_ON(nla_len(data[IFLA_VXLAN_GROUP]) != 4);
+		vxlan.n_group = 1;
+		vxlan.group = nla_data(data[IFLA_VXLAN_GROUP]);
+	}
+
+	if (data[IFLA_VXLAN_GROUP6]) {
+		BUG_ON(nla_len(data[IFLA_VXLAN_GROUP6]) != 16);
+		vxlan.n_group6 = 4;
+		vxlan.group6 = nla_data(data[IFLA_VXLAN_GROUP6]);
+	}
+
+	ENCODE_ENTRY(u32, IFLA_VXLAN_LINK, link);
+
+	if (data[IFLA_VXLAN_LOCAL]) {
+		BUG_ON(nla_len(data[IFLA_VXLAN_LOCAL]) != 4);
+		vxlan.n_local = 1;
+		vxlan.local = nla_data(data[IFLA_VXLAN_LOCAL]);
+	}
+
+	if (data[IFLA_VXLAN_LOCAL6]) {
+		BUG_ON(nla_len(data[IFLA_VXLAN_LOCAL6]) != 16);
+		vxlan.n_local6 = 4;
+		vxlan.local6 = nla_data(data[IFLA_VXLAN_LOCAL6]);
+	}
+
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_TOS,      tos);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_TTL,      ttl);
+	ENCODE_ENTRY(u32, IFLA_VXLAN_LABEL,    label);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_LEARNING, learning);
+	ENCODE_ENTRY(u32, IFLA_VXLAN_AGEING,   ageing);
+	ENCODE_ENTRY(u32, IFLA_VXLAN_LIMIT,    limit);
+
+	if (data[IFLA_VXLAN_PORT_RANGE]) {
+		vxlan.port_range.data = nla_data(data[IFLA_VXLAN_PORT_RANGE]);
+		vxlan.port_range.len = nla_len(data[IFLA_VXLAN_PORT_RANGE]);
+	}
+
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_PROXY,             proxy);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_RSC,               rsc);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_L2MISS,            l2miss);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_L3MISS,            l3miss);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_COLLECT_METADATA,  collect_metadata);
+	ENCODE_ENTRY(u16, IFLA_VXLAN_PORT,              port);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_UDP_CSUM,          udp_csum);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_UDP_ZERO_CSUM6_TX, udp_zero_csum6_tx);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_UDP_ZERO_CSUM6_RX, udp_zero_csum6_rx);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_REMCSUM_TX,        remcsum_tx);
+	ENCODE_ENTRY(u8,  IFLA_VXLAN_REMCSUM_RX,        remcsum_rx);
+
+	ENCODE_ENTRY(u8, IFLA_VXLAN_GBP,               gbp);
+	ENCODE_ENTRY(u8, IFLA_VXLAN_GPE,               gpe);
+	ENCODE_ENTRY(u8, IFLA_VXLAN_REMCSUM_NOPARTIAL, remcsum_nopartial);
+	ENCODE_ENTRY(u8, IFLA_VXLAN_TTL_INHERIT,       ttl_inherit);
+	ENCODE_ENTRY(u8, IFLA_VXLAN_DF,                df);
+
+#undef ENCODE_ENTRY
+
+	nde->vz_vxlan = &vxlan;
+	return write_netdev_img(nde, imgset, info);
+}
+
 static int dump_one_ethernet(struct ifinfomsg *ifi, char *kind,
 		struct nlattr **tb, struct ns_id *ns, struct cr_imgset *fds)
 {
@@ -703,6 +799,8 @@ static int dump_one_ethernet(struct ifinfomsg *ifi, char *kind,
 	}
 	if (!strcmp(kind, "macvlan"))
 		return dump_one_netdev(ND_TYPE__MACVLAN, ifi, tb, ns, fds, dump_macvlan);
+	if (!strcmp(kind, "vxlan"))
+		return dump_one_netdev(ND_TYPE__VZ_VXLAN, ifi, tb, ns, fds, dump_vxlan);
 
 	return dump_unknown_device(ifi, kind, tb, ns, fds);
 }
@@ -1351,6 +1449,83 @@ static int venet_link_info(struct ns_id *ns, struct net_link *link, struct newli
 	return 0;
 }
 
+static int vxlan_link_info(struct ns_id *ns, struct net_link *link, struct newlink_req *req)
+{
+	struct rtattr *vxlan_data;
+	NetDeviceEntry *nde = link->nde;
+	VxlanLinkEntry *vxlan = nde->vz_vxlan;
+
+	if (!vxlan) {
+		pr_err("Missing vxlan link entry %d\n", nde->ifindex);
+		return -1;
+	}
+
+	addattr_l(&req->h, sizeof(*req), IFLA_INFO_KIND, "vxlan", 5);
+
+	vxlan_data = NLMSG_TAIL(&req->h);
+	addattr_l(&req->h, sizeof(*req), IFLA_INFO_DATA, NULL, 0);
+
+#define DECODE_ENTRY(__type, __ifla, __proto) \
+		do {								\
+			__type aux;						\
+			if (vxlan->has_##__proto) {				\
+				aux = vxlan->__proto;				\
+				addattr_l(&req->h, sizeof(*req), __ifla,	\
+						&aux, sizeof(__type));		\
+			}							\
+		} while (0)
+
+	addattr_l(&req->h, sizeof(*req), IFLA_VXLAN_ID, &vxlan->id, sizeof(uint32_t));
+
+	if (vxlan->n_group)
+		addattr_l(&req->h, sizeof(*req), IFLA_VXLAN_GROUP, vxlan->group, sizeof(uint32_t) * vxlan->n_group);
+
+	if (vxlan->n_group6)
+		addattr_l(&req->h, sizeof(*req), IFLA_VXLAN_GROUP6, vxlan->group6, sizeof(uint32_t) * vxlan->n_group6);
+
+	DECODE_ENTRY(u32, IFLA_VXLAN_LINK, link);
+
+	if (vxlan->n_local)
+		addattr_l(&req->h, sizeof(*req), IFLA_VXLAN_LOCAL, vxlan->local, sizeof(uint32_t) * vxlan->n_local);
+
+	if (vxlan->n_local6)
+		addattr_l(&req->h, sizeof(*req), IFLA_VXLAN_LOCAL6, vxlan->local6, sizeof(uint32_t) * vxlan->n_local6);
+
+	DECODE_ENTRY(u8,  IFLA_VXLAN_TOS,      tos);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_TTL,      ttl);
+	DECODE_ENTRY(u32, IFLA_VXLAN_LABEL,    label);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_LEARNING, learning);
+	DECODE_ENTRY(u32, IFLA_VXLAN_AGEING,   ageing);
+	DECODE_ENTRY(u32, IFLA_VXLAN_LIMIT,    limit);
+
+	if (vxlan->has_port_range)
+		addattr_l(&req->h, sizeof(*req), IFLA_VXLAN_PORT_RANGE, vxlan->port_range.data, vxlan->port_range.len);
+
+	DECODE_ENTRY(u8,  IFLA_VXLAN_PROXY,             proxy);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_RSC,               rsc);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_L2MISS,            l2miss);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_L3MISS,            l3miss);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_COLLECT_METADATA,  collect_metadata);
+	DECODE_ENTRY(u16, IFLA_VXLAN_PORT,              port);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_UDP_CSUM,          udp_csum);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_UDP_ZERO_CSUM6_TX, udp_zero_csum6_tx);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_UDP_ZERO_CSUM6_RX, udp_zero_csum6_rx);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_REMCSUM_TX,        remcsum_tx);
+	DECODE_ENTRY(u8,  IFLA_VXLAN_REMCSUM_RX,        remcsum_rx);
+
+	DECODE_ENTRY(u8, IFLA_VXLAN_GBP,               gbp);
+	DECODE_ENTRY(u8, IFLA_VXLAN_GPE,               gpe);
+	DECODE_ENTRY(u8, IFLA_VXLAN_REMCSUM_NOPARTIAL, remcsum_nopartial);
+	DECODE_ENTRY(u8, IFLA_VXLAN_TTL_INHERIT,       ttl_inherit);
+	DECODE_ENTRY(u8, IFLA_VXLAN_DF,                df);
+
+#undef DECODE_ENTRY
+
+	vxlan_data->rta_len = (void *)NLMSG_TAIL(&req->h) - (void *)vxlan_data;
+
+	return 0;
+}
+
 static int bridge_link_info(struct ns_id *ns, struct net_link *link, struct newlink_req *req)
 {
 	struct rtattr *bridge_data;
@@ -1593,6 +1768,8 @@ static int __restore_link(struct ns_id *ns, struct net_link *link, int nlsk)
 		return restore_one_link(ns, link, nlsk, bridge_link_info, NULL);
 	case ND_TYPE__MACVLAN:
 		return restore_one_macvlan(ns, link, nlsk);
+	case ND_TYPE__VZ_VXLAN:
+		return restore_one_link(ns, link, nlsk, vxlan_link_info, NULL);
 	case ND_TYPE__SIT:
 		return restore_one_link(ns, link, nlsk, sit_link_info, NULL);
 	default:
@@ -1725,6 +1902,27 @@ static int __restore_links(struct ns_id *nsid, int *nrlinks, int *nrcreated)
 			if (!mlink->created) {
 				pr_debug("The master %d:%d:%s isn't created yet",
 					nsid->id, mlink->nde->ifindex, mlink->nde->name);
+				continue;
+			}
+		}
+
+		/*
+		 * vxlan link may have IFLA_VXLAN_LINK set. In that field stored
+		 * ifindex of the interface which will be used for transmitting
+		 * vxlan UDP traffic. We should restore this interface at first.
+		 */
+		if (link->nde->type == ND_TYPE__VZ_VXLAN && link->nde->vz_vxlan->has_link) {
+			uint32_t idx = link->nde->vz_vxlan->link;
+			mlink = lookup_net_link(nsid, idx);
+			if (mlink == NULL) {
+				pr_err("Unable to find the interface with ifindex %d\n", idx);
+				return -1;
+			}
+
+			if (!mlink->created) {
+				pr_debug("The iface %d:%d:%s isn't created yet for vxlan %d:%d:%s",
+					nsid->id, mlink->nde->ifindex, mlink->nde->name,
+					nsid->id, link->nde->ifindex, link->nde->name);
 				continue;
 			}
 		}

images/Makefile
@@ -62,6 +62,7 @@ proto-obj-y	+= sysctl.o
 proto-obj-y	+= autofs.o
 proto-obj-y	+= macvlan.o
 proto-obj-y	+= sit.o
+proto-obj-y	+= vxlan.o
 
 CFLAGS		+= -iquote $(obj)/
 

images/netdev.proto
@@ -5,6 +5,7 @@ import "opts.proto";
 import "tun.proto";
 import "sysctl.proto";
 import "sit.proto";
+import "vxlan.proto";
 
 enum nd_type {
 	LOOPBACK	= 1;
@@ -21,6 +22,7 @@ enum nd_type {
 	BRIDGE		= 6;
 	MACVLAN		= 7;
 	SIT		= 8;
+	VZ_VXLAN	= 1001;
 }
 
 message net_device_entry {
@@ -46,6 +48,7 @@ message net_device_entry {
 	optional uint32 peer_nsid	= 13;
 	optional uint32 master		= 14;
 	optional sit_entry sit		= 15;
+	optional vxlan_link_entry vz_vxlan	= 1001;
 }
 
 message netns_id {

images/vxlan.proto
@@ -1,0 +1,35 @@
+syntax = "proto2";
+
+import "opts.proto";
+
+message vxlan_link_entry {
+	required uint32	id			= 1;
+	repeated uint32 group			= 2 [(criu).ipadd = true];
+	repeated uint32 group6			= 3 [(criu).ipadd = true];
+	optional uint32 link			= 4;
+	repeated uint32 local			= 5 [(criu).ipadd = true];
+	repeated uint32 local6			= 6 [(criu).ipadd = true];
+	optional uint32 tos			= 7;
+	optional uint32 ttl			= 8;
+	optional uint32 label			= 9;
+	optional uint32 learning		= 10;
+	optional uint32 ageing			= 11;
+	optional uint32 limit			= 12;
+	optional bytes port_range		= 13;
+	optional uint32 proxy			= 14;
+	optional uint32 rsc			= 15;
+	optional uint32 l2miss			= 16;
+	optional uint32 l3miss			= 17;
+	optional uint32 collect_metadata	= 18;
+	optional uint32 port			= 19;
+	optional uint32 udp_csum		= 20;
+	optional uint32 udp_zero_csum6_tx	= 21;
+	optional uint32 udp_zero_csum6_rx	= 22;
+	optional uint32 remcsum_tx		= 23;
+	optional uint32 remcsum_rx		= 24;
+	optional bool gbp			= 25;
+	optional bool gpe			= 26;
+	optional bool remcsum_nopartial		= 27;
+	optional bool ttl_inherit		= 28;
+	optional uint32 df			= 29;
+}

commit 3eb4561a454d3600a1c50995c250a648c441c02d

vxlan: fix excess call to restore_master_link in __restore_links
We should not re-use mlink variable for vxlan as it triggers
restore_master_link() call even if vxlan has no master. Which adds unexpected
RTM_SETLINK with possibly zero IFLA_MASTER.

Fixes: b7e0724f0 ("net: add vxlan interfaces support")

Reviewed-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/net.c
@@ -2401,15 +2401,17 @@ static int __restore_links(struct ns_id *nsid, int *nrlinks, int *nrcreated)
 		 */
 		if (link->nde->type == ND_TYPE__VZ_VXLAN && link->nde->vz_vxlan->has_link) {
 			uint32_t idx = link->nde->vz_vxlan->link;
-			mlink = lookup_net_link(nsid, idx);
-			if (mlink == NULL) {
+			struct net_link *tlink;
+
+			tlink = lookup_net_link(nsid, idx);
+			if (tlink == NULL) {
 				pr_err("Unable to find the interface with ifindex %d\n", idx);
 				return -1;
 			}
 
-			if (!mlink->created) {
+			if (!tlink->created) {
 				pr_debug("The iface %d:%d:%s isn't created yet for vxlan %d:%d:%s",
-					nsid->id, mlink->nde->ifindex, mlink->nde->name,
+					nsid->id, tlink->nde->ifindex, tlink->nde->name,
 					nsid->id, link->nde->ifindex, link->nde->name);
 				continue;
 			}