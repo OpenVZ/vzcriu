OpenVZ/vzcriu

commit 9ad02d24525fb5ea166a39727bb28c2c826ddd64

freezer: added cmd-line option to skip 'freezer.state' dump/restore
During 'vzctl snapshot CT' command execution vzctl first freezes
container, then spawnes criu with 'dump' command. Because container
is in FROZEN state, criu needs --freeze-cgroup flag to call
freeze_processes, which will unfreeze it back to be able seize to
container processes. The same --freeze-cgroup flag is later used
to determine if freezer state should be dumped to image.
In case of snapshot it is impossible to dump orignal state of root
container's freeze cgroup due to the fact that it has already been
overwritten by vzctl and it's in FROZEN state.
Because of that at snapshot-switch this FROZEN state will be restored
by criu and will lead to hang of any processess joining that container.
This problem is new because at snapshot-switch criu did not restore cgroup
properties by default and thus "FROZEN" freezer.state in dump image was
ignored. But due to https://jira.sw.ru/browse/PSBM-106413 criu now
restores properties and so it has to know weather or not it should
handle freezer state. For common case we could only add flag to not dump
freezer state during criu dump. But for compatibility of older snapshots
that dumped FROZEN states before this fix we also need flag at criu
restore that tells criu to ignore freezer.state property from dump
image.

https://jira.sw.ru/browse/PSBM-107590
Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>

criu/seize.c
@@ -582,6 +582,20 @@ again:
 	if (state == thawed) {
 		freezer_thawed = true;
 
+		/*
+		 * Option skip_freezer_state provides guarantee that
+		 * parent process/caller has freezed the container before
+		 * spawning criu, in that case criu doesn't have to freeze it.
+		 * We can check it here by checking the value of a freezer
+		 * cgroup state.
+		 */
+		if (opts.skip_freezer_state) {
+			pr_err("Freezer state mismatch: expected '%s'\n",
+				frozen);
+			close(fd);
+			return -1;
+		}
+
 		if (set_freezer_state(fd, frozen, sizeof(frozen))) {
 			pr_err("Unable to freeze tasks\n");
 			close(fd);

criu/cgroup.c
@@ -728,7 +728,8 @@ static int collect_cgroups(struct list_head *ctls)
 		if (ret < 0)
 			return ret;
 
-		if (opts.freeze_cgroup && !strcmp(cc->name, "freezer") &&
+		if (opts.freeze_cgroup && !opts.skip_freezer_state &&
+				!strcmp(cc->name, "freezer") &&
 				add_freezer_state(current_controller))
 			return -1;
 	}
@@ -1502,7 +1503,8 @@ static int prepare_cgroup_dir_properties(char *path, int off, CgroupDirEntry **e
 			CgroupPropEntry *p = e->properties[j];
 
 			if (!strcmp(p->name, "freezer.state")) {
-				add_freezer_state_for_restore(p, path, off2);
+				if (!opts.skip_freezer_state)
+					add_freezer_state_for_restore(p, path, off2);
 				continue; /* skip restore now */
 			}
 

criu/config.c
@@ -510,6 +510,7 @@ int parse_options(int argc, char **argv, bool *usage_error,
 		{ "no-default-config",		no_argument,		0, 1090},
 		BOOL_OPT("check-mounts", &opts.check_mounts),
 		BOOL_OPT("mounts-compat", &opts.mounts_compat),
+		BOOL_OPT("skip-freezer-state",&opts.skip_freezer_state),
 		{ },
 	};
 

criu/include/cr_options.h
@@ -86,6 +86,7 @@ struct cr_options {
 	char			*root;
 	char			*pidfile;
 	char			*freeze_cgroup;
+	int			skip_freezer_state;
 	struct list_head	ext_mounts;
 	struct list_head	inherit_fds;
 	struct list_head	external;

commit 5798876341f151e9d62d7ee8bca8f8f405ebf8b2

criu/dump: remove freezer.state == 'FROZEN' assertion
This partially reverts strict check in freeze_processes, because by
current design decision --skip-freezer-state will always be present in
vz-cpt script, thus criu dump will always have this flag, when called
from vzctl. vzctl resume will provide --freeze-cgroup= option, but in
the same time freezer will have freezer.state set to "THAWED". In this
case we do not want criu to exit with error.

Fixes: 9ad02d24525fb5ea166a39727bb28c2c826ddd64
https://jira.sw.ru/browse/PSBM-107590

Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>

criu/seize.c
@@ -582,20 +582,6 @@ again:
 	if (state == thawed) {
 		freezer_thawed = true;
 
-		/*
-		 * Option skip_freezer_state provides guarantee that
-		 * parent process/caller has freezed the container before
-		 * spawning criu, in that case criu doesn't have to freeze it.
-		 * We can check it here by checking the value of a freezer
-		 * cgroup state.
-		 */
-		if (opts.skip_freezer_state) {
-			pr_err("Freezer state mismatch: expected '%s'\n",
-				frozen);
-			close(fd);
-			return -1;
-		}
-
 		if (set_freezer_state(fd, frozen, sizeof(frozen))) {
 			pr_err("Unable to freeze tasks\n");
 			close(fd);