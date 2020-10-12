OpenVZ/vzcriu

commit 870a647d5f9c4b7b7ae97b9f57fc62ada09ad2e0

vz7: netlink: restore data in a received queue for netlink sockets
Messages from a receive queue are dumped with help of SO_PEEK_OFF
and restored back with help of NETLINK_REPAIR.

Here is nothin special, we just dump data and queue them back.

https://jira.sw.ru/browse/PSBM-28386

Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

criu/sk-netlink.c
@@ -14,6 +14,7 @@
 #include "libnetlink.h"
 #include "namespaces.h"
 #include "sk-queue.h"
+#include "kerndat.h"
 
 #undef  LOG_PREFIX
 #define LOG_PREFIX "netlink: "
@@ -83,15 +84,19 @@ int netlink_receive_one(struct nlmsghdr *hdr, struct ns_id *ns, void *arg)
 	return sk_collect_one(m->ndiag_ino, PF_NETLINK, &sd->sd, ns);
 }
 
-static bool can_dump_netlink_sk(int lfd)
+static bool can_dump_netlink_sk(int lfd, struct netlink_sk_desc *sk)
 {
 	int ret;
 
 	ret = fd_has_data(lfd);
-	if (ret == 1)
+	if (ret < 0)
+		return false;
+	if (ret == 1 && (sk->nl_flags & NDIAG_FLAG_CB_RUNNING)) {
 		pr_err("The socket has data to read\n");
+		return false;
+	}
 
-	return ret == 0;
+	return true;
 }
 
 static int dump_one_netlink_fd(int lfd, u32 id, const struct fd_parms *p)
@@ -108,7 +113,7 @@ static int dump_one_netlink_fd(int lfd, u32 id, const struct fd_parms *p)
 	ne.id = id;
 	ne.ino = p->stat.st_ino;
 
-	if (!can_dump_netlink_sk(lfd))
+	if (!can_dump_netlink_sk(lfd, sk))
 		goto err;
 
 	if (sk) {
@@ -187,6 +192,9 @@ static int dump_one_netlink_fd(int lfd, u32 id, const struct fd_parms *p)
 	fe.id = ne.id;
 	fe.nlsk = &ne;
 
+	if (kdat.has_nl_repair && dump_sk_queue(lfd, id, true))
+		goto err;
+
 	if (pb_write_one(img_from_set(glob_imgset, CR_FD_FILES), &fe, PB_FILE))
 		goto err;
 
@@ -204,6 +212,29 @@ struct netlink_sock_info {
 	NetlinkSkEntry *nse;
 	struct file_desc d;
 };
+
+static int restore_netlink_queue(int sk, int id)
+{
+	int val;
+
+	if (!kdat.has_nl_repair)
+		return 0;
+
+	val = 1;
+	if (setsockopt(sk, SOL_NETLINK, NETLINK_REPAIR, &val, sizeof(val))) {
+		pr_perror("Unable to set NETLINK_REPAIR");
+		return -1;
+	}
+
+	if (restore_sk_queue(sk, id))
+		return -1;
+
+	val = 0;
+	if (setsockopt(sk, SOL_NETLINK, NETLINK_REPAIR, &val, sizeof(val)))
+		return -1;
+
+	return 0;
+}
 
 static int open_netlink_sk(struct file_desc *d, int *new_fd)
 {
@@ -257,6 +288,9 @@ static int open_netlink_sk(struct file_desc *d, int *new_fd)
 		goto err;
 
 	if (restore_socket_opts(sk, nse->opts))
+		goto err;
+
+	if (restore_netlink_queue(sk, nse->id))
 		goto err;
 
 	*new_fd = sk;

commit 4a1e220d1cadcc74380bced9af6a70d53c9c5564

sk-netlink: fix order between restoring queue and file params
We have path which makes netlink sockets non-blocking to be able to
restore their queue:

  +-> open_netlink_sk
    +-> restore_netlink_queue
      +-> restore_sk_queue
        +-> restore_prepare_socket enable O_NONBLOCK

But we restore file params (including O_NONBLOCK flag) just before that,
this looks like a wrong order. So lets fix it.

Also we don't really dump status flags for netlink sockets, so lets also
do it. In case of old image now we would restore all sockets with zero
flags instead of all non-blocking as it was before this patch.

This problem leads to kubernetes weave-net network do die on EAGAIN
returned from socket which is expected to be blocking. And also weave
can't restart, as it can't remove weave and another veth devices as all
it's netlink controlling sockets are broken.

Fixes: 870a647d5f9c ("vz7: netlink: restore data in a received queue for
netlink sockets")
Note: 6ca2cb1a0e88 ("vz7: netlink: restore queues and only then restore
opts")

https://jira.sw.ru/browse/PSBM-120924

Reviewed-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
Reviewed-by: Alexander Mikhalitsyn (Virtuozzo) <alexander@mihalicyn.com>
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/sk-netlink.c
@@ -260,6 +260,7 @@ static int dump_one_netlink_fd(int lfd, u32 id, const struct fd_parms *p)
 	}
 
 	ne.fown = (FownEntry *)&p->fown;
+	ne.flags = p->flags;
 	ne.opts	= &skopts;
 	ne.nl_opts = &nlopts;
 
@@ -383,10 +384,10 @@ static int open_netlink_sk(struct file_desc *d, int *new_fd)
 		}
 	}
 
-	if (rst_file_params(sk, nse->fown, nse->flags))
+	if (restore_netlink_queue(sk, nse->id))
 		goto err;
 
-	if (restore_netlink_queue(sk, nse->id))
+	if (rst_file_params(sk, nse->fown, nse->flags))
 		goto err;
 
 	if (nse->nl_opts && restore_nl_opts(sk, nse->nl_opts))