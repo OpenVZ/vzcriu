74f4d70fdc8bd910cfc2e1c03a7c37d0ac37ae58
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-10-12 11:28:54 +0300

    zdtm: add new sk-netlink-flags test
    This checks that blocking sockets remain blocking after c/r.
    
    https://jira.sw.ru/browse/PSBM-120924
    Reviewed-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
    Reviewed-by: Alexander Mikhalitsyn (Virtuozzo) <alexander@mihalicyn.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Netlink packet queue

---
+++ test/zdtm/static/sk-netlink-flags.c
@@ -0,0 +1,80 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/netlink.h>
+#include <sys/socket.h>
+#include <linux/socket.h>
+
+#include "zdtmtst.h"
+
+#ifndef SOL_NETLINK
+#define SOL_NETLINK 270
+#endif
+
+const char *test_doc = "Check that blocking sockets remain blocking";
+const char *test_author = "Pavel Tikhomirov <ptikhomirov@virtuozzo.com>";
+
+int main(int argc, char **argv)
+{
+	int sk, skn = -1, ret;
+
+	test_init(argc, argv);
+
+	sk = socket(PF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+	if (sk < 0) {
+		pr_perror("Can't create generic raw socket");
+		return 1;
+	}
+
+	if (fcntl(sk, F_SETFL, 0) == -1) {
+		pr_perror("Can't drop file status flags");
+		goto err;
+	}
+
+	skn = socket(PF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+	if (skn < 0) {
+		pr_perror("Can't create generic raw socket");
+		goto err;
+	}
+
+	if (fcntl(skn, F_SETFL, O_NONBLOCK) == -1) {
+		pr_perror("Can't set O_NONBLOCK");
+		goto err;
+	}
+
+	test_daemon();
+	test_waitsig();
+
+	ret = fcntl(sk, F_GETFL, 0);
+	if (ret == -1) {
+		pr_perror("Can't get status flags for %d", sk);
+		goto err;
+	}
+
+	if (ret & O_NONBLOCK) {
+		fail("Unexpected O_NONBLOCK for %d", sk);
+		goto err;
+	}
+
+	ret = fcntl(skn, F_GETFL, 0);
+	if (ret == -1) {
+		pr_perror("Can't get status flags for %d", skn);
+		goto err;
+	}
+
+	if (!(ret & O_NONBLOCK)) {
+		fail("%d should have O_NONBLOCK", skn);
+		goto err;
+	}
+
+	close(sk);
+	close(skn);
+	pass();
+
+	return 0;
+err:
+	if (sk >= 0)
+		close(sk);
+	if (skn >= 0)
+		close(skn);
+	return 1;
+}
