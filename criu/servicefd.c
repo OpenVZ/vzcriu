OpenVZ/vzcriu

commit 18a6a782c036f2bd0bec618c6ac74e73ed7d2ebb

servicefd: fully cleanup parent's shared fd table on service fd update
Before this patch while moving parent's service fds to a new place we
didn't close what previously was in this old place, this way if we try
to open some service fd which has this undesired open fd lefover in
it's place we will get "busy target" error from install_service_fd.

It's a preparation to make /proc/self to be a service fd, if we do so
without this patch the problem with busy target is triggered on zdtm.

https://jira.sw.ru/browse/PSBM-121533

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/servicefd.c
@@ -206,11 +206,22 @@ int close_service_fd(enum sfd_type type)
 	return 0;
 }
 
-static int move_service_fd(struct pstree_item *me, int type, int new_id, int new_base)
+static int move_service_fd(struct pstree_item *me, int type, int new_id,
+			   int new_base, bool cleanup_parent_sfdt)
 {
 	int old = get_service_fd(type);
 	int new = new_base - type - SERVICE_FD_MAX * new_id;
 	int ret;
+
+	/**
+	 * When our parent has shared fd table, we've inherited service fds of
+	 * other tasks from this fd table along with parent's service fds, so
+	 * those other copies of service fds can be open within an area we move
+	 * the copy of our parent's service fds. So we need to close all fds in
+	 * new service fd area.
+	 */
+	if (cleanup_parent_sfdt)
+		close(new);
 
 	if (old < 0)
 		return 0;
@@ -281,7 +292,7 @@ static int choose_service_fd_base(struct pstree_item *me)
 	return nr;
 }
 
-int clone_service_fd(struct pstree_item *me)
+int clone_service_fd(struct pstree_item *me, bool cleanup_parent_sfdt)
 {
 	int id, new_base, i, ret = -1;
 
@@ -296,10 +307,12 @@ int clone_service_fd(struct pstree_item *me)
 	/* Dup sfds in memmove() style: they may overlap */
 	if (get_service_fd(LOG_FD_OFF) < new_base - LOG_FD_OFF - SERVICE_FD_MAX * id)
 		for (i = SERVICE_FD_MIN + 1; i < SERVICE_FD_MAX; i++)
-			move_service_fd(me, i, id, new_base);
+			move_service_fd(me, i, id, new_base,
+					cleanup_parent_sfdt);
 	else
 		for (i = SERVICE_FD_MAX - 1; i > SERVICE_FD_MIN; i--)
-			move_service_fd(me, i, id, new_base);
+			move_service_fd(me, i, id, new_base,
+					cleanup_parent_sfdt);
 
 	service_fd_base = new_base;
 	service_fd_id = id;

criu/cr-restore.c
@@ -1107,21 +1107,22 @@ static int restore_one_zombie(CoreEntry *core)
 
 static int setup_newborn_fds(struct pstree_item *me)
 {
-	if (clone_service_fd(me))
+	bool cleanup_parent_sfdt = !me->parent ||
+				   (rsti(me->parent)->fdt &&
+				    !(rsti(me)->clone_flags & CLONE_FILES));
+
+	if (clone_service_fd(me, cleanup_parent_sfdt))
 		return -1;
 
-	if (!me->parent ||
-	    (rsti(me->parent)->fdt && !(rsti(me)->clone_flags & CLONE_FILES))) {
-		/*
-		 * When our parent has shared fd table, some of the table owners
-		 * may be already created. Files, they open, will be inherited
-		 * by current process, and here we close them. Also, service fds
-		 * of parent are closed here. And root_item closes the files,
-		 * that were inherited from criu process.
-		 */
-		if (close_old_fds())
-			return -1;
-	}
+	/*
+	 * When our parent has shared fd table, some of the table owners
+	 * may be already created. Files, they open, will be inherited
+	 * by current process, and here we close them. Also, service fds
+	 * of parent are closed here. And root_item closes the files,
+	 * that were inherited from criu process.
+	 */
+	if (cleanup_parent_sfdt && close_old_fds())
+		return -1;
 
 	return 0;
 }

criu/include/servicefd.h
@@ -44,6 +44,6 @@ extern bool is_service_fd(int fd, enum sfd_type type);
 extern int service_fd_min_fd(struct pstree_item *item);
 extern int install_service_fd(enum sfd_type type, int fd);
 extern int close_service_fd(enum sfd_type type);
-extern int clone_service_fd(struct pstree_item *me);
+extern int clone_service_fd(struct pstree_item *me, bool cleanup_parent_sfdt);
 
 #endif /* __CR_SERVICE_FD_H__ */

commit aea726be6c52d4fe80f0845ae26f9b8e924e91c3

servicefd: don't cleanup new fd in case it is same as old one
Let's be overprotective and improve checks in two places:

1) Improve the check to skip moving service fds in clone_service_fd
becaues we don't need to move anything if old service fds resulting
offset is the same as new service fds resulting offset. It saves us from
excess calls to dup/fcntl(F_DUPFD).

2) Improve check in move_service_fd to not to close "new" fd to free
space in fdtable for actual new fd, in case new == old. It saves us from
errors like "Error (criu/servicefd.c:236): sfd: FDSTORE_SK_OFF unable
to clone 114->114: Bad file descriptor" as old is closed.

https://jira.sw.ru/browse/PSBM-122957

Fixes: 18a6a782c ("servicefd: fully cleanup parent's shared fd table on
service fd update")
Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/servicefd.c
@@ -221,7 +221,7 @@ static int move_service_fd(struct pstree_item *me, int type, int new_id,
 	 * the copy of our parent's service fds. So we need to close all fds in
 	 * new service fd area.
 	 */
-	if (cleanup_parent_sfdt)
+	if (cleanup_parent_sfdt && new != old)
 		close(new);
 
 	if (old < 0)
@@ -302,7 +302,8 @@ int clone_service_fd(struct pstree_item *me, bool cleanup_parent_sfdt)
 
 	if (new_base == -1)
 		return -1;
-	if (service_fd_base == new_base && service_fd_id == id)
+
+	if (get_service_fd(LOG_FD_OFF) == new_base - LOG_FD_OFF - SERVICE_FD_MAX * id)
 		return 0;
 
 	/* Dup sfds in memmove() style: they may overlap */