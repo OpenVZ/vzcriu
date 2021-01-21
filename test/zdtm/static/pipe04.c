3c01ce458e1ba57947aa20a4e05a5cbd3935e2d4
Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
2021-01-21 18:19:46 +0300

    zdtm: adjust pipe count at pipe04 test
    In some cases hard limit for opened files is not enough for 2048 pipes in
    pipe04 test. Adjust number of pipes automatically.
    
    https://jira.sw.ru/browse/PSBM-122418
    Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>

--- test/zdtm/static/pipe04.c
+++ test/zdtm/static/pipe04.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <linux/limits.h>
+#include <sys/resource.h>
 
 #include "zdtmtst.h"
 
@@ -15,17 +16,40 @@ const char *test_author	= "Cyrill Gorcunov <gorcunov@openvz.org>";
 
 #define TEST_STRING "Hello world"
 #define NR_PIPES 2048
+#define FD_MAX (NR_PIPES * 2 + 16)
 
 int main(int argc, char ** argv)
 {
 	int pfd[NR_PIPES][2];
+	struct rlimit lim;
 	uint8_t buf[256];
-	int ret, size;
+	int ret, size, pipe_count = NR_PIPES;
 	size_t i;
 
 	test_init(argc, argv);
 
-	for (i = 0; i < NR_PIPES; i++) {
+	if (getrlimit(RLIMIT_NOFILE, &lim) < 0) {
+		pr_perror("Unable to getrlimit");
+		return 1;
+	}
+
+	if (lim.rlim_cur < FD_MAX) {
+		test_msg("Unsufficient soft file descriptor rlimit to run original test. Trying to increase limit or lower pipe count.\n");
+
+		if (lim.rlim_cur < lim.rlim_max) {
+			rlim_t old_lim = lim.rlim_cur;
+			lim.rlim_cur = lim.rlim_max;
+			if (setrlimit(RLIMIT_NOFILE, &lim) < 0) {
+				test_msg("Unable to raise fd limit from softlim to hardlim\n");
+				lim.rlim_cur = old_lim;
+			}
+		}
+
+		pipe_count = (FD_MAX < lim.rlim_cur) ? NR_PIPES : (lim.rlim_cur - 16) / 2;
+		test_msg("Set pipe count to %d\n", pipe_count);
+	}
+
+	for (i = 0; i < pipe_count; i++) {
 		ret = pipe(pfd[i]);
 		if (ret < 0) {
 			pr_perror("Can't create pipe %zu", i);
@@ -50,7 +74,7 @@ int main(int argc, char ** argv)
 	test_daemon();
 	test_waitsig();
 
-	for (i = 0; i < NR_PIPES; i++) {
+	for (i = 0; i < pipe_count; i++) {
 		size = 0;
 		while (size < sizeof(buf)) {
 			ret = read(pfd[i][0], buf, sizeof(buf));
