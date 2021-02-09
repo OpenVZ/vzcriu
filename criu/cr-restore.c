daa0bc34c77423926d0adf74df1d91d3605c34a1
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-02-09 18:48:19 +0300

    devices/restore: prepare devices_list with all devices from images
    Let's load info about devices map on restore.
    
    Note: if there is no device image it would just create an empty
    devices_list for backward compatibility.
    
    https://jira.sw.ru/browse/PSBM-123668
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Alow external devices to change major/minor

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -79,6 +79,7 @@
 #include "timens.h"
 #include "bpfmap.h"
 #include "apparmor.h"
+#include "devices.h"
 
 #include "parasite-syscall.h"
 #include "files-reg.h"
@@ -254,6 +255,9 @@ static int crtools_prepare_shared(void)
 		return -1;
 
 	if (prepare_apparmor_namespaces())
+		return -1;
+
+	if (prepare_devices())
 		return -1;
 
 	return 0;
