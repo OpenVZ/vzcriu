OpenVZ/vzcriu

commit 4ab1a32cfca80a4bcd3b1322c8dc1d87155f8c91

cgroup/restore: split prepare_task_cgroup code into two separate functions.
Right now prepare_task_cgroup performs two logically separated
operations on cgroups. First, if root task is passed as an argument, it
restores the whole cgroup namespace and puts the root task there.
Second, it puts any specified task into it's particular cgoup set,
which info is recovered from the dump image.
Because two of this operations are made inside one function they are
forced to follow one another and after cgroup namespace had been
introduced in VZ8, it has become a problem. Right now the logical order
is:
  1. start ve
  2. enter cgroup namespace
  3. put init task into restored cgroups.

Starting from VZ8, ve should be started after entering cgroup
namespace, so the order should be:
  2. enter cgroup namespace
  1. start ve
  3. put init task into restored cgroups.

This is because in VZ8 in ve starting code, the kernel looks at the
starting task's nsproxy->cgroup_ns to get information about virtualizing
cgroup roots. In VZ7 it looked at the current cgroups the starting task
was in at start.
To ensure the correct order prepare_task_cgroups has been refactored
into 'prepare_cgroup_namespace' and 'restore_task_cgroup'.

https://jira.sw.ru/browse/PSBM-123431
Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>

Fixed some nits.

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/cgroup.c
@@ -1172,24 +1172,22 @@ static int prepare_cgns(CgSetEntry *se)
 
 	}
 
-	if (do_unshare && unshare(CLONE_NEWCGROUP) < 0) {
-		pr_perror("couldn't unshare cgns");
-		return -1;
+	if (do_unshare) {
+		if (unshare(CLONE_NEWCGROUP) < 0) {
+			pr_perror("couldn't unshare cgns");
+			return -1;
+		}
+		pr_info("Creating new cgroup namespace for a root task\n");
 	}
 
 	return 0;
 }
 
-static int move_in_cgroup(CgSetEntry *se, bool setup_cgns)
+static int move_in_cgroup(CgSetEntry *se)
 {
 	int i;
 
-	pr_info("Move into %d\n", se->id);
-
-	if (setup_cgns && prepare_cgns(se) < 0) {
-		pr_err("failed preparing cgns\n");
-		return -1;
-	}
+	pr_info("Moving into %d\n", se->id);
 
 	for (i = 0; i < se->n_ctls; i++) {
 		char aux[PATH_MAX];
@@ -1230,7 +1228,35 @@ static int move_in_cgroup(CgSetEntry *se, bool setup_cgns)
 	return 0;
 }
 
-int prepare_task_cgroup(struct pstree_item *me)
+int prepare_cgroup_namespace(struct pstree_item *root_task)
+{
+	CgSetEntry *se;
+
+	if (!rsti(root_task)->cg_set) {
+		pr_err("No cg_set found for root task\n");
+		return -1;
+	}
+
+	if (root_task->parent) {
+		pr_err("Expecting root_task to restore cgroup namespace\n");
+		return -1;
+	}
+
+	se = find_rst_set_by_id(rsti(root_task)->cg_set);
+	if (!se) {
+		pr_err("No set %d found\n", rsti(root_task)->cg_set);
+		return -1;
+	}
+
+	if (prepare_cgns(se) < 0) {
+		pr_err("failed preparing cgns\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+int restore_task_cgroup(struct pstree_item *me)
 {
 	struct pstree_item *parent = me->parent;
 	CgSetEntry *se;
@@ -1259,13 +1285,7 @@ int prepare_task_cgroup(struct pstree_item *me)
 		return -1;
 	}
 
-	/* Since don't support nesting of cgroup namespaces, let's only set up
-	 * the cgns (if it exists) in the init task. In the future, we should
-	 * just check that the cgns prefix string matches for all the entries
-	 * in the cgset, and only unshare if that's true.
-	 */
-
-	return move_in_cgroup(se, !me->parent);
+	return move_in_cgroup(se);
 }
 
 void fini_cgroup(void)

criu/cr-restore.c
@@ -2282,6 +2282,18 @@ static int restore_task_with_children(void *_arg)
 		if (restore_finish_ns_stage(CR_STATE_ROOT_TASK, CR_STATE_PREPARE_NAMESPACES) < 0)
 			goto err;
 
+		/*
+		 * VZ8 containers use nsproxy of a starting task to read about
+		 * virtual cgroup roots. task's nsproxy should already have a
+		 * valid cgroup namespace, so we have to enter to this ns before
+		 * start_ve.
+		 *
+		 * Since we don't support nesting of cgroup namespaces, let's
+		 * only set up the cgns in the init task (if it exists).
+		 */
+		if (prepare_cgroup_namespace(current) < 0)
+			goto err;
+
 		/**
 		 * Note: previousely we were starting VE from action
 		 * scripts ACT_SETUP_NS (setup-namespaces), it happened just
@@ -2313,7 +2325,7 @@ static int restore_task_with_children(void *_arg)
 	 * we will only move the root one there, others will
 	 * just have it inherited.
 	 */
-	if (prepare_task_cgroup(current) < 0)
+	if (restore_task_cgroup(current) < 0)
 		goto err;
 
 	/* Restore root task */

criu/include/cgroup.h
@@ -9,7 +9,8 @@ struct parasite_dump_cgroup_args;
 extern u32 root_cg_set;
 int dump_task_cgroup(const struct pstree_item *, u32 *, struct parasite_dump_cgroup_args *args);
 int dump_cgroups(void);
-int prepare_task_cgroup(struct pstree_item *);
+int restore_task_cgroup(struct pstree_item *);
+int prepare_cgroup_namespace(struct pstree_item *);
 int prepare_cgroup(void);
 /* Restore things like cpu_limit in known cgroups. */
 int prepare_cgroup_properties(void);

commit fafbe61a65e0bf6e6afb027edec4123b1884ec87

cgroup: skip cgroup namespace restore when cgset is not dumped
If on dump all dumped tasks are in same cgset with criu we don't dump
cgsets and thus cgroup namespaces and rely that on restore criu would
prepare proper cgset/cgns for us.

This is needed to fix zdtm tests as they mostly run in same cgroups with
criu without any cgroup namespace. We saw errors like this:

  Error (criu/cgroup.c:1239): cg: No set 1 found

Note: This skip is the same to what we do in restore_task_cgroup.

https://jira.sw.ru/browse/PSBM-123431

Fixes: 4ab1a32cf ("cgroup/restore: split prepare_task_cgroup code into
two separate functions.")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/cgroup.c
@@ -1232,14 +1232,19 @@ int prepare_cgroup_namespace(struct pstree_item *root_task)
 {
 	CgSetEntry *se;
 
+	if (root_task->parent) {
+		pr_err("Expecting root_task to restore cgroup namespace\n");
+		return -1;
+	}
+
 	if (!rsti(root_task)->cg_set) {
 		pr_err("No cg_set found for root task\n");
 		return -1;
 	}
 
-	if (root_task->parent) {
-		pr_err("Expecting root_task to restore cgroup namespace\n");
-		return -1;
+	if (rsti(root_task)->cg_set == root_cg_set) {
+		pr_info("Cgroup namespace inherited from parent\n");
+		return 0;
 	}
 
 	se = find_rst_set_by_id(rsti(root_task)->cg_set);