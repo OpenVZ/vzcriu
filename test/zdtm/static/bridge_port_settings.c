cf5331ee2111ae9ba11f32ccb354971af8621445
Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
2021-02-26 18:20:10 +0300

    zdtm: add bridge and fdb tests
    Criu does not save/restore fdb tables, so any manually added rules will cease
    to exist.
    While investigating this another problem was found: criu does not fully c/r
    bridges. For every interface pinned in bridge there is entity with some
    properties like flooding, learning. etc. (some of them are controlling fdb
    table entries) which is not saved.
    
    Add two tests to indicate these problems. Set noauto.
    
    https://jira.sw.ru/browse/PSBM-106442
    Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
    
    rebase to mainstream 3.18:
    merge ssprintf fixup
    https://src.openvz.org/projects/OVZ/repos/criu/commits/510bf6396
    
    https://jira.sw.ru/browse/PSBM-144416
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: ZDTM

---
+++ test/zdtm/static/bridge_port_settings.c
@@ -0,0 +1,80 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "zdtmtst.h"
+
+const char *test_doc = "Check bridge link properties and fdb table after restore";
+const char *test_author = "Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>";
+
+#define EXEC_CMD(cmdfmt, arg...)                           \
+	do {                                               \
+		ssprintf(cmd, cmdfmt, ## arg);             \
+		if (system(cmd)) {                         \
+			pr_err("FAILED: %s\n", cmd);       \
+			goto out;                          \
+		}                                          \
+	} while (0)
+
+char *dirname;
+TEST_OPTION(dirname, string, "directory name", 1);
+
+int main(int argc, char **argv)
+{
+	char file1[256], file2[256];
+	char cmd[128];
+	int ret = -1;
+
+	test_init(argc, argv);
+
+	ssprintf(file1, "%s/settings_before", dirname);
+	ssprintf(file2, "%s/settings_after", dirname);
+
+	if (mkdir(dirname, 0700)) {
+		pr_perror("can't make directory %s", dirname);
+		exit(1);
+	}
+
+	EXEC_CMD("ip link add br0 type bridge");
+	EXEC_CMD("ip link add veth0 type veth peer name veth1");
+	EXEC_CMD("ip link set veth0 master br0");
+
+#ifdef BRIDGE_FDB
+	EXEC_CMD("bridge fdb add FF:FF:00:00:00:0F dev veth0");
+#else
+	EXEC_CMD("bridge link set dev veth0 flood off");
+	EXEC_CMD("bridge link set dev veth0 cost 50");
+	EXEC_CMD("bridge link set dev veth0 learning off");
+#endif
+
+	EXEC_CMD("ip link set br0 up");
+	EXEC_CMD("ip link set veth0 up");
+	EXEC_CMD("ip link set veth1 up");
+
+#ifdef BRIDGE_FDB
+	EXEC_CMD("bridge fdb show | grep permanent > %s", file1);
+#else
+	EXEC_CMD("bridge -d link show > %s", file1);
+#endif
+
+	test_daemon();
+	test_waitsig();
+
+#ifdef BRIDGE_FDB
+	EXEC_CMD("bridge fdb show | grep permanent > %s", file2);
+#else
+	EXEC_CMD("bridge -d link show > %s", file2);
+#endif
+	EXEC_CMD("diff %s %s", file1, file2);
+
+	ret = 0;
+	pass();
+
+out:
+	unlink(file1);
+	unlink(file2);
+	rmdir(dirname);
+
+	return ret;
+}
