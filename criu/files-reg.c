OpenVZ/vzcriu

commit 034498b283e3aae82cd05815dc2b383ccbcca760

mount: fix external mounts so that they pass check-mounts
1) Prohibit external mounts in non-root mntns. - Currently when we add
an external mount, it is identified by the path of the mountpoint on
dump irrelevant to mntns. If one creates a new mntns in CT all external
mounts are copied to these new ns, and shared mounts become shared with
corresponding copies. So if shared external mount in CT root mntns has a
shared copy in other mntns in CT (same shared_id), the copy is currently
detected as external too, and both of them are restored with external
binding. These way mounts loose share between each other.

2) Restore shared options for external mounts. - Since commit ("vz7:
mount: create a temporary mount to restore shared and master groups") we
don't restore shared options here but we should.

3) Assume manual external mounts as internal sharing. - We have a full
control over manual external mounts, we (vzctl) give them to CT, thus we
can remove external sharing from them, which only adds excessive
complexity to mount restore. Rely on it though it is not yet done, these
can only be detected by --check-mounts.

https://jira.sw.ru/browse/PSBM-96385

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -555,6 +555,9 @@ static int try_resolve_ext_mount(struct mount_info *info)
 		pr_info("Found %s mapping for %s mountpoint\n",
 				ext, info->mountpoint);
 		info->external = ext;
+		/* assume manual external mounts as internal sharing */
+		if (info->shared_id)
+			info->internal_sharing = true;
 		return 1;
 	}
 
@@ -957,6 +960,14 @@ static int resolve_external_mounts(struct mount_info *info)
 		struct mount_info *match;
 
 		if (m->parent == NULL || m->is_ns_root)
+			continue;
+
+		/*
+		 * Only allow external mounts in root mntns. External mounts
+		 * lookup is based on mountpoint path, but there may be a lot of
+		 * mounts with same mountpoint across mntnses.
+		 */
+		if (m->nsid->type != NS_ROOT)
 			continue;
 
 		ret = try_resolve_ext_mount(m);
@@ -2693,6 +2704,10 @@ do_bind:
 		}
 	}
 out:
+	if (mi->external && restore_shared_options(mi, private, mi->shared_id,
+						   mi->master_id))
+		return -1;
+
 	mi->mounted = true;
 	exit_code = 0;
 err:

commit 1105e1e3f5fd68726b5a5d5c7e07d8f0f7c4a673

mount: fix checks using mountinfo->external field
After [1] we only mark mount of root CT mount namespace as external, all
other mounts should use helper mnt_is_external to detect that they can
be bindmounted from explicitly marked as external mount. But there are
some places where old if(->external) check remained.

This fixes three places:

1) external nfs mounts should not be handled via spfs
2) external fuse mounts should not be prohibited
3) external mounts content should not be dumped

Problems actually happen only when there are bindmounts of those
external mounts in non-ct-root mntns.

All other uses of ->external are vaild as in those places we explicitly
want to handle only mounts which are actually created from external
source and not their bindmounts.

Fixes: 034498b28 ("mount: fix external mounts so that they pass check-mounts") [1]
Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/files-reg.c
@@ -1480,7 +1480,7 @@ static inline bool spfs_file(const struct fd_parms *parms, struct ns_id *nsid)
 	if (is_root_mount(mi))
 		return false;
 
-	if (mi->external)
+	if (mnt_is_external(mi))
 		return false;
 
 	return true;

criu/filesystems.c
@@ -608,7 +608,7 @@ static int fusectl_dump(struct mount_info *pm)
 
 		for (it = mntinfo; it; it = it->next) {
 			if (it->fstype->code == FSTYPE__FUSE &&
-					id == kdev_minor(it->s_dev) && !it->external) {
+			    id == kdev_minor(it->s_dev) && !mnt_is_external(it)) {
 				pr_err("%s is a fuse mount but not external\n", it->ns_mountpoint);
 				goto out;
 			}

criu/mount.c
@@ -2021,7 +2021,7 @@ static int dump_one_mountpoint(struct mount_info *pm, struct cr_img *img)
 	if (me.fstype == FSTYPE__AUTO)
 		me.fsname = pm->fsname;
 
-	if (!pm->external) {
+	if (!mnt_is_external(pm)) {
 		if (!pm->dumped && dump_one_fs(pm))
 			return -1;
 