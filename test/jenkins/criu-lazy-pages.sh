75251f19985aa943e2a2a26726cb599a7c1c68b6
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-03-15 12:22:18 +0300

    jenkins: run ve flavor tests without extra pidns in a separate job
    The script test/jenkins/run_ct creates new pidns for running zdtm. For ve
    flavor we can't afford it as starting ve is not supported on too deep nested
    pidns (depth >2).
    
    So let's skip ve flavour in normal run (criu.sh) and prepare to run it in a
    separate Jenkins job.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: VE

--- test/jenkins/criu-lazy-pages.sh
+++ test/jenkins/criu-lazy-pages.sh
@@ -8,12 +8,12 @@ prep
 source `dirname $0`/criu-lazy-common.sh
 
 # lazy restore from images
-./test/zdtm.py run --all --keep-going --report report --parallel 4 \
+./test/zdtm.py run --all --flavor h,ns,uns --keep-going --report report --parallel 4 \
 	       --lazy-pages $LAZY_EXCLUDE || fail
 
 # During pre-dump + lazy-pages we leave VM_NOHUGEPAGE set
 LAZY_EXCLUDE="$LAZY_EXCLUDE -x maps02"
 
 # lazy restore from images with pre-dumps
-./test/zdtm.py run --all --keep-going --report report --parallel 4 \
+./test/zdtm.py run --all --flavor h,ns,uns --keep-going --report report --parallel 4 \
 	       --lazy-pages --pre 2 $LAZY_EXCLUDE || fail
