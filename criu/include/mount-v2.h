OpenVZ/vzcriu

commit a2ac72ab1d35ba49ee2cc59ab973ac624d6125f0

mount-v2: dump owner pidns'es of proc mounts
If we have procfs fsroot mount we can resolve pidns from which it was
mounted by just reading link of "1/ns/pid" file.

On restore copy owner pidns id to all other non-fsroot bindmounts. Also
put mounts with nested pidns owner to list. So that now restore knows
which proc should be mounted from which pidns, but actual mounting will
be in next patches.

In case there is no info about pidns owner in image we assume root
pidns.

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/include/mount-v2.h
@@ -38,6 +38,8 @@ struct sharing_group {
 	char			*source;
 };
 
+struct list_head nested_pidns_procs;
+
 extern int prepare_mnt_ns_v2(void);
 extern int read_mnt_ns_img_v2(struct mount_info *info);
 extern int fini_restore_mntns_v2(void);

criu/filesystems.c
@@ -392,6 +392,51 @@ int collect_binfmt_misc(void)
 #define binfmt_misc_collect NULL
 #endif
 
+static int procfs_dump(struct mount_info *pm)
+{
+	struct ns_desc *ns_d;
+	unsigned int ns_kid;
+	struct ns_id *nsid;
+	int proc_fd, len;
+	char link[PATH_MAX];
+
+	proc_fd = open_mountpoint(pm);
+	if (proc_fd < 0)
+		return MNT_UNREACHABLE;
+
+	len = readlinkat(proc_fd, "1/ns/pid", link, sizeof(link) - 1);
+	if (len < 0) {
+		pr_perror("Can't readlink 1/ns/pid at mount %d\n", pm->mnt_id);
+		close(proc_fd);
+		return -1;
+	}
+	close(proc_fd);
+	link[len] = '\0';
+
+	ns_d = get_ns_kid(link, len, &ns_kid);
+	if (!ns_d || ns_d != &pid_ns_desc) {
+		pr_err("Failed to get ns kid from %s for mount %d\n", link,
+		       pm->mnt_id);
+		return -1;
+	}
+
+	nsid = lookup_ns_by_kid(ns_kid, ns_d);
+	if (!nsid) {
+		pr_err("Found proc mount %d with bad pid namespace %d\n",
+		       pm->mnt_id, ns_kid);
+		return -1;
+	}
+
+	if (nsid->type == NS_CRIU) {
+		pr_err("Proc mount %d has external (not supported) pid namespace %d\n",
+		       pm->mnt_id, ns_kid);
+		return -1;
+	}
+
+	pm->nses.pidns_id = nsid->id;
+	return 0;
+}
+
 static int tmpfs_dump(struct mount_info *pm)
 {
 	int ret = -1, fd = -1, userns_pid = -1;
@@ -1023,6 +1068,7 @@ static struct fstype fstypes[] = {
 	}, {
 		.name = "proc",
 		.code = FSTYPE__PROC,
+		.dump = procfs_dump,
 	}, {
 		.name = "sysfs",
 		.code = FSTYPE__SYSFS,

criu/include/mount.h
@@ -59,6 +59,10 @@ struct rst_mount_info {
 	bool	mounted;
 };
 
+struct mount_namespaces {
+	unsigned int pidns_id;
+};
+
 struct mount_info {
 	int			mnt_id;
 	int			parent_mnt_id;
@@ -130,6 +134,9 @@ struct mount_info {
 
 	unsigned int		ns_bind_id;
 	unsigned int		ns_bind_desc;
+
+	struct mount_namespaces nses;
+	struct list_head	mnt_proc;
 
 	struct rst_mount_info	*rmi;
 

criu/mount-v2.c
@@ -1155,6 +1155,47 @@ err:
 	return -1;
 }
 
+LIST_HEAD(nested_pidns_procs);
+
+static void search_nested_pidns_proc(void) {
+	struct mount_info *mi, *t;
+
+	for (mi = mntinfo; mi; mi = mi->next) {
+		if (mi->fstype->code != FSTYPE__PROC)
+			continue;
+
+		if (mi->nses.pidns_id)
+			continue;
+
+		BUG_ON(list_empty(&mi->mnt_bind) && !mi->mnt_no_bind);
+		list_for_each_entry(t, &mi->mnt_bind, mnt_bind) {
+			if (t->nses.pidns_id) {
+				mi->nses.pidns_id = t->nses.pidns_id;
+				break;
+			}
+		}
+
+		if (!mi->nses.pidns_id) {
+			pr_warn("Proc %d lacks owner pidns info, assume root pidns\n", mi->mnt_id);
+			mi->nses.pidns_id = root_item->ids->pid_ns_id;
+		}
+
+		list_for_each_entry(t, &mi->mnt_bind, mnt_bind) {
+			BUG_ON(t->nses.pidns_id &&
+			       t->nses.pidns_id != mi->nses.pidns_id);
+			t->nses.pidns_id = mi->nses.pidns_id;
+		}
+	}
+
+	for (mi = mntinfo; mi; mi = mi->next) {
+		if (mi->fstype->code != FSTYPE__PROC)
+			continue;
+
+		if (mi->nses.pidns_id != root_item->ids->pid_ns_id)
+			list_add(&mi->mnt_proc, &nested_pidns_procs);
+	}
+}
+
 static int setup_internal_yards(void)
 {
 	struct ns_id *ns;
@@ -1203,6 +1244,8 @@ static int setup_internal_yards(void)
 }
 
 int read_mnt_ns_img_v2(struct mount_info *info) {
+	search_nested_pidns_proc();
+
 	if (resolve_shared_mounts_v2(info))
 		return -1;
 

criu/mount.c
@@ -2032,6 +2032,7 @@ static int dump_one_fs(struct mount_info *mi)
 static int dump_one_mountpoint(struct mount_info *pm, struct cr_img *img)
 {
 	MntEntry me = MNT_ENTRY__INIT;
+	MntNsesEntry mne = MNT_NSES_ENTRY__INIT;
 
 	pr_info("\t%d: %x:%s @ %s\n", pm->mnt_id, pm->s_dev,
 		pm->root, pm->ns_mountpoint);
@@ -2095,6 +2096,12 @@ static int dump_one_mountpoint(struct mount_info *pm, struct cr_img *img)
 		me.ns_bind_id = pm->ns_bind_id;
 		me.has_ns_bind_desc = true;
 		me.ns_bind_desc = pm->ns_bind_desc;
+	}
+
+	if (pm->nses.pidns_id) {
+		mne.has_pidns_id = true;
+		mne.pidns_id = pm->nses.pidns_id;
+		me.nses = &mne;
 	}
 
 	if (pb_write_one(img, &me, PB_MNT))
@@ -3428,6 +3435,7 @@ struct mount_info *mnt_entry_alloc(bool rst)
 		INIT_LIST_HEAD(&new->postpone);
 		INIT_LIST_HEAD(&new->mnt_sharing);
 		INIT_LIST_HEAD(&new->deleted_list);
+		INIT_LIST_HEAD(&new->mnt_proc);
 	}
 	return new;
 }
@@ -3698,7 +3706,6 @@ static int collect_mnt_from_image(struct mount_info **head, struct mount_info **
 		if (pm->fstype->collect && (pm->fstype->collect(pm) < 0))
 			goto err;
 
-
 		if (me->fsname) {
 			pm->fsname = xstrdup(me->fsname);
 			if (!pm->fsname)
@@ -3715,6 +3722,9 @@ static int collect_mnt_from_image(struct mount_info **head, struct mount_info **
 			pm->ns_bind_id = me->ns_bind_id;
 			pm->ns_bind_desc = me->ns_bind_desc;
 		}
+
+		if (me->nses && me->nses->has_pidns_id)
+			pm->nses.pidns_id = me->nses->pidns_id;
 
 		pr_debug("\tRead %d mp @ %s\n", pm->mnt_id, pm->ns_mountpoint);
 	}

images/mnt.proto
@@ -34,6 +34,11 @@ enum fstype {
 	NSFS			= 24;
 };
 
+/* Namespaces from which mount should be mounted */
+message mnt_nses_entry {
+	optional uint32		pidns_id		= 1;
+}
+
 message mnt_entry {
 	required uint32		fstype			= 1;
 	required uint32		mnt_id			= 2;
@@ -61,4 +66,6 @@ message mnt_entry {
 	optional string		ext_key			= 18;
 	optional uint32		ns_bind_id		= 19;
 	optional uint32		ns_bind_desc		= 20;
+
+	optional mnt_nses_entry	nses			= 21;
 }

commit 0c91e37f3855088dc39fd1dc4a39de00573c9e9d

mount-v2: fix nested_pidns_procs declaration
Global vars should be defined as extern ones,
otherwise linker will complain about multiple
definition of symbols.

Fixes: a2ac72ab1 ("mount-v2: dump owner pidns'es of proc mounts")

Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

criu/include/mount-v2.h
@@ -38,7 +38,7 @@ struct sharing_group {
 	char			*source;
 };
 
-struct list_head nested_pidns_procs;
+extern struct list_head nested_pidns_procs;
 
 extern int prepare_mnt_ns_v2(void);
 extern int read_mnt_ns_img_v2(struct mount_info *info);