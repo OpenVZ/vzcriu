OpenVZ/vzcriu

commit 596651d0222ff5196cf80db6e8812a535bf99734

mount: add new mounts-v2 engine
Design of mounts-v2:

  New function prepare_mnt_ns_v2 is our main entry point.

  - First we do pre-namespace work.

  We classify mounts in groups by (shared_id, master_id) in new
  resolve_shared_mounts_v2.

  We walk the mount tree and mount each mount in plain mountpoint not in
  mount tree but as a sub-directory of root_yard_mp. New can_mount_now_v2
  only prohibits mounting external bind before external soucrce, no
  sharing heuristics as it was in v1. All mounts are remounted MS_PRIVATE.

  - Second we create each final mount namespace.

  New assemble_tree_from_plain_mounts helper constructs with MS_MOVE the
  mount tree of each final mount ns and opens fds on the mount and it's
  mountpoint so that we can access each file later from final mntns via
  those fds.

  - Third we set sharing options.

  New restore_mount_sharing_options walks each root sharing group and
  their descendants with dfs tree lookup. It creates sharing for the first
  mount in the sharing group and then sets the same sharing on all other
  mounts in this group.

  Sharing creation for fist mount is two step:

  a) If we have master_id we either copy sharing from parent sharing group
  or from external source and then make us slave.
  b) Next if we have shared_id we make us shared.

Export common functions variables and defines from mount.c so that we
can reuse them. Copy and resork several functions and call them _v2.

FIXME: unix socket bindmount support is temporary disabled will be fixed
in separate task.

v2: prepare is_overmounted properly as file restore code still uses it,
remove back deleted mount source, cleanup headers.

https://jira.sw.ru/browse/PSBM-58617

v3: remove unnecesarry ->mountpoint usages; don't close fd if it is not
open; fix error handling in set_unbindable_v2

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

rebase to 3.15 mainstream:
rework do_mount_root_v2 to reflect d99ee9753 ("mount: Bind-mount root

criu/include/mount.h
@@ -1,6 +1,7 @@
 #ifndef __CR_MOUNT_H__
 #define __CR_MOUNT_H__
 
+#include <stdio.h>
 #include <sys/types.h>
 
 #include "common/list.h"
@@ -10,6 +11,30 @@ struct proc_mountinfo;
 struct pstree_item;
 struct fstype;
 struct ns_id;
+
+/*
+ * Put a : in here since those are invalid on
+ * the cli, so we know it's autogenerated in
+ * debugging.
+ */
+#define AUTODETECTED_MOUNT "CRIU:AUTOGENERATED"
+#define EXTERNAL_DEV_MOUNT "CRIU:EXTERNAL_DEV"
+#define NO_ROOT_MOUNT "CRIU:NO_ROOT"
+#define MS_PROPAGATE (MS_SHARED | MS_PRIVATE | MS_UNBINDABLE | MS_SLAVE)
+
+/*
+ * Here are a set of flags which we know how to handle for the one mount call.
+ * All of them except MS_RDONLY are set only as mnt flags.
+ * MS_RDONLY is set for both mnt ans sb flags, so we can restore it for one
+ * mount call only if it set for both masks.
+ */
+#define MS_MNT_KNOWN_FLAGS (MS_NOSUID | MS_NOEXEC | MS_NODEV | MS_NOATIME | \
+				MS_NODIRATIME | MS_RELATIME | MS_RDONLY)
+
+
+#define BINFMT_MISC_HOME "proc/sys/fs/binfmt_misc"
+
+#define HELPER_MNT_ID 0
 
 #define MOUNT_INVALID_DEV	(0)
 
@@ -45,7 +70,15 @@ struct mount_info {
 	 */
 	char			*mountpoint;
 	char			*ns_mountpoint;
+
+	/* Mount-v2 specific */
 	char			*plain_mountpoint;
+	bool			is_dir;
+	int			mp_fd_id;
+	int			mnt_fd_id;
+	struct sharing_group	*sg;
+	struct list_head	mnt_sharing;
+
 	int			fd;
 	unsigned		flags;
 	unsigned		sb_flags;
@@ -62,6 +95,8 @@ struct mount_info {
 	bool			need_plugin;
 	bool			is_ns_root;
 	bool			deleted;
+	int			deleted_level;
+	struct list_head	deleted_list;
 	struct mount_info	*next;
 	struct ns_id		*nsid;
 
@@ -172,5 +207,35 @@ extern int remount_readonly_mounts(void);
 extern int check_mounts(void);
 extern int try_remount_writable(struct mount_info *mi, bool ns);
 extern bool mnt_is_overmounted(struct mount_info *mi);
+
+/* Exported for mount-v2.c */
+extern struct mount_info *mnt_is_external(struct mount_info *m);
+extern bool has_mounted_external_bind(struct mount_info *m);
+extern struct mount_info *mnt_get_external_nodev(struct mount_info *m);
+extern bool rst_mnt_is_root(struct mount_info *m);
+extern struct mount_info *mnt_get_root(struct mount_info *m);
+extern int do_simple_mount(struct mount_info *mi, const char *src,
+			   const char *fstype, unsigned long mountflags);
+extern char *resolve_source(struct mount_info *mi);
+extern char *mnt_fsname(struct mount_info *mi);
+extern int apply_sb_flags(void *args, int fd, pid_t pid);
+extern int mount_root(void *args, int fd, pid_t pid);
+extern int restore_ext_mount(struct mount_info *mi);
+extern int fetch_rt_stat(struct mount_info *m, const char *where);
+extern int print_ns_root(struct ns_id *ns, int remap_id, char *buf, int bs);
+extern void search_bindmounts(void);
+extern int merge_mount_trees(struct mount_info *root_yard);
+extern struct mount_info
+__maybe_unused *add_cr_time_mount(struct mount_info *root, char *fsname,
+				  const char *path, unsigned int s_dev,
+				  bool rst);
+extern int validate_mounts(struct mount_info *info, bool for_dump);
+extern int mnt_tree_for_each(struct mount_info *start,
+			     int (*fn)(struct mount_info *));
+extern int cr_pivot_root(char *root);
+extern void set_is_overmounted(void);
+
+struct mount_info *root_yard_mp;
+char *mnt_roots;
 
 #endif /* __CR_MOUNT_H__ */

criu/Makefile.crtools
@@ -40,6 +40,7 @@ obj-y			+= lsm.o
 obj-y			+= mem.o
 obj-y			+= memfd.o
 obj-y			+= mount.o
+obj-y			+= mount-v2.o
 obj-y			+= filesystems.o
 obj-y			+= namespaces.o
 obj-y			+= netfilter.o

criu/include/mount-v2.h
@@ -1,0 +1,43 @@
+#ifndef __CR_MOUNT_V2_H__
+#define __CR_MOUNT_V2_H__
+
+#include <sys/types.h>
+
+#include "common/list.h"
+#include "mount.h"
+
+#define MS_SET_GROUP (1<<26)
+
+struct sharing_group {
+	/* This pair identifies the group */
+	int                     shared_id;
+	int                     master_id;
+
+	/* List of shared groups */
+	struct list_head        list;
+
+	/* List of mounts in this group */
+	struct list_head        mnt_list;
+
+	/*
+	 * List of dependant shared groups:
+	 * - all siblings have equal master_id
+	 * - the parent has shared_id equal to children's master_id
+	 *
+	 * This is a bit tricky: parent pointer indicates if there is one
+	 * parent sharing_group in list or only siblings.
+	 * So for traversal if parent pointer is set we can do:
+	 *   list_for_each_entry(t, &sg->parent->children, siblings)
+	 * and overvise we can do:
+	 *   list_for_each_entry(t, &sg->siblings, siblings)
+	 */
+	struct list_head        children;
+	struct list_head        siblings;
+	struct sharing_group    *parent;
+
+	char			*source;
+};
+
+extern int prepare_mnt_ns_v2(void);
+
+#endif /* __CR_MOUNT_V2_H__ */

criu/mount.c
@@ -17,6 +17,7 @@
 #include "plugin.h"
 #include "filesystems.h"
 #include "mount.h"
+#include "mount-v2.h"
 #include "pstree.h"
 #include "image.h"
 #include "namespaces.h"
@@ -33,25 +34,11 @@
 
 #include "images/mnt.pb-c.h"
 
-/*
- * Put a : in here since those are invalid on
- * the cli, so we know it's autogenerated in
- * debugging.
- */
-#define AUTODETECTED_MOUNT "CRIU:AUTOGENERATED"
-#define EXTERNAL_DEV_MOUNT "CRIU:EXTERNAL_DEV"
-#define NO_ROOT_MOUNT "CRIU:NO_ROOT"
-#define MS_PROPAGATE (MS_SHARED | MS_PRIVATE | MS_UNBINDABLE | MS_SLAVE)
-
 #undef	LOG_PREFIX
 #define LOG_PREFIX "mnt: "
 
-#define BINFMT_MISC_HOME "proc/sys/fs/binfmt_misc"
-
-#define HELPER_MNT_ID 0
-
 /* A helper mount_info entry for the roots yard */
-static struct mount_info *root_yard_mp = NULL;
+struct mount_info *root_yard_mp = NULL;
 
 static LIST_HEAD(delayed_unbindable);
 
@@ -428,7 +415,7 @@ static bool mounts_equal(struct mount_info *a, struct mount_info *b)
  * mnt_roots is a temporary directory for restoring sub-trees of
  * non-root namespaces.
  */
-static char *mnt_roots;
+char *mnt_roots;
 
 static struct mount_info *mnt_build_ids_tree(struct mount_info *list)
 {
@@ -677,7 +664,7 @@ static struct mount_info *__mnt_get_external(struct mount_info *m, bool need_mas
  * Say mount is external if it was explicitly specified as an external or it
  * can be bind-mounted from such an explicit external mount.
  */
-static struct mount_info *mnt_is_external(struct mount_info *m)
+struct mount_info *mnt_is_external(struct mount_info *m)
 {
 	return __mnt_get_external(m, 0, 0, 0);
 }
@@ -687,12 +674,12 @@ static struct mount_info *mnt_get_external_need_master(struct mount_info *m)
 	return __mnt_get_external(m, 1, 0, 0);
 }
 
-static bool has_mounted_external_bind(struct mount_info *m)
+bool has_mounted_external_bind(struct mount_info *m)
 {
 	return __mnt_get_external(m, 0, 1, 0);
 }
 
-struct mount_info __maybe_unused *mnt_get_external_nodev(struct mount_info *m)
+struct mount_info *mnt_get_external_nodev(struct mount_info *m)
 {
 	return __mnt_get_external(m, 0, 0, 1);
 }
@@ -831,9 +818,7 @@ static bool unsupported_mount(const struct mount_info *m)
 	return nfs_mount(m) ? unsupported_nfs_mount(m) : false;
 }
 
-static struct mount_info *mnt_get_root(struct mount_info *m);
-
-static int validate_mounts(struct mount_info *info, bool for_dump)
+int validate_mounts(struct mount_info *info, bool for_dump)
 {
 	struct mount_info *m, *t;
 
@@ -1177,7 +1162,7 @@ static void __search_bindmounts(struct mount_info *m)
 		m->mnt_no_bind = true;
 }
 
-static void search_bindmounts(void)
+void search_bindmounts(void)
 {
 	struct mount_info *mi;
 
@@ -1486,15 +1471,12 @@ static int __set_is_overmounted(struct mount_info *mi)
 	return 0;
 }
 
-static int mnt_tree_for_each(struct mount_info *start,
-			     int (*fn)(struct mount_info *));
-
 /*
  * mnt_is_overmounted should only work on initial mount tree, then we merge all
  * trees under the root yard all roots become siblings with same ns_mountpoint
  * ("/"), and it breaks the mnt_is_overmounted function.
  */
-static void set_is_overmounted(void)
+void set_is_overmounted(void)
 {
 	struct ns_id *nsid;
 
@@ -1771,7 +1753,7 @@ err:
 	return -1;
 }
 
-static __maybe_unused struct mount_info *add_cr_time_mount(struct mount_info *root, char *fsname, const char *path, unsigned int s_dev, bool rst)
+struct mount_info __maybe_unused *add_cr_time_mount(struct mount_info *root, char *fsname, const char *path, unsigned int s_dev, bool rst)
 {
 	struct mount_info *mi, *t, *parent;
 	bool add_slash = false;
@@ -2198,8 +2180,8 @@ err:
 #define MNT_WALK_NONE	0 &&
 
 
-static int mnt_tree_for_each(struct mount_info *start,
-		int (*fn)(struct mount_info *))
+int mnt_tree_for_each(struct mount_info *start,
+		      int (*fn)(struct mount_info *))
 {
 	struct mount_info *tmp;
 	LIST_HEAD(postpone);
@@ -2246,7 +2228,7 @@ static int mnt_tree_for_each_reverse(struct mount_info *m,
 	return 0;
 }
 
-static char *resolve_source(struct mount_info *mi)
+char *resolve_source(struct mount_info *mi)
 {
 	if (kdev_major(mi->s_dev) == 0)
 		/*
@@ -2450,7 +2432,7 @@ skip_parent:
 	return 0;
 }
 
-static int fetch_rt_stat(struct mount_info *m, const char *where)
+int fetch_rt_stat(struct mount_info *m, const char *where)
 {
 	struct stat st;
 
@@ -2463,16 +2445,7 @@ static int fetch_rt_stat(struct mount_info *m, const char *where)
 	return 0;
 }
 
-/*
- * Here are a set of flags which we know how to handle for the one mount call.
- * All of them except MS_RDONLY are set only as mnt flags.
- * MS_RDONLY is set for both mnt ans sb flags, so we can restore it for one
- * mount call only if it set for both masks.
- */
-#define MS_MNT_KNOWN_FLAGS (MS_NOSUID | MS_NOEXEC | MS_NODEV | MS_NOATIME | \
-				MS_NODIRATIME | MS_RELATIME | MS_RDONLY)
-
-static int do_simple_mount(struct mount_info *mi, const char *src, const
+int do_simple_mount(struct mount_info *mi, const char *src, const
 			   char *fstype, unsigned long mountflags)
 {
 	int ret = mount(src, service_mountpoint(mi), fstype, mountflags, mi->options);
@@ -2481,7 +2454,7 @@ static int do_simple_mount(struct mount_info *mi, const char *src, const
 	return ret;
 }
 
-static char *mnt_fsname(struct mount_info *mi)
+char *mnt_fsname(struct mount_info *mi)
 {
 	if (mi->fstype->code == FSTYPE__AUTO)
 		return mi->fsname;
@@ -2509,12 +2482,12 @@ static int userns_mount(char *src, void *args, int fd, pid_t pid)
 	return err;
 }
 
-static int apply_sb_flags(void *args, int fd, pid_t pid)
+int apply_sb_flags(void *args, int fd, pid_t pid)
 {
 	return userns_mount(NULL, args, fd, pid);
 }
 
-static int mount_root(void *args, int fd, pid_t pid)
+int mount_root(void *args, int fd, pid_t pid)
 {
 	return userns_mount(opts.root, args, fd, pid);
 }
@@ -2585,7 +2558,7 @@ static int do_new_mount(struct mount_info *mi)
 	return 0;
 }
 
-static int restore_ext_mount(struct mount_info *mi)
+int restore_ext_mount(struct mount_info *mi)
 {
 	int ret;
 
@@ -2874,12 +2847,12 @@ err:
 	return exit_code;
 }
 
-static bool rst_mnt_is_root(struct mount_info *m)
+bool rst_mnt_is_root(struct mount_info *m)
 {
 	return (m->is_ns_root && m->nsid->id == root_item->ids->mnt_ns_id);
 }
 
-static struct mount_info *mnt_get_root(struct mount_info *m)
+struct mount_info *mnt_get_root(struct mount_info *m)
 {
 	struct mount_info *t;
 
@@ -3194,10 +3167,6 @@ static int do_umount_one(struct mount_info *mi)
  * roots_yard where it will be restored. The remapped mount will be
  * moved to the right places after restoring all mounts.
  */
-
-static inline int print_ns_root(struct ns_id *ns, int remap_id, char *buf, int bs);
-static int get_mp_mountpoint(char *mountpoint, struct mount_info *mi, char *root, int root_len);
-
 static LIST_HEAD(mnt_remap_list);
 static int remap_id;
 
@@ -3296,7 +3265,7 @@ static int fixup_remap_mounts(void)
 	return 0;
 }
 
-static int cr_pivot_root(char *root)
+int cr_pivot_root(char *root)
 {
 	char tmp_dir_tmpl[] = "crtools-put-root.XXXXXX";
 	bool tmp_dir = false;
@@ -3402,6 +3371,9 @@ struct mount_info *mnt_entry_alloc(bool rst)
 				return NULL;
 			}
 		}
+		new->mp_fd_id = -1;
+		new->mnt_fd_id = -1;
+		new->is_dir = true;
 		new->fd = -1;
 		new->is_overmounted = -1;
 		INIT_LIST_HEAD(&new->children);
@@ -3415,6 +3387,7 @@ struct mount_info *mnt_entry_alloc(bool rst)
 		INIT_LIST_HEAD(&new->mnt_notprop);
 		INIT_LIST_HEAD(&new->mnt_unbindable);
 		INIT_LIST_HEAD(&new->postpone);
+		INIT_LIST_HEAD(&new->deleted_list);
 	}
 	return new;
 }
@@ -3436,7 +3409,7 @@ void mnt_entry_free(struct mount_info *mi)
  * Helper for getting a path to where the namespace's root
  * is re-constructed.
  */
-static inline int print_ns_root(struct ns_id *ns, int remap_id, char *buf, int bs)
+int print_ns_root(struct ns_id *ns, int remap_id, char *buf, int bs)
 {
 	return snprintf(buf, bs, "%s/%d-%010d", mnt_roots, ns->id, remap_id);
 }
@@ -3832,7 +3805,7 @@ void fini_restore_mntns(void)
 	}
 }
 
-static int merge_mount_trees(struct mount_info *root_yard)
+int merge_mount_trees(struct mount_info *root_yard)
 {
 	struct ns_id *nsid;
 
@@ -3988,6 +3961,8 @@ static int populate_mnt_ns(void)
 		BUG_ON(!list_empty(&cr_time->mnt_notprop));
 		BUG_ON(!list_empty(&cr_time->postpone));
 		BUG_ON(!list_empty(&cr_time->mnt_unbindable));
+		BUG_ON(!list_empty(&cr_time->mnt_sharing));
+		BUG_ON(!list_empty(&cr_time->deleted_list));
 
 		cr_time->parent->next = cr_time->next;
 		list_del(&cr_time->siblings);
@@ -4128,6 +4103,9 @@ int prepare_mnt_ns(void)
 
 		free_mntinfo(old);
 	}
+
+	if (opts.mounts_v2)
+		return prepare_mnt_ns_v2();
 
 	ret = populate_mnt_ns();
 	if (ret)

commit f2957ee431696eaf49c1a7bc5c40907d76a32d59

mount: fix declaration of root_yard_mp and mnt_roots
They should be external otherwise linker will complain
about multiple declaration.

Fixes: 596651d02 ("mount: add new mounts-v2 engine")

Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

criu/include/mount.h
@@ -292,7 +292,7 @@ extern int do_restore_task_mnt_ns(struct ns_id *nsid);
 extern struct mount_info *mnt_subtree_next(struct mount_info *mi,
 					   struct mount_info *root);
 
-struct mount_info *root_yard_mp;
-char *mnt_roots;
+extern struct mount_info *root_yard_mp;
+extern char *mnt_roots;
 
 #endif /* __CR_MOUNT_H__ */