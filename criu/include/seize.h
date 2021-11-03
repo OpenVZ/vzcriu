2aa36da46fe6eb68ac431cfbabdb6e4cddb7ce78
Yuriy Vasiliev <yuriy.vasiliev@virtuozzo.com>
2021-11-03 18:26:40 +0300

    seize: recover original freezer sub-tree state after dump
    The problem is that we release all nested freezers because tasks need
    to be seized by ptrace and don't return nested freezers to their
    initial state.
    
    So to solve this problem we should save nested freezer states before
    thawing.
    
    https://jira.sw.ru/browse/PSBM-107663
    Signed-off-by: Yuriy Vasiliev <yuriy.vasiliev@virtuozzo.com>
    
    Feature: Freezer

--- criu/include/seize.h
+++ criu/include/seize.h
@@ -4,6 +4,7 @@
 extern int collect_pstree(void);
 extern void pstree_switch_state(struct pstree_item *root_item, int st);
 extern const char *get_real_freezer_state(void);
+extern void free_freezer_real_states(void);
 extern bool alarm_timeouted(void);
 
 extern char *task_comm_info(pid_t pid, char *comm, size_t size);
