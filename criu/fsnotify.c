OpenVZ/vzcriu

commit 56c7b078457d847b79e05c1a860111ff262f1944

mount: add open_mountpoint_with_flags()
So we can open mounpoint with custom flags. This is needed for bindmounts
because they may not be a directories.
Add O_NOFOLLOW to ensure we won't open something instead of bindmounted
file itself.

https://jira.sw.ru/browse/PSBM-135398
Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>

criu/fsnotify.c
@@ -131,7 +131,7 @@ static char *alloc_openable(unsigned int s_dev, unsigned long i_ino, FhEntry *f_
 		if (!mnt_is_dir(m))
 			continue;
 
-		mntfd = __open_mountpoint(m, -1);
+		mntfd = __open_mountpoint(m, -1, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
 		pr_debug("\t\tTrying via mntid %d root %s ns_mountpoint @%s (%d)\n",
 			 m->mnt_id, m->root, m->ns_mountpoint, mntfd);
 		if (mntfd < 0)
@@ -207,7 +207,7 @@ static int open_handle(unsigned int s_dev, unsigned long i_ino,
 		if (m->s_dev != s_dev || !mnt_is_dir(m))
 			continue;
 
-		mntfd = __open_mountpoint(m, -1);
+		mntfd = __open_mountpoint(m, -1, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
 		if (mntfd < 0) {
 			pr_warn("Can't open mount for s_dev %x, continue\n", s_dev);
 			continue;

criu/mount.c
@@ -1327,8 +1327,9 @@ int mnt_is_dir(struct mount_info *pm)
  * mnt_fd is a file descriptor on the mountpoint, which is closed in an error case.
  * If mnt_fd is -1, the mountpoint will be opened by this function.
  */
-int __open_mountpoint(struct mount_info *pm, int mnt_fd)
+int __open_mountpoint(struct mount_info *pm, int mnt_fd, int flags)
 {
+	int follow = (flags & O_NOFOLLOW) ? AT_SYMLINK_NOFOLLOW : 0;
 	struct stat st;
 	int dev;
 	int ret;
@@ -1340,14 +1341,17 @@ int __open_mountpoint(struct mount_info *pm, int mnt_fd)
 		if (mntns_root < 0)
 			return -1;
 
-		mnt_fd = openat(mntns_root, pm->ns_mountpoint, O_RDONLY);
+		mnt_fd = openat(mntns_root, pm->ns_mountpoint, flags);
 		if (mnt_fd < 0) {
 			pr_perror("Can't open %s", pm->ns_mountpoint);
 			return -1;
 		}
 	}
 
-	ret = fstat(mnt_fd, &st);
+	if (flags & O_PATH)
+		ret = fstatat(mnt_fd, "", &st, AT_EMPTY_PATH | follow);
+	else
+		ret = fstat(mnt_fd, &st);
 	if (ret < 0) {
 		pr_perror("fstat(%s) failed", pm->ns_mountpoint);
 		goto err;
@@ -1387,7 +1391,7 @@ int open_mount(unsigned int s_dev)
 	if (!m)
 		return -ENOENT;
 
-	return __open_mountpoint(m, -1);
+	return __open_mountpoint(m, -1, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
 }
 
 /* Bind-mount a mount point in a temporary place without children */
@@ -1654,6 +1658,7 @@ static int umount_overmounts(struct mount_info *m)
 struct clone_arg {
 	struct mount_info *mi;
 	int *fd;
+	int flags;
 };
 
 /*
@@ -1702,7 +1707,7 @@ int ns_open_mountpoint(void *arg)
 	 * explicitly as when last process exits mntns all mounts in it are
 	 * cleaned from their children, and we are exactly the last process.
 	 */
-	*fd = open(mi->ns_mountpoint, O_DIRECTORY|O_RDONLY);
+	*fd = open(mi->ns_mountpoint, ca->flags);
 	if (*fd < 0) {
 		pr_perror("Unable to open %s(%d)", mi->ns_mountpoint, mi->mnt_id);
 		goto err;
@@ -1715,11 +1720,16 @@ err:
 
 int open_mountpoint(struct mount_info *pm)
 {
+	return open_mountpoint_with_flags(pm, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
+}
+
+int open_mountpoint_with_flags(struct mount_info *pm, int flags)
+{
 	int fd = -1, cwd_fd, ns_old = -1;
 
 	/* No overmounts and children - the entire mount is visible */
 	if (list_empty(&pm->children) && !mnt_is_overmounted(pm))
-		return __open_mountpoint(pm, -1);
+		return __open_mountpoint(pm, -1, flags);
 
 	pr_info("Mount is not fully visible %s(%d)\n", pm->ns_mountpoint, pm->mnt_id);
 
@@ -1756,7 +1766,8 @@ int open_mountpoint(struct mount_info *pm)
 		int pid, status;
 		struct clone_arg ca = {
 			.mi = pm,
-			.fd = &fd
+			.fd = &fd,
+			.flags = flags
 		};
 
 		pr_info("\tmount is overmounted or has children %s(%d)\n",
@@ -1801,7 +1812,7 @@ int open_mountpoint(struct mount_info *pm)
 	}
 	close(cwd_fd);
 
-	return __open_mountpoint(pm, fd);
+	return __open_mountpoint(pm, fd, flags);
 err:
 	if (ns_old >= 0)
 		restore_ns(ns_old, &mnt_ns_desc);

criu/include/mount.h
@@ -211,9 +211,10 @@ extern int mntns_get_root_by_mnt_id(int mnt_id);
 extern struct ns_id *lookup_nsid_by_mnt_id(int mnt_id);
 
 extern int open_mount(unsigned int s_dev);
-extern int __open_mountpoint(struct mount_info *pm, int mnt_fd);
+extern int __open_mountpoint(struct mount_info *pm, int mnt_fd, int flags);
 extern int mnt_is_dir(struct mount_info *pm);
 extern int open_mountpoint(struct mount_info *pm);
+extern int open_mountpoint_with_flags(struct mount_info *pm, int flags);
 
 extern struct mount_info *collect_mntinfo(struct ns_id *ns, bool for_dump);
 extern int prepare_mnt_ns(void);

commit 356eca7306f51ad1a02a2229372ce4cd8e998dbb

mount: remove O_DIRECTORY flag for mount opening helpers
Because mounts can be bindmounts, so opening with O_DIRECTORY will
definitely fail.

First of all this fixes the detached_namespace_with_bindmount test
currently failing with
(00.026273) Error (criu/mount.c:1346): mnt: Can't open ./zdtm/static/detached_namespace_with_bindmount.test/net_bind: Not a directory
(00.026275) Error (criu/mount.c:4499): mnt: Failed to after_parse on 611

Also I removed O_DIRECTORY flag from fsnotify code which iterates
over mounts for the same reason.

Note: we do not hit this problem with open_mountpoint functions working
only with directories before as we luckily used it only for directory
mounts, the problem existed since [2].

https://jira.sw.ru/browse/PSBM-139948
Fixes: 56c7b078457d ("mount: add open_mountpoint_with_flags()")
Fixes: e54d0d26ba63 ("mount: fix regression where open_mountpoint failed on readonly fs") [2]
Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>

criu/fsnotify.c
@@ -131,7 +131,7 @@ static char *alloc_openable(unsigned int s_dev, unsigned long i_ino, FhEntry *f_
 		if (!mnt_is_dir(m))
 			continue;
 
-		mntfd = __open_mountpoint(m, -1, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
+		mntfd = __open_mountpoint(m, -1, O_RDONLY | O_NOFOLLOW);
 		pr_debug("\t\tTrying via mntid %d root %s ns_mountpoint @%s (%d)\n",
 			 m->mnt_id, m->root, m->ns_mountpoint, mntfd);
 		if (mntfd < 0)
@@ -207,7 +207,7 @@ static int open_handle(unsigned int s_dev, unsigned long i_ino,
 		if (m->s_dev != s_dev || !mnt_is_dir(m))
 			continue;
 
-		mntfd = __open_mountpoint(m, -1, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
+		mntfd = __open_mountpoint(m, -1, O_RDONLY | O_NOFOLLOW);
 		if (mntfd < 0) {
 			pr_warn("Can't open mount for s_dev %x, continue\n", s_dev);
 			continue;

criu/mount.c
@@ -1391,7 +1391,7 @@ int open_mount(unsigned int s_dev)
 	if (!m)
 		return -ENOENT;
 
-	return __open_mountpoint(m, -1, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
+	return __open_mountpoint(m, -1, O_RDONLY | O_NOFOLLOW);
 }
 
 /* Bind-mount a mount point in a temporary place without children */
@@ -1720,7 +1720,7 @@ err:
 
 int open_mountpoint(struct mount_info *pm)
 {
-	return open_mountpoint_with_flags(pm, O_DIRECTORY | O_RDONLY | O_NOFOLLOW);
+	return open_mountpoint_with_flags(pm, O_RDONLY | O_NOFOLLOW);
 }
 
 int open_mountpoint_with_flags(struct mount_info *pm, int flags)