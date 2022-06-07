OpenVZ/vzcriu

commit e4956b0b15c93f11bcb2f74d8f20a5eca6eb51c4

vz7: sk-queue -- Add support of ucreds in unix sockets
Checkpoint and restore ucreds on unix sockets are not
that trivial: the @pid returned by the kernel upon
message fetching is mapped to current user namespace
so it is not the same as it would be received inside
container. So we need to map real pid returned into
virtual seen from inside, but we know virtual pids
only when complete process tree is built.

Thus what we do -- queue such packets into own
queue and when process tree is build walk over
and transform former pids into virtual ones.

https://jira.sw.ru/browse/PSBM-61095

Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>

criu/sk-queue.c
@@ -36,7 +36,10 @@
 struct sk_packet {
 	struct list_head	list;
 	SkPacketEntry		*entry;
-	char			*data;
+	union {
+		char		*data;
+		size_t		data_off;
+	};
 	unsigned		scm_len;
 	int			*scm;
 };
@@ -125,6 +128,113 @@ static int dump_scm_rights(struct cmsghdr *ch, SkPacketEntry *pe)
  * */
 #define CMSG_MAX_SIZE	1024
 
+int sk_queue_post_actions(void)
+{
+	struct sk_packet *pkt, *t;
+	struct cr_img *img;
+	int ret = 0;
+
+	img = img_from_set(glob_imgset, CR_FD_SK_QUEUES);
+
+	list_for_each_entry_safe(pkt, t, &packets_list, list) {
+		if (!pkt->entry->ucred) {
+			pr_err("ucred: corruption on id_for %x\n",
+			       pkt->entry->id_for);
+			ret = -1;
+		}
+
+		if (!ret) {
+			struct pstree_item *item, *found = NULL;
+			SkUcredEntry *ue = pkt->entry->ucred;
+
+			for_each_pstree_item(item) {
+				if (item->pid->real == ue->pid) {
+					found = item;
+					break;
+				}
+			}
+
+			if (!found) {
+				pr_err("ucred: Can't lookup process with pid %d\n",
+				       ue->pid);
+				ret = -ENOENT;
+				goto next;
+			} else {
+				pr_debug("ucred: Fixup ucred pids %d -> %d\n",
+					 ue->pid, vpid(item));
+				ue->pid = vpid(item);
+			}
+
+			ret = pb_write_one(img, pkt->entry, PB_SK_QUEUES);
+			if (ret < 0) {
+				ret = -EIO;
+				goto next;
+			}
+
+			ret = write_img_buf(img, (void *)pkt + pkt->data_off,
+					    pkt->entry->length);
+			if (ret < 0) {
+				ret = -EIO;
+				goto next;
+			}
+
+		}
+
+next:
+		list_del(&pkt->list);
+		xfree(pkt);
+	}
+	return ret;
+}
+
+static int queue_packet_entry(SkPacketEntry *entry, void *data, size_t len)
+{
+	struct sk_packet *pkt;
+	size_t sum = 0;
+
+	sum += sizeof(*pkt);
+	sum += sizeof(*pkt->entry);
+	sum += sizeof(*pkt->entry->ucred);
+	sum += _K_SS_MAXSIZE;
+	sum += len;
+
+	pkt = xmalloc(sum);
+
+	if (pkt) {
+		SkPacketEntry *pe = (void *)pkt + sizeof(*pkt);
+		SkUcredEntry *ue = (void *)pe + sizeof(*pe);
+		void *addr = (void *)ue + sizeof(*ue);
+		void *p = (void *)addr + _K_SS_MAXSIZE;
+
+		sk_packet_entry__init(pe);
+		sk_ucred_entry__init(ue);
+
+		pkt->entry	= pe;
+		pkt->data_off	= p - (void *)pkt;
+		list_add_tail(&pkt->list, &packets_list);
+
+		pe->id_for	= entry->id_for;
+		pe->length	= entry->length;
+		pe->has_addr	= entry->has_addr;
+		if (entry->has_addr) {
+			pe->addr.data	= addr;
+			pe->addr.len	= entry->addr.len;
+			memcpy(addr, entry->addr.data, entry->addr.len);
+		}
+		pe->ucred	= ue;
+		ue->uid		= entry->ucred->uid;
+		ue->gid		= entry->ucred->gid;
+		ue->pid		= entry->ucred->pid;
+
+		memcpy(p, data, len);
+		pr_debug("ucred: Queued ucred packet id_for %x\n",
+			 pkt->entry->id_for);
+		return 0;
+	}
+
+	return -ENOMEM;
+}
+
 static int dump_sk_creds(struct ucred *ucred, SkPacketEntry *pe, int flags)
 {
 	SkUcredEntry *ent;
@@ -136,13 +246,22 @@ static int dump_sk_creds(struct ucred *ucred, SkPacketEntry *pe, int flags)
 	sk_ucred_entry__init(ent);
 	ent->uid = userns_uid(ucred->uid);
 	ent->gid = userns_gid(ucred->gid);
+	ent->pid = ucred->pid;
+
+	if (pe->ucred)
+		pr_warn("ucred: ucred already assigned\n");
+	pe->ucred = ent;
+
 	if (flags & SK_QUEUE_REAL_PID) {
 		/*
 		 * It is impossible to conver pid from real to virt,
-		 * because virt pid-s are known for dumped task only
+		 * because virt pid-s are known for dumped task only.
+		 * Thus defer the image writting, we will do it at the
+		 * end, where all processes are collected already.
 		 */
-		pr_err("ucred-s for unix sockets aren't supported yet");
-		goto out;
+		pr_debug("ucred: Detected ucreds on id_for %x (uid %d gid %d pid %d)\n",
+			 pe->id_for, ent->uid, ent->gid, ent->pid);
+		return 1;
 	} else {
 		int pidns = root_ns_mask & CLONE_NEWPID;
 		char path[64];
@@ -163,13 +282,11 @@ static int dump_sk_creds(struct ucred *ucred, SkPacketEntry *pe, int flags)
 			pr_err("Unable to dump ucred for a dead process %d\n", ucred->pid);
 			goto out;
 		}
-		ent->pid = ucred->pid;
 	}
-
-	pe->ucred = ent;
 
 	return 0;
 out:
+	pe->ucred = NULL;
 	xfree(ent);
 	return -1;
 }
@@ -178,6 +295,7 @@ static int dump_packet_cmsg(struct msghdr *mh, SkPacketEntry *pe, int flags)
 {
 	struct cmsghdr *ch;
 	int n_rights = 0;
+	int ret = 0;
 
 	for (ch = CMSG_FIRSTHDR(mh); ch; ch = CMSG_NXTHDR(mh, ch)) {
 		if (ch->cmsg_type == SCM_RIGHTS) {
@@ -202,7 +320,8 @@ static int dump_packet_cmsg(struct msghdr *mh, SkPacketEntry *pe, int flags)
 			    ch->cmsg_type == SCM_CREDENTIALS) {
 				struct ucred *ucred = (struct ucred *)CMSG_DATA(ch);
 
-				if (dump_sk_creds(ucred, pe, flags))
+				ret |= dump_sk_creds(ucred, pe, flags);
+				if (ret < 0)
 					return -1;
 				continue;
 			} else if (ch->cmsg_type == SCM_TIMESTAMP ||
@@ -233,7 +352,7 @@ static int dump_packet_cmsg(struct msghdr *mh, SkPacketEntry *pe, int flags)
 		return -1;
 	}
 
-	return 0;
+	return ret;
 }
 
 static void release_cmsg(SkPacketEntry *pe)
@@ -339,13 +458,21 @@ int dump_sk_queue(int sock_fd, int sock_id, int flags)
 			goto err_set_sock;
 		}
 
-		if (dump_packet_cmsg(&msg, &pe, flags))
+		ret = dump_packet_cmsg(&msg, &pe, flags);
+		if (ret < 0)
 			goto err_set_sock;
 
 		if (msg.msg_namelen) {
 			pe.has_addr = true;
 			pe.addr.data = addr;
 			pe.addr.len = msg.msg_namelen;
+		}
+
+		if (ret > 0) {
+			ret = -1;
+			if (queue_packet_entry(&pe, data, pe.length))
+				goto err_set_sock;
+			continue;
 		}
 
 		ret = pb_write_one(img_from_set(glob_imgset, CR_FD_SK_QUEUES), &pe, PB_SK_QUEUES);

criu/cr-dump.c
@@ -55,6 +55,7 @@
 #include "tty.h"
 #include "net.h"
 #include "sk-packet.h"
+#include "sk-queue.h"
 #include "cpu.h"
 #include "elf.h"
 #include "cgroup.h"
@@ -2231,6 +2232,10 @@ int cr_dump_tasks(pid_t pid)
 		goto err;
 
 	ret = tty_post_actions();
+	if (ret)
+		goto err;
+
+	ret = sk_queue_post_actions();
 	if (ret)
 		goto err;
 

criu/include/sk-queue.h
@@ -6,6 +6,7 @@ extern struct collect_image_info sk_queues_cinfo;
 #define SK_QUEUE_REAL_PID	0x1 /* scm creds contains a real pid */
 #define SK_QUEUE_DUMP_ADDR	0x2 /* save a sender address for messages */
 extern int dump_sk_queue(int sock_fd, int sock_id, int flags);
+extern int sk_queue_post_actions(void);
 extern int restore_sk_queue(int fd, unsigned int peer_id);
 
 #endif /* __CR_SK_QUEUE_H__ */

commit c7c5c94d04b2d847e966b2f30e3512d732fa8fef

sk-queue: fix dumping queue packets
When we have several SCM_RIGHTS packets and at least one SCM_CREDENTIALS
sent simultaneousely into same unix socket we see that SCM_RIGHTS are
dumped wrong. It happens because before this patch SkPacketEntry had
been reused when dumping consequent packets, thus when dumping
SCM_RIGHTS for non-first packet it's pe.scm also contained SCM_RIGHTS
for previous packet.

To fix this let's not reuse SkPacketEntry. Also while on it let's split
dump_sk_queue_packet() - separate function for dumping one packet, to
add more readability to the code. Also drop meaningless "ret = -E2BIG"
and staff. Note that after this change pe.ucred is also not reused as it
probably should've always been.

https://jira.sw.ru/browse/PSBM-139953
Fixes: e4956b0b1 ("vz7: sk-queue -- Add support of ucreds in unix sockets")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/sk-queue.c
@@ -413,10 +413,99 @@ static int dump_packet_cmsg(struct msghdr *mh, SkPacketEntry *pe, int flags)
 	return ret;
 }
 
-int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val)
+static int dump_sk_queue_packet(int sock_fd, int sock_id, void *data, int size, int flags, int *val)
 {
 	SkPacketEntry pe = SK_PACKET_ENTRY__INIT;
+	int ret, exit_code = -1;
+	char cmsg[CMSG_MAX_SIZE];
+	unsigned char addr[_K_SS_MAXSIZE];
+	struct iovec iov = {
+		.iov_base	= data,
+		.iov_len	= size,
+	};
+	struct msghdr msg = {
+		.msg_iov	= &iov,
+		.msg_iovlen	= 1,
+		.msg_control	= &cmsg,
+		.msg_controllen	= sizeof(cmsg),
+	};
+
+	pe.id_for = sock_id;
+
+	if (flags & SK_QUEUE_DUMP_ADDR) {
+		msg.msg_name	= addr;
+		msg.msg_namelen	= _K_SS_MAXSIZE;
+	}
+
+	ret = pe.length = recvmsg(sock_fd, &msg, MSG_DONTWAIT | MSG_PEEK);
+	if (!ret) {
+		/*
+		 * It means, that peer has performed an
+		 * orderly shutdown, so we're done.
+		 */
+		return 1;
+	} else if (ret < 0) {
+		if (errno == EAGAIN)
+			return 1;
+
+		if (errno == ENOBUFS && flags & SK_QUEUE_TRACK_ENOBUFS && val) {
+			*val = ENOBUFS;
+			pr_debug("Detected ENOBUFS in socket\n");
+			return 0;
+		}
+
+		pr_perror("recvmsg fail: error");
+		return -1;
+	}
+
+	if (msg.msg_flags & MSG_TRUNC) {
+		/*
+		 * DGRAM truncated. This should not happen. But we have
+		 * to check...
+		 */
+		pr_err("recvmsg failed: truncated\n");
+		return -1;
+	}
+
+	ret = dump_packet_cmsg(&msg, &pe, flags);
+	if (ret < 0)
+		goto cleanup_packet;
+
+	if (flags & SK_QUEUE_DUMP_ADDR && msg.msg_namelen) {
+		pe.has_addr = true;
+		pe.addr.data = msg.msg_name;
+		pe.addr.len = msg.msg_namelen;
+	}
+
+	if (ret > 0) {
+		if (ret == 1) {
+			if (queue_packet_entry(&pe, data, pe.length))
+				goto cleanup_packet;
+		}
+		exit_code = 0;
+		goto cleanup_packet;
+	}
+
+	ret = pb_write_one(img_from_set(glob_imgset, CR_FD_SK_QUEUES), &pe, PB_SK_QUEUES);
+	if (ret < 0)
+		goto cleanup_packet;
+
+	ret = write_img_buf(img_from_set(glob_imgset, CR_FD_SK_QUEUES), data, pe.length);
+	if (ret < 0)
+		goto cleanup_packet;
+
+	exit_code = 0;
+cleanup_packet:
+	if (pe.scm)
+		release_cmsg(&pe);
+	xfree(pe.ucred);
+	return exit_code;
+}
+
+int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val)
+{
 	int ret, size, orig_peek_off;
+	int exit_code = -1;
 	void *data;
 	socklen_t tmp;
 
@@ -428,7 +517,7 @@ int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val)
 	ret = getsockopt(sock_fd, SOL_SOCKET, SO_PEEK_OFF, &orig_peek_off, &tmp);
 	if (ret < 0) {
 		pr_perror("getsockopt failed");
-		return ret;
+		return -1;
 	}
 	/*
 	 * Discover max DGRAM size
@@ -438,7 +527,7 @@ int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val)
 	ret = getsockopt(sock_fd, SOL_SOCKET, SO_SNDBUF, &size, &tmp);
 	if (ret < 0) {
 		pr_perror("getsockopt failed");
-		return ret;
+		return -1;
 	}
 
 	/* Note: 32 bytes will be used by kernel for protocol header. */
@@ -457,95 +546,18 @@ int dump_sk_queue(int sock_fd, int sock_id, int flags, int *val)
 	ret = setsockopt(sock_fd, SOL_SOCKET, SO_PEEK_OFF, &ret, sizeof(int));
 	if (ret < 0) {
 		pr_perror("setsockopt fail");
-		goto err_brk;
+		goto err_free;
 	}
 
-	pe.id_for = sock_id;
 
 	while (1) {
-		char cmsg[CMSG_MAX_SIZE];
-		unsigned char addr[_K_SS_MAXSIZE];
-		struct iovec iov = {
-			.iov_base	= data,
-			.iov_len	= size,
-		};
-		struct msghdr msg = {
-			.msg_iov	= &iov,
-			.msg_iovlen	= 1,
-			.msg_control	= &cmsg,
-			.msg_controllen	= sizeof(cmsg),
-		};
-
-		if (flags & SK_QUEUE_DUMP_ADDR) {
-			msg.msg_name	= addr;
-			msg.msg_namelen	= _K_SS_MAXSIZE;
-		}
-
-		ret = pe.length = recvmsg(sock_fd, &msg, MSG_DONTWAIT | MSG_PEEK);
-		if (!ret)
-			/*
-			 * It means, that peer has performed an
-			 * orderly shutdown, so we're done.
-			 */
+		ret = dump_sk_queue_packet(sock_fd, sock_id, data, size, flags, val);
+		if (ret == 1)
 			break;
-		else if (ret < 0) {
-			if (errno == EAGAIN)
-				break; /* we're done */
-
-			if (errno == ENOBUFS && flags & SK_QUEUE_TRACK_ENOBUFS && val) {
-				*val = ENOBUFS;
-				pr_debug("Detected ENOBUFS in socket\n");
-				continue;
-			}
-
-			pr_perror("recvmsg fail: error");
+		else if (ret == -1)
 			goto err_set_sock;
-		}
-		if (msg.msg_flags & MSG_TRUNC) {
-			/*
-			 * DGRAM truncated. This should not happen. But we have
-			 * to check...
-			 */
-			pr_err("sys_recvmsg failed: truncated\n");
-			ret = -E2BIG;
-			goto err_set_sock;
-		}
-
-		ret = dump_packet_cmsg(&msg, &pe, flags);
-		if (ret < 0)
-			goto err_set_sock;
-
-		if (msg.msg_namelen) {
-			pe.has_addr = true;
-			pe.addr.data = addr;
-			pe.addr.len = msg.msg_namelen;
-		}
-
-		if (ret > 0) {
-			if (ret == 1) {
-				ret = -1;
-				if (queue_packet_entry(&pe, data, pe.length))
-					goto err_set_sock;
-			}
-			continue;
-		}
-
-		ret = pb_write_one(img_from_set(glob_imgset, CR_FD_SK_QUEUES), &pe, PB_SK_QUEUES);
-		if (ret < 0) {
-			ret = -EIO;
-			goto err_set_sock;
-		}
-
-		ret = write_img_buf(img_from_set(glob_imgset, CR_FD_SK_QUEUES), data, pe.length);
-		if (ret < 0) {
-			ret = -EIO;
-			goto err_set_sock;
-		}
-
-		if (pe.scm)
-			release_cmsg(&pe);
 	}
-	ret = 0;
+	exit_code = 0;
 
 err_set_sock:
 	/*
@@ -555,12 +567,9 @@ err_set_sock:
 		pr_perror("setsockopt failed on restore");
 		ret = -1;
 	}
-	if (pe.scm)
-		release_cmsg(&pe);
-err_brk:
-	xfree(pe.ucred);
+err_free:
 	xfree(data);
-	return ret;
+	return exit_code;
 }
 
 static int send_one_pkt(int fd, struct sk_packet *pkt)