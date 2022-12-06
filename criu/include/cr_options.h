d986e6df1aa9013556cc86440e8b7616c07299f0
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2022-12-06 18:04:20 +0300

    net: dump nft rules via both nft and iptables-nft
    We see nft rules with matches like "physdev" or "match-set" which can be
    created by iptables-nft but can not be dumped/restored by nft utility.
    We also see custom nft tables (not "filter", "nat" or "mangle", etc.)
    created by nft are not dumped by iptables-nft.
    
    The idea of this patch is to dump nftables rules both with nft and
    iptables-nft, and on restore try to restore them both.
    
    The --nftables-mode=ipt/nft/all option is added to switch to nft only or
    itpables-nft only mode (default is all).
    
    Feature: Nftables
    
    https://jira.sw.ru/browse/PSBM-138902
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/include/cr_options.h
+++ criu/include/cr_options.h
@@ -93,6 +93,13 @@ enum FILE_VALIDATION_OPTIONS {
 /* This constant dictates which file validation method should be tried by default. */
 #define FILE_VALIDATION_DEFAULT FILE_VALIDATION_BUILD_ID
 
+enum {
+	NFTABLES_MODE_IPT,
+	NFTABLES_MODE_NFT,
+	NFTABLES_MODE_ALL,
+};
+#define NFTABLES_MODE_DEFAULT NFTABLES_MODE_ALL
+
 struct irmap;
 
 struct irmap_path_opt {
@@ -212,6 +219,7 @@ struct cr_options {
 	char *ve;
 	char *ve_clock_fallback;
 	int cgroup_force_create_ns;
+	int nftables_mode;
 };
 
 extern struct cr_options opts;
