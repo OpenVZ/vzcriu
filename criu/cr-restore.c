OpenVZ/vzcriu

commit 58639f9c62e6b7b7359b7d3bfcad20ba5ae56d98

sk-unix: make add_fake_unix_queuers earier and rework find_queuer_for
Before this patch, if we had a unixsk with incomming scm packets (with
fds) and with the sender side fd closed, we got an error:

Error (criu/sk-unix.c:1125): unix: Can't find sender for 0x1e

First part of the problem is that unix_note_scm_rights() expects to see
a "queuer" which would send scm packets to the unixsk, and there is no
as the sender side is closed.

Second part of the problem is that we already have "fake" queuers
feature so that it already creates a unix socket pair and leaves other
end open for later queuing packets. But function add_fake_unix_queuers()
is called after unix_note_scm_rights() thus there is no chance to find
queuer at the point of failure.

Third part is that when we look for a queuer in find_queuer_for() we
actually look for a socket for which we are a queuer and not for the
socket which is a queuer for us, which is opposite to the name. For
cases where both ends are alive both are queuers for each other so this
was not important, but for our closed sender case it breaks.

So let's reorder add_fake_unix_queuers() before unix_note_scm_rights()
and make find_queuer_for() actually do what it's name implies.

This situation is started to reproduce on our start/stop tests with the
unixsk belonging to systemd, we suppose that this state where the sender
fd side is closed happens rarely only on systemd start/stop, so we don't
see it in regular suspend resume of long-living containers.

https://jira.sw.ru/browse/PSBM-140414

Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/cr-restore.c
@@ -385,6 +385,10 @@ static int root_prepare_shared(void)
 	if (ret)
 		goto err;
 
+	ret = add_fake_unix_queuers();
+	if (ret)
+		goto err;
+
 	/*
 	 * This should be called with all packets collected AND all
 	 * fdescs and fles prepared BUT post-prep-s not run.
@@ -398,10 +402,6 @@ static int root_prepare_shared(void)
 		goto err;
 
 	ret = unix_prepare_root_shared();
-	if (ret)
-		goto err;
-
-	ret = add_fake_unix_queuers();
 	if (ret)
 		goto err;
 

criu/sk-unix.c
@@ -1029,8 +1029,8 @@ static struct unix_sk_info *find_queuer_for(int id)
 	struct unix_sk_info *ui;
 
 	list_for_each_entry(ui, &unix_sockets, list) {
-		if (ui->queuer && ui->queuer->ue->id == id)
-			return ui;
+		if (ui->queuer && ui->ue->id == id)
+			return ui->queuer;
 	}
 
 	return NULL;

commit fb7ac8a5334aec9e40cd43905220374d0f592580

sk-unix: split add_fake_unix_queuers in two parts
Standard "chicken of the egg" problem here.

In stack:

root_prepare_shared
  add_fake_unix_queuers
    add_fake_queuer
      file_master
        try_file_master

We need non-empty desc->fd_info_head.

But fd_info_head list is populated for socket in scm only in:

root_prepare_shared
  prepare_scms
    unix_note_scm_rights
      get_fle_for_task
        collect_fd_to
          collect_desc_fle

Which is executed later.

We also have a problem from [1] where instead prepare_scms should be
strictly after add_fake_unix_queuers.

So let's split add_fake_unix_queuers to overcome this.

https://jira.sw.ru/browse/PSBM-143751

Fixes: 58639f9c6 ("sk-unix: make add_fake_unix_queuers earier and rework find_queuer_for") [1]
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/cr-restore.c
@@ -397,6 +397,10 @@ static int root_prepare_shared(void)
 	if (ret)
 		goto err;
 
+	ret = add_fake_unix_queuers_finish();
+	if (ret)
+		goto err;
+
 	ret = run_post_prepare();
 	if (ret)
 		goto err;

criu/include/sockets.h
@@ -43,6 +43,7 @@ extern struct collect_image_info inet_sk_cinfo;
 extern int fini_dump_sockets(struct ns_id *ns);
 extern struct collect_image_info unix_sk_cinfo;
 extern int add_fake_unix_queuers(void);
+extern int add_fake_unix_queuers_finish(void);
 extern int fix_external_unix_sockets(void);
 extern int prepare_scms(void);
 extern int unix_note_scm_rights(int id_for, uint32_t *file_ids, int *fds, int n_ids);

criu/sk-unix.c
@@ -2911,7 +2911,6 @@ static void set_peer(struct unix_sk_info *ui, struct unix_sk_info *peer)
 static int add_fake_queuer(struct unix_sk_info *ui)
 {
 	struct unix_sk_info *peer;
-	struct pstree_item *task;
 	UnixSkEntry *peer_ue;
 	SkOptsEntry *skopts;
 	FownEntry *fown;
@@ -2949,9 +2948,14 @@ static int add_fake_queuer(struct unix_sk_info *ui)
 	file_desc_add(&peer->d, peer_ue->id, &unix_desc_ops);
 	list_del_init(&peer->d.fake_master_list);
 	list_add(&peer->list, &unix_sockets);
-	task = file_master(&ui->d)->task;
+	return 0;
+}
 
-	return (get_fle_for_task(&peer->d, task, true) == NULL);
+static int add_fake_queuer_finish(struct unix_sk_info *queuer)
+{
+	struct pstree_item *task = file_master(&queuer->peer->d)->task;
+
+	return get_fle_for_task(&queuer->d, task, true) == NULL;
 }
 
 int add_fake_unix_queuers(void)
@@ -2967,6 +2971,22 @@ int add_fake_unix_queuers(void)
 		    !(ui->ue->type == SOCK_DGRAM  && !ui->peer))
 			continue;
 		if (add_fake_queuer(ui))
+			return -1;
+	}
+	return 0;
+}
+
+int add_fake_unix_queuers_finish(void)
+{
+	struct unix_sk_info *ui;
+
+	pr_info("Adding fake unix queuers (finish)\n");
+
+	list_for_each_entry(ui, &unix_sockets, list) {
+		if (ui->ue->ino != FAKE_INO)
+			continue;
+
+		if (add_fake_queuer_finish(ui))
 			return -1;
 	}
 	return 0;