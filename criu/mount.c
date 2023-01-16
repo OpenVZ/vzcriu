8cc9ac86eb2a7748f6466e674a547bafe45a697d
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2023-01-16 16:33:39 +0300

    mount/dump: check opts.ve for binfmt_misc earlier
    There is no point in calling anything if opts.ve is not set, binfmt_misc
    is only supported in ve.
    
    Feature: Binfmtmisc
    
    https://jira.sw.ru/browse/PSBM-144416
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -1770,11 +1770,6 @@ static __maybe_unused int mount_and_open_binfmt_misc(unsigned int *s_dev)
 	struct stat st;
 	int mnt_fd;
 
-	if (!opts.ve) {
-		pr_debug("Skipping binfmt_misc as --ve was not specifided\n");
-		return BINFMT_MOUNT_SKIPPED;
-	}
-
 	if (join_veX())
 		return BINFMT_MOUNT_FAILED;
 
@@ -4068,9 +4063,13 @@ int collect_mnt_namespaces(bool for_dump)
 
 #ifdef CONFIG_BINFMT_MISC_VIRTUALIZED
 	if (for_dump) {
-		ret = call_in_child_process(mount_and_dump_binfmt_misc, NULL);
-		if (ret)
-			goto err;
+		if (!opts.ve) {
+			pr_debug("Skipping binfmt_misc as --ve was not specifided\n");
+		} else {
+			ret = call_in_child_process(mount_and_dump_binfmt_misc, NULL);
+			if (ret)
+				goto err;
+		}
 	}
 #endif
 
