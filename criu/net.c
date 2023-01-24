OpenVZ/vzcriu

commit 715371b99344c151abe50dc68ffce686da5b34f8

net: dump nft rules via both nft and iptables-nft
We see nft rules with matches like "physdev" or "match-set" which can be
created by iptables-nft but can not be dumped/restored by nft utility.
We also see custom nft tables (not "filter", "nat" or "mangle", etc.)
created by nft are not dumped by iptables-nft.

The idea of this patch is to dump nftables rules both with nft and
iptables-nft, and on restore try to restore them both.

The --nftables-mode=ipt/nft/all option is added to switch to nft only or
itpables-nft only mode (default is all).

https://jira.sw.ru/browse/PSBM-138902

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/net.c
@@ -3203,6 +3203,16 @@ static inline int dump_nftables(struct cr_imgset *fds)
 {
 	struct cr_img *img;
 
+	img = img_from_set(fds, CR_FD_IPTABLES_NFT);
+	if (nftables_tool_dump("xtables-nft-multi iptables-save", -1, img_raw_fd(img)))
+		return -1;
+
+	if (kdat.ipv6) {
+		img = img_from_set(fds, CR_FD_IP6TABLES_NFT);
+		if (nftables_tool_dump("xtables-nft-multi ip6tables-save", -1, img_raw_fd(img)))
+			return -1;
+	}
+
 	img = img_from_set(fds, CR_FD_NFTABLES);
 	if (nftables_tool_dump("nft list ruleset", -1, img_raw_fd(img)))
 		return -1;
@@ -3217,23 +3227,59 @@ static int nftables_tool_restore(char *def_cmd, int fdin, int fdout)
 
 static inline int restore_nftables(int pid)
 {
-	int ret = -1;
+	int ret;
 	struct cr_img *img;
+
+	if (opts.nftables_mode == NFTABLES_MODE_IPT)
+		goto ipt;
 
 	img = open_image(CR_FD_NFTABLES, O_RSTR, pid);
 	if (img == NULL)
 		return -1;
 	if (empty_image(img)) {
 		/* Backward compatibility */
-		pr_info("Skipping nft restore, no image");
-		ret = 0;
-		goto out;
+		pr_info("Skipping nft restore, no image\n");
+		close_image(img);
+		goto ipt;
 	}
 
 	ret = nftables_tool_restore("nft -f /proc/self/fd/0", img_raw_fd(img), -1);
-out:
 	close_image(img);
-	return ret;
+	if (ret)
+		return -1;
+
+	if (opts.nftables_mode == NFTABLES_MODE_NFT)
+		return 0;
+ipt:
+	img = open_image(CR_FD_IPTABLES_NFT, O_RSTR, pid);
+	if (img == NULL)
+		return -1;
+	if (empty_image(img)) {
+		/* Backward compatibility */
+		pr_info("Skipping ipt-nft restore, no image\n");
+		close_image(img);
+		return 0;
+	}
+
+	ret = nftables_tool_restore("xtables-nft-multi iptables-restore -w", img_raw_fd(img), -1);
+	close_image(img);
+	if (ret)
+		return -1;
+
+	img = open_image(CR_FD_IP6TABLES_NFT, O_RSTR, pid);
+	if (img == NULL)
+		return -1;
+	if (empty_image(img)) {
+		pr_info("Skipping ip6t-nft restore, no image\n");
+		close_image(img);
+		return 0;
+	}
+
+	ret = nftables_tool_restore("xtables-nft-multi ip6tables-restore -w", img_raw_fd(img), -1);
+	close_image(img);
+	if (ret)
+		return -1;
+	return 0;
 }
 
 int read_net_ns_img(void)

criu/config.c
@@ -281,6 +281,7 @@ void init_opts(void)
 	opts.log_level = DEFAULT_LOGLEVEL;
 	opts.pre_dump_mode = PRE_DUMP_SPLICE;
 	opts.file_validation_method = FILE_VALIDATION_DEFAULT;
+	opts.nftables_mode = NFTABLES_MODE_DEFAULT;
 }
 
 bool deprecated_ok(char *what)
@@ -547,6 +548,7 @@ int parse_options(int argc, char **argv, bool *usage_error,
 		{ "ve",				required_argument,	0, 2001},
 		BOOL_OPT("cgroup-force-create-ns", &opts.cgroup_force_create_ns),
 		{ "ve-clock-fallback",		required_argument,	0, 2002},
+		{ "nftables-mode",		required_argument,	0, 2003},
 		{ },
 	};
 
@@ -885,6 +887,18 @@ int parse_options(int argc, char **argv, bool *usage_error,
 			break;
 		case 2002:
 			SET_CHAR_OPTS(ve_clock_fallback, optarg);
+			break;
+		case 2003:
+			if (!strcmp("ipt", optarg)) {
+				opts.nftables_mode = NFTABLES_MODE_IPT;
+			} else if (!strcmp("nft", optarg)) {
+				opts.nftables_mode = NFTABLES_MODE_NFT;
+			} else if (!strcmp("all", optarg)) {
+				opts.nftables_mode = NFTABLES_MODE_ALL;
+			} else {
+				pr_err("Unable to parse value of --nftables-mode\n");
+				return 1;
+			}
 			break;
 		case 'V':
 			pr_msg("Version: %s\n", CRIU_VERSION);

criu/image-desc.c
@@ -79,6 +79,8 @@ struct cr_fd_desc_tmpl imgset_template[CR_FD_MAX] = {
 	FD_ENTRY_F(IPTABLES,	"iptables-%u", O_NOBUF),
 	FD_ENTRY_F(IP6TABLES,	"ip6tables-%u", O_NOBUF),
 	FD_ENTRY_F(NFTABLES,	"nftables-%u", O_NOBUF),
+	FD_ENTRY_F(IPTABLES_NFT, "vz-iptables-nft-%u", O_NOBUF),
+	FD_ENTRY_F(IP6TABLES_NFT, "vz-ip6tables-nft-%u", O_NOBUF),
 	FD_ENTRY_F(TMPFS_IMG,	"tmpfs-%u.tar.gz", O_NOBUF),
 	FD_ENTRY_F(TMPFS_DEV,	"tmpfs-dev-%u.tar.gz", O_NOBUF),
 	FD_ENTRY_F(AUTOFS,	"autofs-%u", O_NOBUF),

criu/include/cr_options.h
@@ -81,6 +81,13 @@ enum FILE_VALIDATION_OPTIONS
 /* This constant dictates which file validation method should be tried by default. */
 #define FILE_VALIDATION_DEFAULT			FILE_VALIDATION_BUILD_ID
 
+enum {
+	NFTABLES_MODE_IPT,
+	NFTABLES_MODE_NFT,
+	NFTABLES_MODE_ALL,
+};
+#define NFTABLES_MODE_DEFAULT NFTABLES_MODE_ALL
+
 struct irmap;
 
 struct irmap_path_opt {
@@ -181,6 +188,7 @@ struct cr_options {
 	int			cgroup_force_create_ns;
 
 	char			*ve_clock_fallback;
+	int			nftables_mode;
 };
 
 extern struct cr_options opts;

criu/include/image-desc.h
@@ -46,6 +46,8 @@ enum {
 	CR_FD_IPTABLES,
 	CR_FD_IP6TABLES,
 	CR_FD_NFTABLES,
+	CR_FD_IPTABLES_NFT,
+	CR_FD_IP6TABLES_NFT,
 	CR_FD_NETNS,
 	CR_FD_NETNF_CT,
 	CR_FD_NETNF_EXP,

criu/include/magic.h
@@ -112,6 +112,8 @@
 #define IPTABLES_MAGIC		RAW_IMAGE_MAGIC
 #define IP6TABLES_MAGIC		RAW_IMAGE_MAGIC
 #define NFTABLES_MAGIC		RAW_IMAGE_MAGIC
+#define IPTABLES_NFT_MAGIC	RAW_IMAGE_MAGIC
+#define IP6TABLES_NFT_MAGIC	RAW_IMAGE_MAGIC
 #define NETNF_CT_MAGIC		RAW_IMAGE_MAGIC
 #define NETNF_EXP_MAGIC		RAW_IMAGE_MAGIC
 

commit 728c21452f6c690948a98bae03177745cd2ad2d2

net: ignore if one of nft or iptables-nft fails and other succeeds
We can create rule with iptables-nft like:

ip6tables-nft -A INPUT -p ipv6-icmp -m icmp6 --icmpv6-type 130 -j ACCEPT  # Multicast Listener Query [RFC2710]
ip6tables-nft -A INPUT -p ipv6-icmp -m icmp6 --icmpv6-type 131 -j ACCEPT  # Multicast Listener Report [RFC2710]
ip6tables-nft -A INPUT -p ipv6-icmp -m icmp6 --icmpv6-type 132 -j ACCEPT  # Multicast Listener Done [RFC2710]

Which would fail if saved/restored with nft util.

So the idea is to automatically skip nft util fail if iptables-nft
succeeds and vice versa, so that criu works even if only one tool is
able to restore nftables set.

Feature: Nftables

https://jira.sw.ru/browse/PSBM-144625
Fixes: 715371b99 ("net: dump nft rules via both nft and iptables-nft")j
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/net.c
@@ -3222,9 +3222,17 @@ static int nftables_tool_restore(char *def_cmd, int fdin, int fdout)
 	return tables_restore(run_nftables_tool, def_cmd, fdin, fdout);
 }
 
+/*
+ * NFTABLES_MODE_NFT - try to restore nftables with nft tool only, if no image
+ * just silently skip in case we restore from really old criu dump.
+ * NFTABLES_MODE_IPT - try to restore nftables with iptables-nft tool only,
+ * fail if no image.
+ * NFTABLES_MODE_ALL - try to restore nftables with both nft and iptables-nft,
+ * if one or both succeed we are good.
+ */
 static inline int restore_nftables(int pid)
 {
-	int ret;
+	int ret, exit_code = -1;
 	struct cr_img *img;
 
 	if (opts.nftables_mode == NFTABLES_MODE_IPT)
@@ -3237,46 +3245,50 @@ static inline int restore_nftables(int pid)
 		/* Backward compatibility */
 		pr_info("Skipping nft restore, no image\n");
 		close_image(img);
-		goto ipt;
+		return 0;
 	}
 
 	ret = nftables_tool_restore("nft -f /proc/self/fd/0", img_raw_fd(img), -1);
 	close_image(img);
-	if (ret)
-		return -1;
+	if (!ret)
+		exit_code = 0;
 
 	if (opts.nftables_mode == NFTABLES_MODE_NFT)
-		return 0;
+		goto out;
 ipt:
 	img = open_image(CR_FD_IPTABLES_NFT, O_RSTR, pid);
 	if (img == NULL)
 		return -1;
 	if (empty_image(img)) {
+		if (opts.nftables_mode == NFTABLES_MODE_IPT) {
+			pr_err("Missing ipt-nft image. Maybe you should not use --nftables-mode=ipt.\n");
+			return -1;
+		}
 		/* Backward compatibility */
 		pr_info("Skipping ipt-nft restore, no image\n");
 		close_image(img);
-		return 0;
+		goto out;
 	}
 
 	ret = nftables_tool_restore("xtables-nft-multi iptables-restore -w", img_raw_fd(img), -1);
 	close_image(img);
 	if (ret)
-		return -1;
+		goto out;
 
 	img = open_image(CR_FD_IP6TABLES_NFT, O_RSTR, pid);
 	if (img == NULL)
 		return -1;
 	if (empty_image(img)) {
-		pr_info("Skipping ip6t-nft restore, no image\n");
-		close_image(img);
-		return 0;
+		pr_err("Missing ip6t-nft image. Try using --nftables-mode=nft.\n");
+		return -1;
 	}
 
 	ret = nftables_tool_restore("xtables-nft-multi ip6tables-restore -w", img_raw_fd(img), -1);
 	close_image(img);
-	if (ret)
-		return -1;
-	return 0;
+	if (!ret)
+		exit_code = 0;
+out:
+	return exit_code;
 }
 
 int read_net_ns_img(void)