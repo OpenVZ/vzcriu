7354c89430b2fa7e34ee7d2e8d78370e38e20263
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2023-01-25 12:53:21 +0300

    sk-queue: use flags in dump_sk_queue
    https://jira.sw.ru/browse/PSBM-144416
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Netlink packet queue

--- criu/include/sk-queue.h
+++ criu/include/sk-queue.h
@@ -2,7 +2,9 @@
 #define __CR_SK_QUEUE_H__
 
 extern struct collect_image_info sk_queues_cinfo;
-extern int dump_sk_queue(int sock_fd, int sock_id, bool dump_addr);
+
+#define SK_QUEUE_DUMP_ADDR 0x2 /* save a sender address for messages */
+extern int dump_sk_queue(int sock_fd, int sock_id, int flags);
 extern int restore_sk_queue(int fd, unsigned int peer_id);
 
 #endif /* __CR_SK_QUEUE_H__ */
