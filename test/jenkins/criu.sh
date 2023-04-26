f5d7bc4b984588f5326bef944525562e2406f00e
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2023-04-26 11:54:48 +0800

    jenkins/zdtm: use --mntns-compat-mode option without --no-
    Jenkins does not know about --no- option and also it would have no efect
    anyway because --no- is default.
    
    https://jira.vzint.dev/browse/PSBM-147000
    Fixes: 35e4a243a ("jenkins: run all tests both for old mounts and mounts-v2")
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: Jenkins

--- test/jenkins/criu.sh
+++ test/jenkins/criu.sh
@@ -6,7 +6,7 @@ source `dirname $0`/criu-lib.sh
 prep
 FAIL=0
 ./test/zdtm.py run --all --flavor h,ns,uns --keep-going --report report --parallel 4 || FAIL=$?
-./test/zdtm.py run --all --flavor h,ns,uns --no-mntns-compat-mode --keep-going --report report --parallel 4 || FAIL=$?
+./test/zdtm.py run --all --flavor h,ns,uns --mntns-compat-mode --keep-going --report report --parallel 4 || FAIL=$?
 
 if [ $FAIL -ne 0 ]; then
         fail
