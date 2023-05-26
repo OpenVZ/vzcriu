OpenVZ/vzcriu

commit 458370e42825d574240b7a603210af1875c90750

vz7: scripts: check service mountpoint is not empty in systemd-autofs-restart.sh
Systemctl may fail. Let's check its result and mountpoint string after call.

https://jira.sw.ru/browse/PSBM-67544
Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

Feature: Auto mounts

scripts/systemd-autofs-restart.sh
@@ -151,7 +151,15 @@ function restore_mountpoint {
 function restart_service {
 	local service=$1
 	local mountpoint
-	mountpoint=$($JOIN_CT "$SYSTEMCTL" show "$service" -p Where | sed 's/.*=//g')
+	if mountpoint=$($JOIN_CT "$SYSTEMCTL" show "$service" -p Where | sed 's/.*=//g'); then
+		echo "Failed to get mountpoint for $service service"
+		return 1
+	fi
+
+	if [ -z "$mountpoint" ]; then
+		echo "$service service mountpoint string is empty"
+		return 1
+	fi
 
 	# Try to move restored bind-mount aside and exit if Failed
 	# Nothing to do, if we Failed

commit d89c93f50dfb9004b8f980aa6d3e35b3e5059f30

vz7: systemd-autofs-restart.sh: fix error condition check
When fixing lint errors at rebase, $? result check was replaced by using
the comand in if condition directly but logic was broken as now error
hangling branch triggers when command is succesful, let's fix that by
inverting the condition.

https://jira.vzint.dev/browse/PSBM-147268
Fixes: 458370e42 ("vz7: scripts: check service mountpoint is not empty in systemd-autofs-restart.sh")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

Feature: Auto mounts

scripts/systemd-autofs-restart.sh
@@ -154,7 +154,7 @@ function restore_mountpoint {
 function restart_service {
 	local service=$1
 	local mountpoint
-	if mountpoint=$($JOIN_CT "$SYSTEMCTL" show "$service" -p Where </dev/null | sed 's/.*=//g'); then
+	if ! mountpoint=$($JOIN_CT "$SYSTEMCTL" show "$service" -p Where </dev/null | sed 's/.*=//g'); then
 		echo "Failed to get mountpoint for $service service"
 		return 1
 	fi