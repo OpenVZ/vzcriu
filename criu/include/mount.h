OpenVZ/vzcriu

commit ce34b982ae5827eaa675e4aab961a864f9785d58

spfs: do mount request from mount namespace of nfs mount
Spfs takes mount namespace from root task pid and saves it, that's why
we need to change root task mount namespace for the time of request.

https://jira.vzint.dev/browse/PSBM-147276
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

Feature: SPFS

criu/spfs.c
@@ -13,8 +13,10 @@
 #include <sys/un.h>
 #include <sys/stat.h>
 #include <sys/xattr.h>
+#include <sys/mount.h>
 
 #include "mount.h"
+#include "mount-v2.h"
 #include "log.h"
 #include "util.h"
 #include "cr_options.h"
@@ -380,28 +382,73 @@ out:
 int spfs_mount(struct mount_info *mi, const char *source,
 	       const char *filesystemtype, unsigned long mountflags)
 {
-	int ret;
-	int sock;
+	int nsfd = -1, orig_nsfd = -1;
+	int ret, exit_code = 1;
+	int sock = -1;
 
 	sock = start_spfs_mngr();
 	if (sock < 0) {
 		pr_err("failed to connect to SPFS manager: %d\n", sock);
-		ret = sock;
 		goto err;
 	}
+
+	/*
+	 * Switch to mntns of nfs mount, so that spfs saves right mntns from us
+	 */
+	if (!opts.mntns_compat_mode) {
+		if (remove_plain_mountpoint(mi))
+			goto err;
+
+		nsfd = fdstore_get(mi->nsid->mnt.nsfd_id);
+		if (nsfd < 0)
+			goto err;
+
+		if (switch_ns_by_fd(nsfd, &mnt_ns_desc, &orig_nsfd))
+			goto err;
+
+		if (create_plain_mountpoint(mi))
+			goto err;
+	}
+
 	ret = spfs_request_mount(sock, mi, source, filesystemtype, mountflags);
-	close(sock);
+	close_safe(&sock);
 	if (ret) {
 		pr_err("mount request for %s (%s) failed: %d\n",
 		       source, filesystemtype, ret);
 		goto err;
 	}
 
-	return 0;
+	/*
+	 * Move spfs mount back to service mntns
+	 */
+	if (!opts.mntns_compat_mode) {
+		if (bind_plain_to_other_mntns(mi, orig_nsfd, NULL)) {
+			pr_err("Failed to move mount %d to service mntns\n", mi->mnt_id);
+			goto err;
+		}
 
+		if (switch_ns_by_fd(nsfd, &mnt_ns_desc, NULL))
+			goto err;
+
+		if (umount2(service_mountpoint(mi), MNT_DETACH)) {
+			pr_perror("Failed to umount %d\n", mi->mnt_id);
+			goto err;
+		}
+
+		if (remove_plain_mountpoint(mi))
+			goto err;
+	}
+
+	exit_code = 0;
 err:
-	pr_err("failed to mount NFS to path %s\n", service_mountpoint(mi));
-	return ret;
+	if (orig_nsfd >= 0 && restore_ns(orig_nsfd, &mnt_ns_desc))
+                exit_code = -1;
+	close_safe(&nsfd);
+	close_safe(&sock);
+
+	if (exit_code)
+		pr_err("failed to mount NFS to path %s\n", service_mountpoint(mi));
+	return exit_code;
 }
 
 int spfs_set_env(void)

commit 342f4d9774dfe9cf32d4525cf44b1b47c0973c16

spfs: do not umount original mount
Spfs remembers original spfs mount id (update_spfs_info) and later
checks it (is_mnt_fd) on fd/map/cwd/root/exe replacement, so fd openned
on bindmount of spfs is not swapped back to nfs. We can workaround it by
leaving original spfs mount in restored mntns.

note: Right fix here should be to collect in spfs all ids of all spfs
mounts belonging to one superblock and replace all fds from all those
mounts with fds on nfs from corresponding nfs mount. Now we only fix
degradation which appeared after switching criu mount-v2 to mainstream
mount-v2. Problem that spfs replaces fds only on one mount is out of
scope of this fix.

https://jira.vzint.dev/browse/PSBM-147180
Fixes: ce34b982a ("spfs: do mount request from mount namespace of nfs mount")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

Feature: SPFS

criu/include/mount.h
@@ -173,6 +173,8 @@ struct mount_info {
 
 	struct super_block *sb;
 
+	bool plain_mounted_in_right_mntns;
+
 	void *private; /* associated filesystem data */
 };
 

criu/mount-v2.c
@@ -1096,8 +1096,13 @@ static int do_mount_in_right_mntns(struct mount_info *mi)
 	if (nsfd < 0)
 		return -1;
 
-	if (bind_plain_to_other_mntns(mi, nsfd, &orig_nsfd))
-		goto err;
+	if (mi->plain_mounted_in_right_mntns) {
+		if (switch_ns_by_fd(nsfd, &mnt_ns_desc, &orig_nsfd))
+			goto err;
+	} else {
+		if (bind_plain_to_other_mntns(mi, nsfd, &orig_nsfd))
+			goto err;
+	}
 
 	if (prepare_unix_sockets(mi))
 		goto err;

criu/spfs.c
@@ -426,17 +426,9 @@ int spfs_mount(struct mount_info *mi, const char *source,
 			pr_err("Failed to move mount %d to service mntns\n", mi->mnt_id);
 			goto err;
 		}
-
-		if (switch_ns_by_fd(nsfd, &mnt_ns_desc, NULL))
-			goto err;
-
-		if (umount2(service_mountpoint(mi), MNT_DETACH)) {
-			pr_perror("Failed to umount %d\n", mi->mnt_id);
-			goto err;
-		}
-
-		if (remove_plain_mountpoint(mi))
-			goto err;
+		/* bind_plain_to_other_mntns already switched us to orig_nsfd */
+		close_safe(&orig_nsfd);
+		mi->plain_mounted_in_right_mntns = true;
 	}
 
 	exit_code = 0;